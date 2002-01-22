82c5dc401a8b9af0ff9a1205298c8a09280680a7
Giacomo Pati <giacomo@apache.org>
2002-01-22 16:59:18 +0000

    some improvements concerning Auth....Actions
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@37849 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/cocoon/acting/IMLAuthenticatorAction.java
+++ src/java/org/apache/lenya/cms/cocoon/acting/IMLAuthenticatorAction.java
@@ -2,6 +2,7 @@ package org.wyona.cms.cocoon.acting;
 
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.thread.ThreadSafe;
 import org.apache.cocoon.environment.Request;
 import org.apache.cocoon.environment.Session;
 
@@ -24,100 +25,112 @@ import org.wyona.cms.ac.Password;
  * @created 1.12.29
  * @version 2.1.6
  */
-public class IMLAuthenticatorAction extends AbstractUsernamePasswordAuthenticatorAction{
-  private String domain=null;
-  private String port=null;
-  private String context=null;
-  private String passwd=null;
-  private String id=null;
-/**
- *
- */
-  public void configure(Configuration conf) throws ConfigurationException{
-    super.configure(conf);
+public class IMLAuthenticatorAction extends AbstractUsernamePasswordAuthenticatorAction implements ThreadSafe {
+    private String domain=null;
+    private String port=null;
+    private String context=null;
+    private String passwd=null;
+    private String type=null;
+    /**
+     *
+     */
+    public void configure(Configuration conf) throws ConfigurationException{
+        super.configure(conf);
 
-    Configuration domainConf=conf.getChild("domain");
-    domain=domainConf.getValue("127.0.0.1");
-    getLogger().debug("CONFIGURATION: domain="+domain);
-
-    Configuration portConf=conf.getChild("port");
-    port=portConf.getValue(null);
-    getLogger().debug("CONFIGURATION: port="+port);
-
-    Configuration contextConf=conf.getChild("context");
-    context=contextConf.getValue(null);
-    getLogger().debug("CONFIGURATION: context="+context);
-
-    Configuration passwdConf=conf.getChild("passwd");
-    passwd=passwdConf.getValue(null);
-    getLogger().debug("CONFIGURATION: passwd="+passwd);
-/*
-    if(passwd == null){
-      throw new ConfigurationException("No passwd path set");
-      }
-*/
-
-    Configuration idConf=conf.getChild("id");
-    id=idConf.getValue(null);
-    getLogger().debug("CONFIGURATION: id="+id);
-/*
-    if(id == null){
-      throw new ConfigurationException("No id set");
-      }
-*/
-    }
-/**
- *
- */
-  public boolean authenticate(String username,String password,Request request,Map map) throws Exception{
-    if((username != null) && (password != null)){
-      String passwordString=null;
-      Document idoc=null;
-      try{
-        String context=request.getContextPath();
-        int port=request.getServerPort();
-        idoc=getIdentityDoc(username,port,context);
-        passwordString=Identity.getPassword(idoc);
+        Configuration domainConf=conf.getChild("domain");
+        domain=domainConf.getValue("127.0.0.1");
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("CONFIGURATION: domain="+domain);
         }
-      catch(Exception e){
-        getLogger().error(".authenticate(): "+e);
-        return false;
+
+        Configuration portConf=conf.getChild("port");
+        port=portConf.getValue(null);
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("CONFIGURATION: port="+port);
         }
-      if(Password.encrypt(password).equals(passwordString)){
-        Session session=request.getSession(true);
-        if(session == null){
-          return false;
+
+        Configuration contextConf=conf.getChild("context");
+        context=contextConf.getValue(null);
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("CONFIGURATION: context="+context);
+        }
+
+        Configuration passwdConf=conf.getChild("passwd");
+        passwd=passwdConf.getValue(null);
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("CONFIGURATION: passwd="+passwd);
+        }
+        /*
+          if(passwd == null){
+          throw new ConfigurationException("No passwd path set");
           }
-        Identity identity=new Identity(username,idoc);
-        getLogger().debug("IDENTITY: "+identity);
-        session.setAttribute("org.wyona.cms.ac.Identity",identity);
-        session.setAttribute("org.wyona.cms.cocoon.acting.IMLAuthenticator.id",id);
-        return true;
+        */
+
+        Configuration typeConf=conf.getChild("type");
+        type=typeConf.getValue(null);
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("CONFIGURATION: type="+type);
         }
-      }
-    return false;
+        /*
+          if(type == null){
+          throw new ConfigurationException("No type set");
+          }
+        */
     }
-/**
- *
- */
-  private Document getIdentityDoc(String username,int port,String context) throws Exception{
-    DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
-    DocumentBuilder db=dbf.newDocumentBuilder();
-    String imlURLString="http://"+domain;
-    if(this.port != null){
-      imlURLString=imlURLString+":"+this.port;
-      }
-    else{
-      imlURLString=imlURLString+":"+port;
-      }
-    if(this.context != null){
-      imlURLString=imlURLString+this.context;
-      }
-    else{
-      imlURLString=imlURLString+context;
-      }
-    imlURLString=imlURLString+"/"+passwd+username+".iml";
-    getLogger().error(imlURLString);
-    return db.parse(new URL(imlURLString).openStream());
+    /**
+     *
+     */
+    public boolean authenticate(String username,String password,Request request,Map map) throws Exception{
+        if((username != null) && (password != null)){
+            String passwordString=null;
+            Document idoc=null;
+            try{
+                String context=request.getContextPath();
+                int port=request.getServerPort();
+                idoc=getIdentityDoc(username,port,context);
+                passwordString=Identity.getPassword(idoc);
+            }
+            catch(Exception e){
+                getLogger().error(".authenticate(): "+e);
+                return false;
+            }
+            if(Password.encrypt(password).equals(passwordString)){
+                Session session=request.getSession(true);
+                if(session == null){
+                    return false;
+                }
+                Identity identity=new Identity(username,idoc);
+                if (getLogger().isDebugEnabled()) {
+                    getLogger().debug("IDENTITY: "+identity);
+                }
+                session.setAttribute("org.wyona.cms.ac.Identity",identity);
+                session.setAttribute("org.wyona.cms.cocoon.acting.IMLAuthenticator.type",type);
+                return true;
+            }
+        }
+        return false;
     }
-  }
+    /**
+     *
+     */
+    private Document getIdentityDoc(String username,int port,String context) throws Exception{
+        DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
+        DocumentBuilder db=dbf.newDocumentBuilder();
+        String imlURLString="http://"+domain;
+        if(this.port != null){
+            imlURLString=imlURLString+":"+this.port;
+        }
+        else{
+            imlURLString=imlURLString+":"+port;
+        }
+        if(this.context != null){
+            imlURLString=imlURLString+this.context;
+        }
+        else{
+            imlURLString=imlURLString+context;
+        }
+        imlURLString=imlURLString+"/"+passwd+username+".iml";
+        getLogger().error(imlURLString);
+        return db.parse(new URL(imlURLString).openStream());
+    }
+}
--- src/java/org/apache/lenya/cms/cocoon/acting/PMLAuthorizerAction.java
+++ src/java/org/apache/lenya/cms/cocoon/acting/PMLAuthorizerAction.java
@@ -1,9 +1,7 @@
 package org.wyona.cms.cocoon.acting;
 
-import org.apache.avalon.framework.configuration.Configurable;
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
-import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.avalon.framework.thread.ThreadSafe;
 import org.apache.cocoon.Constants;
 import org.apache.cocoon.acting.AbstractComplementaryConfigurableAction;
@@ -33,145 +31,157 @@ import org.wyona.cms.ac.Policy;
  * @created 1.11.18
  * @version 2.1.6
  */
-public class PMLAuthorizerAction extends AbstractAuthorizerAction{
-  private String authenticator_id=null;
-  private String domain=null;
-  private String port=null;
-  private String context=null;
-  private String policies=null;
-/**
- *
- */
- public void configure(Configuration conf) throws ConfigurationException{
-   super.configure(conf);
+public class PMLAuthorizerAction extends AbstractAuthorizerAction implements ThreadSafe {
+    private String authenticator_type=null;
+    private String domain=null;
+    private String port=null;
+    private String context=null;
+    private String policies=null;
+    /**
+     *
+     */
+    public void configure(Configuration conf) throws ConfigurationException{
+        super.configure(conf);
 
-   Configuration authenticatorConf=conf.getChild("authenticator");
-   authenticator_id=authenticatorConf.getAttribute("id");
-   getLogger().debug("CONFIGURATION: authenticator id="+authenticator_id);
-/*
-   if(authenticator_id == null){
-     throw new ConfigurationException("No authenticator id set");
-     }
-*/
-
-   Configuration domainConf=conf.getChild("domain");
-   domain=domainConf.getValue("127.0.0.1");
-   getLogger().debug("CONFIGURATION: domain="+domain);
-
-   Configuration portConf=conf.getChild("port");
-   port=portConf.getValue(null);
-   getLogger().debug("CONFIGURATION: port="+port);
-
-   Configuration contextConf=conf.getChild("context");
-   context=contextConf.getValue(null);
-   getLogger().debug("CONFIGURATION: context="+context);
-
-   Configuration policiesConf=conf.getChild("policies");
-   policies=policiesConf.getValue(null);
-   getLogger().debug("CONFIGURATION: policies="+policies);   
-   }
-/**
- *
- */
-  public boolean authorize(Request request,Map map) throws Exception{
-    String remoteAddress=request.getRemoteAddr();
-
-    // Permit Identity and Policy requests for localhost
-    String sitemap_uri=request.getRequestURI();
-    getLogger().error("POLICIES: "+sitemap_uri+" "+policies);
-    if(remoteAddress.equals("127.0.0.1") && (sitemap_uri.indexOf(policies) >= 0)){
-    //if(remoteAddress.equals("127.0.0.1")){
-      return true;
-      }
-    
-    // Get policy
-    Document policyDoc=null;
-    try{
-      policyDoc=getPolicyDoc(request);
-      }
-    catch(Exception e){
-      getLogger().error(".authorize(): "+e);
-      return false;
-      }
-    Policy policy=new Policy(policyDoc,getLogger());
-
-    // Read action (read, write, publish, etc.)
-    String action=XPathAPI.selectSingleNode(policyDoc,"/ac/request/action/@name").getNodeValue(); //"read";
-    getLogger().debug("action: "+action);
-
-    // Check permissions
-    if(policy.authorizeWorld(action)){
-      return true;
-      }
-
-    if(policy.authorizeMachine(action,remoteAddress)){
-      return true;
-      }
-
-    Session session=request.getSession(true);
-    if(session == null){
-      getLogger().error("No session object");
-      return false;
-      }
-
-    // If there are more than one authenticator enabled, then check corresponding id
-    String authenticator_id=(String)session.getAttribute("org.wyona.cms.cocoon.acting.IMLAuthenticator.id");
-    if(this.authenticator_id != authenticator_id){
-      getLogger().error("Bad authenticator: "+authenticator_id+" (Authorizer's authenticator: "+this.authenticator_id+")");
-      return false;
-      }
-
-    Identity identity=(Identity)session.getAttribute("org.wyona.cms.ac.Identity");
-    if(identity != null){
-      if(policy.authorizeUser(action,identity.getUsername())){
-        return true;
+        Configuration authenticatorConf=conf.getChild("authenticator");
+        authenticator_type=authenticatorConf.getAttribute("type");
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("CONFIGURATION: authenticator type="+authenticator_type);
         }
-
-      String[] groupname=identity.getGroupnames();
-      for(int i=0;i<groupname.length;i++){
-        if(policy.authorizeGroup(action,groupname[i])){
-          return true;
+        /*
+          if(authenticator_type == null){
+          throw new ConfigurationException("No authenticator type set");
           }
-        }
-      }
+        */
 
-    getLogger().error("Permission denied");
-    return false;
+        Configuration domainConf=conf.getChild("domain");
+        domain=domainConf.getValue("127.0.0.1");
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("CONFIGURATION: domain="+domain);
+        }
+
+        Configuration portConf=conf.getChild("port");
+        port=portConf.getValue(null);
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("CONFIGURATION: port="+port);
+        }
+
+        Configuration contextConf=conf.getChild("context");
+        context=contextConf.getValue(null);
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("CONFIGURATION: context="+context);
+        }
+
+        Configuration policiesConf=conf.getChild("policies");
+        policies=policiesConf.getValue(null);
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("CONFIGURATION: policies="+policies);   
+        }
     }
-/**
- *
- */
-  private Document getPolicyDoc(Request request) throws Exception{
-    String context=request.getContextPath();
-    int port=request.getServerPort();
-    String sitemap_uri=request.getSitemapURI();
-    String pmlURLString="http://"+domain;
-    if(this.port != null){
-      pmlURLString=pmlURLString+":"+this.port;
-      }
-    else{
-      pmlURLString=pmlURLString+":"+port;
-      }
-    if(this.context != null){
-      pmlURLString=pmlURLString+this.context;
-      }
-    else{
-      pmlURLString=pmlURLString+context;
-      }
-    pmlURLString=pmlURLString+"/"+policies+sitemap_uri+".acml";
-    //pmlURLString=pmlURLString+"/"+policies+sitemap_uri+".pml";
-    getLogger().error(".getPolicyDoc(): "+pmlURLString);
-    DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
-    DocumentBuilder db=dbf.newDocumentBuilder();
-    return db.parse(new URL(pmlURLString).openStream());
+    /**
+     *
+     */
+    public boolean authorize(Request request,Map map) throws Exception{
+        String remoteAddress=request.getRemoteAddr();
+
+        // Permit Identity and Policy requests for localhost
+        String sitemap_uri=request.getRequestURI();
+        getLogger().error("POLICIES: "+sitemap_uri+" "+policies);
+        if(remoteAddress.equals("127.0.0.1") && (sitemap_uri.indexOf(policies) >= 0)){
+            //if(remoteAddress.equals("127.0.0.1")){
+            return true;
+        }
+    
+        // Get policy
+        Document policyDoc=null;
+        try{
+            policyDoc=getPolicyDoc(request);
+        }
+        catch(Exception e){
+            getLogger().error(".authorize(): "+e);
+            return false;
+        }
+        Policy policy=new Policy(policyDoc,getLogger());
+
+        // Read action (read, write, publish, etc.)
+        String action=XPathAPI.selectSingleNode(policyDoc,"/ac/request/action/@name").getNodeValue(); //"read";
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug("action: "+action);
+        }
+
+        // Check permissions
+        if(policy.authorizeWorld(action)){
+            return true;
+        }
+
+        if(policy.authorizeMachine(action,remoteAddress)){
+            return true;
+        }
+
+        Session session=request.getSession(true);
+        if(session == null){
+            getLogger().error("No session object");
+            return false;
+        }
+
+        // If there are more than one authenticator enabled, then check corresponding type
+        String authenticator_type=(String)session.getAttribute("org.wyona.cms.cocoon.acting.IMLAuthenticator.type");
+        if(this.authenticator_type != authenticator_type){
+            getLogger().error("Bad authenticator: "+authenticator_type+" (Authorizer's authenticator: "+this.authenticator_type+")");
+            return false;
+        }
+
+        Identity identity=(Identity)session.getAttribute("org.wyona.cms.ac.Identity");
+        if(identity != null){
+            if(policy.authorizeUser(action,identity.getUsername())){
+                return true;
+            }
+
+            String[] groupname=identity.getGroupnames();
+            for(int i=0;i<groupname.length;i++){
+                if(policy.authorizeGroup(action,groupname[i])){
+                    return true;
+                }
+            }
+        }
+
+        getLogger().error("Permission denied");
+        return false;
     }
-/**
- *
- */
-/*
-  private void parse() throws Exception{
-    javax.xml.parsers.SAXParserFactory spf=javax.xml.parsers.SAXParserFactory.newInstance();
-    javax.xml.parsers.SAXParser sp=spf.newSAXParser();
+    /**
+     *
+     */
+    private Document getPolicyDoc(Request request) throws Exception{
+        String context=request.getContextPath();
+        int port=request.getServerPort();
+        String sitemap_uri=request.getSitemapURI();
+        String pmlURLString="http://"+domain;
+        if(this.port != null){
+            pmlURLString=pmlURLString+":"+this.port;
+        }
+        else{
+            pmlURLString=pmlURLString+":"+port;
+        }
+        if(this.context != null){
+            pmlURLString=pmlURLString+this.context;
+        }
+        else{
+            pmlURLString=pmlURLString+context;
+        }
+        pmlURLString=pmlURLString+"/"+policies+sitemap_uri+".acml";
+        //pmlURLString=pmlURLString+"/"+policies+sitemap_uri+".pml";
+        getLogger().error(".getPolicyDoc(): "+pmlURLString);
+        DocumentBuilderFactory dbf=DocumentBuilderFactory.newInstance();
+        DocumentBuilder db=dbf.newDocumentBuilder();
+        return db.parse(new URL(pmlURLString).openStream());
     }
-*/
-  }
+    /**
+     *
+     */
+    /*
+      private void parse() throws Exception{
+      javax.xml.parsers.SAXParserFactory spf=javax.xml.parsers.SAXParserFactory.newInstance();
+      javax.xml.parsers.SAXParser sp=spf.newSAXParser();
+      }
+    */
+}
