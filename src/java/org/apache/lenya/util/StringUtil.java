901a29c4a103af79609eb5b316028bb55fe83a19
tinuwyonacms <dev-null@apache.org>
2002-01-24 18:40:57 +0000

    string replacement utility funtion
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@37857 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/util/StringUtil.java
@@ -0,0 +1,67 @@
+package org.wyona.util;
+
+/**
+ * StringUtil.java
+ * $Id: StringUtil.java,v 1.1 2002/01/24 18:40:57 tinuwyonacms Exp $
+ *
+ * Created: Thu Jan 24 18:27:05 2002
+ *
+ * @author <a href="mailto:tinu@email.ch">Martin Lüthi</a>
+ * @version
+ *
+ * Utilty Class for String handling
+ */
+
+public class StringUtil {
+
+  /**
+   * @author beh
+   *
+   * http://jguru.com/forums/view.jsp?EID=539354
+   *
+   * Replaces one substring with another within a main string.
+   *
+   * @param       s       Main string.
+   * @param       find    The substring to find.
+   * @param       rep     Replaces the find substring.
+   * @return      The new string. 
+   */
+  public static String replace(String s, String find, String rep) {
+    return replace(new StringBuffer(s), find, rep).toString();                                   
+  }
+  
+  /**
+   * Replaces one substring with another within a main string.
+   *
+   * @param       sb      Main string. (StringBuffer)
+   * @param       find    The substring to find.
+   * @param       rep     Replaces the find substring.
+   * @return      The new string. (StringBuffer)
+   */
+  public static StringBuffer replace(StringBuffer sb, String find, String rep) {
+    StringBuffer buf = new StringBuffer(sb.toString());
+    int startIndex, endIndex;
+    boolean done = false;
+    
+    startIndex = endIndex = 0;
+    // Halts if they is not need to do any replacements
+    if (!find.equals(rep)) {
+      String s = buf.toString();
+      // Continues while more substring(s) (find) exist
+      while (!done) {
+        // Grab the position of the substring (find)
+        if ((startIndex = s.indexOf(find)) >= 0) {
+          // Replace "find" with "rep"
+          endIndex = startIndex + find.length();
+          buf.delete(startIndex, endIndex);
+          buf.insert(startIndex, rep);
+          s = buf.toString();
+        } else {
+          done = true;
+        }       
+      }
+    }
+    return buf;                                     
+  }
+  
+}// StringUtil
