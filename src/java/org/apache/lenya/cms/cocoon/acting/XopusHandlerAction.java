4aee4265402875fbc716f0e6fb17eb0255f8b3b5
Mehmet Birgi <memo@apache.org>
2002-02-26 18:12:35 +0000

    xopus: getting closer, but still not there
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@37891 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/cocoon/acting/XopusHandlerAction.java
+++ src/java/org/apache/lenya/cms/cocoon/acting/XopusHandlerAction.java
@@ -1,42 +1,24 @@
 package org.wyona.cms.cocoon.acting;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.InputStream;
+import java.lang.String;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
-
+// import javax.servlet.ServletInputStream;
 import org.apache.avalon.framework.parameters.Parameters;
-
 import org.apache.cocoon.acting.ComposerAction;
 import org.apache.cocoon.Constants;
+import org.apache.cocoon.environment.http.HttpEnvironment;
+import org.apache.cocoon.environment.http.HttpRequest;
+import org.apache.cocoon.environment.Request;
 import org.apache.cocoon.environment.Redirector;
 import org.apache.cocoon.environment.SourceResolver;
-
-// import java.io.BufferedReader;
-// import java.io.BufferedWriter;
-// import java.io.File;
-// import java.io.FileWriter;
-// import java.io.InputStreamReader;
-// import java.net.URL;
-// import java.util.Enumeration;
-// import java.util.HashMap;
-// import java.util.Map;
-// import java.util.StringTokenizer;
-// import javax.xml.parsers.DocumentBuilder;
-// import javax.xml.parsers.DocumentBuilderFactory;
-// import org.apache.avalon.framework.configuration.Configurable;
-// import org.apache.avalon.framework.configuration.Configuration;
-// import org.apache.avalon.framework.configuration.ConfigurationException;
-// import org.apache.avalon.framework.parameters.Parameters;
-// import org.apache.avalon.framework.thread.ThreadSafe;
-// import org.apache.cocoon.Constants;
-// import org.apache.cocoon.acting.AbstractComplementaryConfigurableAction;
-// import org.apache.cocoon.acting.ValidatorActionHelper;
-// import org.apache.cocoon.environment.Context;
-// import org.apache.cocoon.environment.Request;
-// import org.apache.cocoon.environment.Session;
-// import org.apache.cocoon.environment.Source;
-// import org.apache.cocoon.util.Tokenizer;
-// import org.xml.sax.EntityResolver;
-// import org.w3c.dom.Document;
+import org.apache.cocoon.util.PostInputStream;
 
 /**
  * Interfaces with Xopus: handles the requests and replies to them
@@ -46,11 +28,69 @@ import org.apache.cocoon.environment.SourceResolver;
  * @version 0.1
  */
 public class XopusHandlerAction extends ComposerAction {
+
   public java.util.Map act (Redirector redirector, 
-                  SourceResolver resolver, 
-                  Map objectModel, 
-                  String source, 
-                  Parameters params) {
+                            SourceResolver resolver, 
+                            Map objectModel, 
+                            String source, 
+                            Parameters params)
+                            throws IOException {
+
+//     File homedir = new File(System.getProperty("user.home"));
+    File workfile = new File("c:/home/work/playground/wyona-cms/xopus", "xopus-temp.txt");
+//     File workfile = new File(homedir, "xopus-temp.txt");
+    getLogger().debug("workfile="+workfile.getAbsoluteFile());
+
+    // Get request object
+    Request request=(Request)objectModel.get(Constants.REQUEST_OBJECT);
+    if(request == null){
+      getLogger().error ("No request object");
+      return null;
+    }
+    else {
+      getLogger().debug ("SitemapURI = " + request.getSitemapURI());
+    }
+    
+    // Get session
+//     Session session=request.getSession(true);
+//     if(session == null){
+//       getLogger().error("No session object");
+//       return null;
+//     }
+
+    boolean appendFlag = true;
+    PrintWriter out = new PrintWriter(new FileWriter(workfile.toString(), appendFlag));
+    
+    HttpRequest httpReq = (HttpRequest) objectModel.get(HttpEnvironment.HTTP_REQUEST_OBJECT);
+    InputStream input = httpReq.getInputStream();
+    int length = request.getContentLength();
+    PostInputStream reqContent = new PostInputStream(input, length);
+        
+//     Enumeration reqParams = request.getParameterNames();
+//     String currParamName = null;
+//     while (reqParams.hasMoreElements()) {
+//       currParamName = (String)reqParams.nextElement();
+//       out.println("Request Parameter: " + currParamName + " = " + request.getParameter(currParamName));
+//       getLogger().debug("Request Parameter: " + currParamName + " = " + request.getParameter(currParamName));
+//     }
+//     Enumeration reqAttribs = request.getAttributeNames();
+//     String currAttribName = null;
+//     while (reqAttribs.hasMoreElements()) {
+//       currAttribName = (String)reqAttribs.nextElement();
+//       out.println("Request Attribute: " + currAttribName + " = " + request.getAttribute(currAttribName));
+//       getLogger().debug("Request Attribute: " + currAttribName + " = " + request.getAttribute(currAttribName));
+//     }
+//     Enumeration reqHeaders = request.getHeaderNames();
+//     String currHeaderName = null;
+//     while (reqHeaders.hasMoreElements()) {
+//       currHeaderName = (String)reqHeaders.nextElement();
+//       out.println("Request Header: " + currHeaderName + " = " + request.getHeader(currHeaderName));
+//       getLogger().debug("Request Header: " + currHeaderName + " = " + request.getHeader(currHeaderName));
+//     }
+
+    out.close();
+    
+    
     Map sitemapParams = new HashMap();
     sitemapParams.put("world", "hello");
 
