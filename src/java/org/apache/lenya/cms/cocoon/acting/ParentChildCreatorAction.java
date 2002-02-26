7ad8e38286a3a0ff251b1c9cce9ff5b4b169baad
michicms <dev-null@apache.org>
2002-02-26 00:33:50 +0000

    Parent child nearly finished
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@37890 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/cocoon/acting/ParentChildCreatorAction.java
+++ src/java/org/apache/lenya/cms/cocoon/acting/ParentChildCreatorAction.java
@@ -21,37 +21,45 @@ import java.io.OutputStream;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.List;
 
 import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
+import org.dom4j.XPath;
 
 /**
  * Describe class <code>ParentChildCreatorAction</code> here.
  *
  * @author Michael Wechner
- * @version 2002.01.30
+ * @version 2002.02.26
  * @created 2002.01.30
  */
 public class ParentChildCreatorAction extends AbstractComplementaryConfigurableAction implements Configurable{
+  private String treeAuthoringPath=null;
+  private String docsPath=null;
+  private String doctypesPath=null;
 /**
  *
  */
   public void configure(Configuration conf) throws ConfigurationException{
     super.configure(conf);
+
+    treeAuthoringPath=conf.getChild("tree-authoring").getAttribute("href");
+    docsPath=conf.getChild("docs").getAttribute("href");
+    doctypesPath=conf.getChild("doctypes").getAttribute("href");
+    getLogger().debug("CONFIGURATION:\nAUTHORING PATH OF TREE="+treeAuthoringPath);
     }
 /**
  *
  */
   public Map act(Redirector redirector,SourceResolver resolver,Map objectModel,String src,Parameters parameters) throws Exception {
-    // Get Source
-    getLogger().error ("SOURCE: "+src);
-    if(src == null){
-      getLogger().error ("No source: e.g. src=\"docs/wyona/cms/tree.xml\"");
-      return null;
-    }
-    org.apache.cocoon.environment.Source inputSource=resolver.resolve(src);
-    getLogger().error("RESOLVED SOURCE: "+inputSource.getSystemId());
+    org.apache.cocoon.environment.Source input_source=resolver.resolve("");
+    String sitemapParentPath=input_source.getSystemId();
+    sitemapParentPath=sitemapParentPath.substring(5); // Remove "file:" protocoll
+    getLogger().debug("PARENT PATH OF SITEMAP: "+sitemapParentPath);
 
     // Get request object
     Request request=(Request)objectModel.get(Constants.REQUEST_OBJECT);
@@ -64,7 +72,7 @@ public class ParentChildCreatorAction extends AbstractComplementaryConfigurableA
     String parentid=request.getParameter("parentid");
     String childid=request.getParameter("childid");
     String childname=request.getParameter("childname");
-    String childtype=request.getParameter("childtype");
+    String childtype=request.getParameter("childtype"); // branch or leaf
     String doctype=request.getParameter("doctype");
     if(!validate(parentid,childid,childname,childtype,doctype)){
       return null;
@@ -77,23 +85,62 @@ public class ParentChildCreatorAction extends AbstractComplementaryConfigurableA
       return null;
     }
 
+    // Transaction should actually be started here!
+
     // Read tree
-    String treefilename=inputSource.getSystemId().substring(5); // 5: remove protocol "file:"
-    getLogger().error("Tree filename: "+treefilename);
+    String treefilename=sitemapParentPath+treeAuthoringPath;
+    getLogger().debug("FILENAME OF TREE: "+treefilename);
     if(!new File(treefilename).exists()){
       return null;
     }
     //InputStream in=intercept(new FileInputStream(treefilename));
     Document doc=new SAXReader().read("file:"+treefilename);
+
+/*
+    // Get trunk
     Element trunk=(Element)doc.selectSingleNode("/tree/branch");
-    trunk.addElement(childtype).addAttribute("relURI",childid).addAttribute("doctype",doctype).addAttribute("menuName",childname);
     getLogger().error("TRUNK: "+trunk.getName()+" "+trunk.getPath());
-    getLogger().error("Modified Tree: "+doc.asXML());
+*/
+
+    // Get parent element
+    StringTokenizer st=new StringTokenizer(parentid,"/");
+    String xpath_string="/tree/branch"; // Trunk of tree
+    while(st.hasMoreTokens()){
+      xpath_string=xpath_string+"/branch[@relURI='"+st.nextToken()+"']";
+      }
+    getLogger().debug("XPATH: "+xpath_string);
+    XPath xpathSelector=DocumentHelper.createXPath(xpath_string);
+    List nodes=xpathSelector.selectNodes(doc);
+    if(nodes.isEmpty()){
+      getLogger().error("No child added");
+      return null;
+      }
+    Element parent_element=(Element)nodes.get(0);
+    getLogger().debug("PARENT ELEMENT: "+parent_element.getPath());
+
+    // Add node: branch or leaf
+    parent_element.addElement(childtype).addAttribute("relURI",childid).addAttribute("doctype",doctype).addAttribute("menuName",childname);
+    getLogger().debug("Tree has been modified: "+doc.asXML());
+
+    // Write new tree
     java.io.FileWriter fileWriter = new java.io.FileWriter(treefilename);
-    //java.io.FileWriter fileWriter = new java.io.FileWriter(treefilename+".tmp");
     doc.write(fileWriter);
     fileWriter.close();
 
+    // Transaction should actually be finished here!
+
+    // Create actual document
+    String filename=sitemapParentPath+docsPath+parentid+"/"+childid+"/index.xml"; // Filename Generator
+    String filenameMeta=sitemapParentPath+docsPath+parentid+"/"+childid+"/index-meta.xml"; // Filename Generator
+    String absoluteDoctypesPath=sitemapParentPath+doctypesPath;
+    String doctypeSample=absoluteDoctypesPath+"Group.xml";
+    String doctypeMeta=absoluteDoctypesPath+"Meta.xml";
+    getLogger().error("Filename: "+filename);
+    getLogger().error("Doctypes: "+absoluteDoctypesPath);
+    copyFile(new File(doctypeSample),new File(filename));
+    copyFile(new File(doctypeMeta),new File(filenameMeta));
+
+    // Redirect to referer
     if(7 > 1){
       String parent_uri=(String)session.getAttribute("org.wyona.cms.cocoon.acting.ParentChildCreatorAction.parent_uri");
       getLogger().error("Child added");
@@ -104,12 +151,12 @@ public class ParentChildCreatorAction extends AbstractComplementaryConfigurableA
       }
     getLogger().error("No child added");
     return null;
-  }
+    }
 /**
  *
  */
   public boolean validate(String parentid,String childid,String childname,String childtype,String doctype){
-    getLogger().error(".validate(): parentid="+parentid+" ; childid="+childid+" ; childname="+childname+" ; childtype="+childtype+" ; doctype="+doctype);
+    getLogger().debug(".validate(): parentid="+parentid+" ; childid="+childid+" ; childname="+childname+" ; childtype="+childtype+" ; doctype="+doctype);
     if((childid.indexOf(" ") >= 0) || (childid.length() == 0)){
       return false;
     }
@@ -117,14 +164,14 @@ public class ParentChildCreatorAction extends AbstractComplementaryConfigurableA
       return false;
     }
     return true;
-  }
-  /**
-   * Write input stream to Logger (for debugging)
-   *
-   * @param in an <code>InputStream</code> value
-   * @return an <code>InputStream</code> value
-   * @exception Exception if an error occurs
-   */
+    }
+/**
+ * Write input stream to Logger (for debugging)
+ *
+ * @param in an <code>InputStream</code> value
+ * @return an <code>InputStream</code> value
+ * @exception Exception if an error occurs
+ */
   private InputStream intercept(InputStream in) throws Exception{
     byte[] buffer=new byte[1024];
     int bytes_read;
@@ -132,17 +179,35 @@ public class ParentChildCreatorAction extends AbstractComplementaryConfigurableA
     while((bytes_read=in.read(buffer)) != -1){
       bufferOut.write(buffer,0,bytes_read);
     }
-    getLogger().error("Intercepted Input Stream:\n\n"+bufferOut.toString());
+    getLogger().debug("Intercepted Input Stream:\n\n"+bufferOut.toString());
     return new ByteArrayInputStream(bufferOut.toByteArray());
-  }
-  /**
-   * Write output stream to Logger (for debugging)
-   *
-   * @param out an <code>OutputStream</code> value
-   * @return an <code>OutputStream</code> value
-   * @exception Exception if an error occurs
-   */
+    }
+/**
+ * Write output stream to Logger (for debugging)
+ *
+ * @param out an <code>OutputStream</code> value
+ * @return an <code>OutputStream</code> value
+  * @exception Exception if an error occurs
+ */
   private OutputStream intercept(OutputStream out) throws Exception{
     return null;
-  }
+    }
+/**
+ *
+ */
+  private boolean copyFile(File source,File destination) throws Exception{
+    if(source.exists()){
+      File parentDestination=new File(destination.getParent());
+      if(!parentDestination.exists()){
+       getLogger().warn("Directory will be created: "+parentDestination);
+        parentDestination.mkdirs();
+        }
+      org.apache.avalon.excalibur.io.FileUtil.copyFile(source,destination);
+      return true;
+      }
+    else{
+      getLogger().error("File does not exist: "+source);
+      return false;
+      }
+    }
   }
