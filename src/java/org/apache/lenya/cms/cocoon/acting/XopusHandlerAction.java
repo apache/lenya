23eda3a26b27234fdf9b70836d23c8a28fea9e93
Mehmet Birgi <memo@apache.org>
2002-03-01 09:51:38 +0000

    xopus stuff, not working!
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@37902 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/cocoon/acting/XopusHandlerAction.java
+++ src/java/org/apache/lenya/cms/cocoon/acting/XopusHandlerAction.java
@@ -1,16 +1,10 @@
 package org.wyona.cms.cocoon.acting;
 
-// import java.io.File;
-// import java.io.FileWriter;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.InputStream;
 import java.io.IOException;
-// import java.io.PrintWriter;
 import java.lang.String;
-// import java.text.DateFormat;
-// import java.util.Date; 
-// import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.avalon.excalibur.io.IOUtil;
@@ -18,18 +12,13 @@ import org.apache.avalon.framework.component.ComponentException;
 import org.apache.avalon.framework.parameters.Parameters;
 import org.apache.cocoon.acting.ComposerAction;
 import org.apache.cocoon.components.parser.Parser;
-// import org.apache.cocoon.Constants;
-// import org.apache.cocoon.environment.http.HttpEnvironment;
 import org.apache.cocoon.environment.http.HttpRequest;
 import org.apache.cocoon.environment.ObjectModelHelper;
 import org.apache.cocoon.environment.Redirector;
-// import org.apache.cocoon.environment.Request;
 import org.apache.cocoon.environment.SourceResolver;
 import org.apache.cocoon.util.PostInputStream;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-// import org.w3c.dom.NamedNodeMap;
-// import org.w3c.dom.Node;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
@@ -74,15 +63,19 @@ public class XopusHandlerAction extends ComposerAction {
     
     // get the root element (should be "request") and its attributes ---> FixMe: Add error handling
     Element root = requestDoc.getDocumentElement();
-    getLogger().debug ("root element (should be 'request'):" + root.getTagName());
+    getLogger().debug ("root element (should be 'request'): " + root.getTagName());
     String reqId = root.getAttribute("id");
+    getLogger().debug ("Request ID: " + reqId);
     String reqType = root.getAttribute("type");
+    getLogger().debug ("Request Type: " + reqType);
     
     // get the first child element for root element (should be "data") and its attributes ---> FixMe: Add error handling
     Element data = (Element) root.getFirstChild();
-    getLogger().debug ("first child element (should be 'data'):" + data.getTagName());
+    getLogger().debug ("first child element (should be 'data'): " + data.getTagName());
     String reqFile = data.getAttribute("id");
+    getLogger().debug ("Requested File: " + reqFile);
     String fileType = data.getAttribute("type");
+    getLogger().debug ("Requested File's Type: " + fileType);
     
     // close the input stream
     reqContent.close();
@@ -97,23 +90,3 @@ public class XopusHandlerAction extends ComposerAction {
     
   }
 }
-    // Set up the custom output file for development dumps
-//     File workfile = new File("c:/home/work/playground/wyona-cms/xopus", "xopus-temp.txt");
-//     boolean appendFlag = true;
-//     PrintWriter out = new PrintWriter(new FileWriter(workfile.toString(), appendFlag));
-//     getLogger().debug("workfile="+workfile.getAbsoluteFile());
-//     DateFormat timeStamp = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.FULL);
-//     out.println("<===== START ===== " + timeStamp.format(new Date()) + " ==========>");
-
-    // output request content to workfile for debugging
-//     out.println("Contents of the Xopus request:");
-//     out.print(request);
-    
-//     out.println("INFO: Root Element Name: " + rootTag);
-
-    // Close the custom output file
-//     out.println("<===== END ======= " + timeStamp.format(new Date()) + " ==========>");
-//     out.println();
-//     out.close();
-    
-
