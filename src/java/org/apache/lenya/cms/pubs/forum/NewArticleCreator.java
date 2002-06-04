a1aafcd418e268128d3f7bc59338534ade04f4c1
michicms <dev-null@apache.org>
2002-06-04 08:38:34 +0000

    Bug fixed within NewArticleCreator
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@38064 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/pubs/forum/NewArticleCreator.java
+++ src/java/org/apache/lenya/cms/pubs/forum/NewArticleCreator.java
@@ -3,14 +3,22 @@ package org.wyona.cms.pubs.forum;
 import org.wyona.cms.authoring.AbstractParentChildCreator;
 
 import java.io.File;
+import java.io.FileWriter;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import org.apache.log4j.Category;
 
 /**
  * @author Michael Wechner
- * @version 2002.6.3
+ * @version 2002.6.4
  */
 public class NewArticleCreator extends AbstractParentChildCreator{
+  static Category log=Category.getInstance(NewArticleCreator.class);
 /**
- * Why?
+ * Why do we have to overwrite this method?
  */
   public short getChildType(short childType) throws Exception{
     return AbstractParentChildCreator.BRANCH_NODE;
@@ -20,14 +28,36 @@ public class NewArticleCreator extends AbstractParentChildCreator{
  */
   public String generateTreeId(String childId,short childType) throws Exception{
     long id=System.currentTimeMillis();
-    return "articles/"+id+"/index.xml";
+    return ""+id;
+    //return "articles/"+id+"/index.xml";
     }
 /**
  *
  */
   public void create(File samplesDir,File parentDir,String childId,short childType) throws Exception{
-    String filename=parentDir+"/"+generateTreeId(childId,childType);
+    // Set filenames
+    String id=generateTreeId(childId,childType);
+    String filename=parentDir+"/articles/"+id+"/index.xml";
     String doctypeSample=samplesDir+"/Article.xml";
-    copyFile(new File(doctypeSample),new File(filename));
+
+    // Read sample file
+    Document doc=new SAXReader().read("file:"+doctypeSample);
+
+    // Replace id
+    Element eid=(Element)doc.selectSingleNode("/article/meta/id");
+    log.error(eid.getPath()+" "+eid.getText());
+    //eid.remove(org.dom4j.DocumentHelper.createText("@ID@"));
+    eid.addText(id);
+    log.error(eid.getPath()+" "+eid.getText());
+
+    // Write file
+    File parent=new File(new File(filename).getParent());
+    if(!parent.exists()){
+      parent.mkdirs();
+      }
+    FileWriter fileWriter=new FileWriter(filename);
+    doc.write(fileWriter);
+    fileWriter.close();
+    //copyFile(new File(doctypeSample),new File(filename));
     }
   }
