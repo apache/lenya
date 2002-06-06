5d39036bf1a891a3143405c6198753ed44bc5e86
michicms <dev-null@apache.org>
2002-06-06 23:59:59 +0000

    Replication Queue started to implement
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@38068 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/cocoon/acting/PublisherAction.java
+++ src/java/org/apache/lenya/cms/cocoon/acting/PublisherAction.java
@@ -37,6 +37,8 @@ public class PublisherAction extends AbstractComplementaryConfigurableAction imp
   private String livePath=null;
   private String treeAuthoringPath=null;
   private String treeLivePath=null;
+
+  private String replication_queue_href=null;
 /**
  *
  */
@@ -47,9 +49,13 @@ public class PublisherAction extends AbstractComplementaryConfigurableAction imp
     livePath=conf.getChild("live").getAttribute("href");
     treeAuthoringPath=conf.getChild("tree-authoring").getAttribute("href");
     treeLivePath=conf.getChild("tree-live").getAttribute("href");
+
+    replication_queue_href=conf.getChild("replication-queue").getAttribute("href");
+
     if(getLogger().isDebugEnabled()) {
       getLogger().debug("CONFIGURATION:\nauthoring path="+authoringPath+"\nlive path="+livePath);
       getLogger().debug("CONFIGURATION:\ntree authoring path="+treeAuthoringPath+"\ntree live path="+treeLivePath);
+      getLogger().debug("CONFIGURATION:\nReplication Queue: href="+replication_queue_href);
       }
     }
 /**
