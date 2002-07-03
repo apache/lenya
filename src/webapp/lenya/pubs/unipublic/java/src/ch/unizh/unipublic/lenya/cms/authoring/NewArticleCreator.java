a3f8da1889dc19e95bbcc597207ca86e7d25f5e8
edithcms <dev-null@apache.org>
2002-07-03 15:00:58 +0000

    Creation of a new Article
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@38121 13f79535-47bb-0310-9956-ffa450edef68

--- src/webapp/lenya/pubs/unipublic/java/src/ch/unizh/unipublic/lenya/cms/authoring/NewArticleCreator.java
+++ src/webapp/lenya/pubs/unipublic/java/src/ch/unizh/unipublic/lenya/cms/authoring/NewArticleCreator.java
@@ -1,14 +1,19 @@
 package ch.unizh.unipublic.wyona.cms.authoring;
 
 import java.io.File;
+import java.io.FileWriter;
 
 import org.wyona.cms.authoring.AbstractParentChildCreator;
+
+import org.dom4j.Document;
+import org.dom4j.io.SAXReader;
 
 import org.apache.log4j.Category;
 
 /**
  * @author Michael Wechner
- * @version 2002.7.2
+ * @author Edith Chevrier
+ * @version 2002.7.4
  */
 public class NewArticleCreator extends AbstractParentChildCreator{
   static Category log=Category.getInstance(NewArticleCreator.class);
@@ -16,7 +21,8 @@ public class NewArticleCreator extends AbstractParentChildCreator{
  *
  */
   public short getChildType(short childType) throws Exception{
-    return childType;
+    return AbstractParentChildCreator.BRANCH_NODE;
+//    return childType;
     }
 /**
  *
@@ -28,6 +34,15 @@ public class NewArticleCreator extends AbstractParentChildCreator{
  *
  */
   public void create(File samplesDir,File parentDir,String childId,short childType) throws Exception{
-    log.warn("NewArticleCreator.create() has been called.");
+    log.debug("NewArticleCreator.create() has been called.");
+
+    //Set filenames
+    String filename=parentDir+"/"+childId+"/"+"index.xml"; 
+    log.debug("filename : "+filename);
+    String doctypeSample=samplesDir+"/Article.xml";                                                                                         
+    log.debug("doctypeSample : "+doctypeSample);
+
+    copyFile(new File(doctypeSample),new File(filename)); 
+
     }
   }
