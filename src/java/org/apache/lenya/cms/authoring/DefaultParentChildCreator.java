74410298f182b065b02bca46f0adfd922aa8a76b
egliwyona <dev-null@apache.org>
2002-07-23 09:55:08 +0000

    Changed the indenting to Apache style. Added comments to all methods.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@38161 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/authoring/DefaultParentChildCreator.java
+++ src/java/org/apache/lenya/cms/authoring/DefaultParentChildCreator.java
@@ -7,24 +7,46 @@ import org.apache.log4j.Category;
 /**
  * @author Michael Wechner
  */
-public class DefaultParentChildCreator extends AbstractParentChildCreator{
-  static Category log=Category.getInstance(DefaultParentChildCreator.class);
-/**
- *
- */
-  public short getChildType(short childType) throws Exception{
-    return childType;
+public class DefaultParentChildCreator extends AbstractParentChildCreator {
+    static Category log = Category.getInstance(DefaultParentChildCreator.class);
+
+    /**
+     * Return the child type.
+     *
+     * @param childType a <code>short</code> value
+     * @return a <code>short</code> value
+     * @exception Exception if an error occurs
+     */
+    public short getChildType(short childType) throws Exception {
+	return childType;
     }
-/**
- *
- */
-  public String generateTreeId(String childId,short childType) throws Exception{
-    return childId;
+
+    /**
+     * Generate a three id.
+     *
+     * @param childId a <code>String</code> value
+     * @param childType a <code>short</code> value
+     * @return a <code>String</code> value
+     * @exception Exception if an error occurs
+     */
+    public String generateTreeId(String childId, short childType)
+	throws Exception {
+	return childId;
     }
-/**
- *
- */
-  public void create(File samplesDir,File parentDir,String childId,short childType) throws Exception{
-    log.warn("DefaultParentChildCreator.create() has been called.");
+
+    /**
+     * Default implementation for creation.
+     *
+     * @param samplesDir a <code>File</code> value
+     * @param parentDir a <code>File</code> value
+     * @param childId a <code>String</code> value
+     * @param childType a <code>short</code> value
+     * @exception Exception if an error occurs
+     */
+    public void create(File samplesDir, File parentDir,
+		       String childId, short childType)
+	throws Exception {
+
+	log.warn("DefaultParentChildCreator.create() has been called.");
     }
-  }
+}
