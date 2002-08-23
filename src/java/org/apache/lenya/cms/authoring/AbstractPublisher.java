55713b4e96694e93bd011f3373a64e99cd4fe133
michicms <dev-null@apache.org>
2002-08-23 06:51:43 +0000

    Extracted Publisher added
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@38213 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/authoring/AbstractPublisher.java
@@ -0,0 +1,20 @@
+package org.wyona.cms.authoring;
+
+import java.io.File;
+
+/**
+ * Describe class <code>AbstractPublisher</code> here.
+ *
+ * @author <a href="mailto:christian.egli@wyona.com">Christian Egli</a>
+ */
+public abstract class AbstractPublisher {
+
+    /**
+     * Describe <code>publish</code> method here.
+     *
+     * @exception Exception if an error occurs
+     */
+    public abstract void publish() throws Exception;
+    
+}
+
