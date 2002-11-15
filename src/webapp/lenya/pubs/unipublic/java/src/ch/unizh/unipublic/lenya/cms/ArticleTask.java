1d4e224ad211f52c1921e1a22304512b32a5f473
ah <dev-null@apache.org>
2002-11-15 10:50:50 +0000

    Added Files:  	src/java/ch/unizh/unipublic/wyona/cms/ArticleTask.java  	src/java/ch/unizh/unipublic/wyona/cms/DossierAction.java  Removed Files:  	src/java/ch/unizh/unipublic/wyona/cms/ArticlePublisherAction.java
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@38436 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/webapp/lenya/pubs/unipublic/java/src/ch/unizh/unipublic/lenya/cms/ArticleTask.java
@@ -0,0 +1,287 @@
+/*
+ * ArticlePublisher.java
+ *
+ * Created on November 4, 2002, 11:25 AM
+ */
+
+package ch.unizh.unipublic.wyona.cms;
+
+import org.apache.avalon.framework.configuration.Configurable;
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.Constants;
+import org.apache.cocoon.acting.AbstractComplementaryConfigurableAction;
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.Session;
+import org.apache.cocoon.environment.SourceResolver;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedOutputStream; 
+import java.io.FileOutputStream;
+
+import java.net.URL;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Date;
+
+import org.dom4j.DocumentHelper;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+import org.dom4j.io.OutputFormat;
+
+import org.wyona.xml.DOM4JUtil;
+import org.wyona.util.DateUtil;
+import org.apache.log4j.Category;
+import org.wyona.cms.publishing.DefaultFilePublisher;
+import org.wyona.cms.publishing.PublishingEnvironment;
+import org.wyona.cms.task.AbstractTask;
+
+
+/**
+ *
+ * @author  ah
+ */
+public class ArticleTask
+    extends AbstractTask {
+    
+    static Category log=Category.getInstance(ArticleTask.class);
+
+    public void prepareArticle(
+        String publicationPath,
+        String authoringPath,
+        String[] sources) {
+
+	String absoluteAuthoringPath = publicationPath + authoringPath;
+
+        for (int i = 0; i < sources.length; i++) {
+            File sourceFile = new File(absoluteAuthoringPath + sources[i]);
+            try {
+                setRevisionDateAndId(absoluteAuthoringPath + sources[i]); 
+                addToHeadlines(sources[i], absoluteAuthoringPath);
+                boolean published = setFirstPublishedDate(absoluteAuthoringPath + sources[i]);
+                log.info("Article prepared: " + sourceFile);
+            }
+            catch (Exception e) {
+                log.error("EXCEPTION: Article not prepared: "
+                    + sourceFile + " " + e);
+            }
+        }
+                
+    }
+    
+    /** insert the article in the frontpage
+     * @param docId id to determine the article
+     * @param domainPath path for the different domain (authoring or live)
+     */
+    private void addToHeadlines(String docId, String domainPath) throws Exception{
+
+        //article's  channel, section, year, dir. FIXME: should be readen from the article, but now:
+        StringTokenizer st=new StringTokenizer(docId,"/");
+        String channel=st.nextToken();
+        String section=st.nextToken();
+        String articles=st.nextToken();
+        String year=st.nextToken();
+        String dir=st.nextToken();
+
+        //headlines in domain 
+        // FIXME    headlinesPath=domainPath+conf.getChild("headlines").getAttribute("href");
+        String headlinesFilename=domainPath+"frontpage/headlines.xml";
+        log.debug("headlines filename "+headlinesFilename);
+        Document doc=new SAXReader().read("file:"+headlinesFilename);
+
+        //insert the article at the top
+        DocumentHelper documentHelper = new DocumentHelper();
+        DOM4JUtil du = new DOM4JUtil();
+
+        Element newArticleE=(Element)doc.selectSingleNode("/Articles/Article[@dir='"+dir+"'][@section='"+section+"']"); 
+        if (newArticleE != null) {
+          log.info("the article  "+dir+" is already on the frontpage"); 
+        } else {
+          newArticleE = documentHelper.createElement("Article");
+          newArticleE.addAttribute("channel", channel);
+          newArticleE.addAttribute("section", section);
+          newArticleE.addAttribute("year", year);
+          newArticleE.addAttribute("dir", dir);                                                                                                   
+          Element articleE=(Element)doc.selectSingleNode("/Articles/Article[1]"); 
+          du.insertElementBefore(articleE, newArticleE);
+        }
+
+        // write the headlines
+        File parent=new File(new File( headlinesFilename).getParent());
+        if(!parent.exists()){
+          parent.mkdirs();
+          }
+
+        OutputFormat format = OutputFormat.createPrettyPrint();
+        try {
+          XMLWriter writer = new XMLWriter(
+                             new BufferedOutputStream(
+                             new FileOutputStream(headlinesFilename)), format);
+          writer.write(doc);
+          writer.close();
+        } catch (Exception e) {
+          log.debug(e);
+        }                                                                                                                                     
+    }                                                                                                                                       
+
+    /** set the published date to the article, only one time 
+     *  @param filename Filename of the article
+     */
+    private boolean setFirstPublishedDate(String filename) throws Exception{
+
+        //get the date
+        Calendar cal=new GregorianCalendar();
+        String year = Integer.toString(cal.get(cal.YEAR));
+        String month = DateUtil.oneToTwoDigits(Integer.toString(cal.get(cal.MONTH)+1));
+        String day = DateUtil.oneToTwoDigits(Integer.toString(cal.get(cal.DAY_OF_MONTH)));
+        String hour=DateUtil.oneToTwoDigits(Integer.toString(cal.get(cal.HOUR_OF_DAY)));
+        String minute=DateUtil.oneToTwoDigits(Integer.toString(cal.get(cal.MINUTE)));
+        String millis = getMillis();
+
+        //read the article 
+        Document doc=new SAXReader().read("file:"+filename);
+
+        //get the PublishedDate Node
+        Element dateE=(Element)doc.selectSingleNode("/NewsML/NewsItem/NewsManagement/PublishDate"); 
+        if (dateE != null) {
+        return false;
+        } 
+
+        DocumentHelper documentHelper = new DocumentHelper();
+
+        dateE = documentHelper.makeElement(doc, "/NewsML/NewsItem/NewsManagement/PublishDate"); 
+        dateE.clearContent();
+
+
+        //set the PublishedDate
+        dateE.addAttribute("year", year);
+        dateE.addAttribute("month", month);
+        dateE.addAttribute("day", day);
+        dateE.addAttribute("hour", hour);
+        dateE.addAttribute("minute", minute);
+        dateE.addAttribute("millis", millis);
+
+        //set the PublishedDate in the head of article (for the frontpage)
+        Element headDateE = documentHelper.makeElement(doc, "/NewsML/NewsItem/NewsComponent/ContentItem/DataContent/nitf/body/body.head/dateline/story.date");
+        headDateE.clearContent();
+        headDateE.addAttribute("norm", day+"."+month+"."+year);  
+        headDateE.addAttribute("millis" , millis);
+
+        //write the article
+        OutputFormat format = OutputFormat.createPrettyPrint();
+        try {
+        XMLWriter writer = new XMLWriter( 
+                           new BufferedOutputStream(
+                           new FileOutputStream(filename)), format);
+        writer.write(doc);
+        writer.close();
+        } catch (Exception e) {
+        log.debug(e);
+        }
+
+        return true;
+    }                                                                                                                                       
+
+    /** set the revision date and the compute the revision id
+     * @param filename Filename of the article 
+     */
+    private void setRevisionDateAndId(String filename) throws Exception{
+        //get the date
+        Calendar cal=new GregorianCalendar();
+        String year = Integer.toString(cal.get(cal.YEAR));
+        String month = DateUtil.oneToTwoDigits(Integer.toString(cal.get(cal.MONTH)+1));
+        String day = DateUtil.oneToTwoDigits(Integer.toString(cal.get(cal.DAY_OF_MONTH)));
+
+        //read the article
+        Document doc=new SAXReader().read("file:"+filename);
+
+        DocumentHelper documentHelper = new DocumentHelper();
+
+        //get the RevisionDate Node
+        Element dateE = documentHelper.makeElement(doc, "/NewsML/NewsItem/NewsManagement/RevisionDate");
+        dateE.clearContent();
+
+        //set the RevisionDate
+        dateE.addAttribute("year", year);
+        dateE.addAttribute("month", month);
+        dateE.addAttribute("day", day);
+
+        //get the RevisionId Node
+        Element revIdE = documentHelper.makeElement(doc, "/NewsML/NewsItem/Identification/NewsIdentifier/RevisionId");
+        String id=revIdE.getText();
+        if (id==null) {
+        id="0"; 
+        }
+        int num = Integer.parseInt(id);
+        num =num+1;
+        String newId = new Integer(num).toString();   
+
+        revIdE.clearContent();
+
+        //set the Revision id
+        revIdE.addAttribute("PreviousRevision", id);
+        revIdE.addAttribute("Update", "N");
+        revIdE.setText(newId);
+
+        //write the article
+        OutputFormat format = OutputFormat.createPrettyPrint();
+        try {
+        XMLWriter writer = new XMLWriter(
+                           new BufferedOutputStream(
+                           new FileOutputStream(filename)), format);
+        writer.write(doc);
+        writer.close();
+        } catch (Exception e) {
+        log.debug(e);
+        }
+    }                                                                                                                                       
+
+    /**
+     *
+     */
+  public synchronized String getMillis(){
+    String millis = Long.toString(new Date().getTime());
+    // wait to be sure the milliseconds are not used more than once
+    try {
+      this.wait(1);
+      } catch (InterruptedException e) {
+      }
+    return millis;
+    }
+  
+    /** Execute the task.
+    *
+    */
+    public void execute(String path) {
+        try {
+            String publicationPath
+                = PublishingEnvironment.getPublicationPath(
+                path, getParameters().getParameter("publication-id"));
+
+            String sourcesString = getParameters().getParameter("sources");
+            StringTokenizer st = new StringTokenizer(sourcesString,",");
+            String sources[] = new String[st.countTokens()];
+            int i = 0;
+            while (st.hasMoreTokens()) {
+                sources[i++] = st.nextToken();
+            }
+
+            prepareArticle(
+                publicationPath,
+                getParameters().getParameter("authoring-path"),
+                sources);
+	} catch (Exception e) {
+	    log.error("Preparing article failed: ", e);
+	}
+    }
+  
+}
---
+++ src/webapp/lenya/pubs/unipublic/java/src/ch/unizh/unipublic/lenya/cms/DossierAction.java
@@ -0,0 +1,334 @@
+/*
+ * DossierAction.java
+ *
+ * Created on November 14, 2002, 3:29 PM
+ */
+
+package ch.unizh.unipublic.wyona.cms;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.acting.AbstractAction;
+import org.apache.cocoon.acting.AbstractComplementaryConfigurableAction;
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.Session;
+import org.apache.cocoon.environment.SourceResolver;
+import org.apache.log4j.Category;
+import org.dom4j.Document;
+import org.dom4j.DocumentFactory;
+import org.dom4j.Element;
+import org.dom4j.Node;
+import org.dom4j.XPath;
+import org.dom4j.io.DOMReader;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
+import org.wyona.cms.publishing.PublishingEnvironment;
+
+/**
+ *
+ * @author  ah
+ */
+public class DossierAction
+    extends AbstractComplementaryConfigurableAction {
+    
+    static Category log = Category.getInstance(DossierAction.class);
+    
+    public static final String ARTICLE_FILE = "article-file";
+    public static final String DOSSIER_ID = "dossier-id";
+  
+    public java.util.Map act(
+            Redirector redirector,
+            SourceResolver sourceResolver,
+            Map objectModel,
+            String str,
+            Parameters parameters) throws Exception {
+                
+        log.debug(
+            "\n--------------------------" +
+            "\n- DossierAction invoked" +
+            "\n--------------------------");
+        
+	// Get Source
+	org.apache.cocoon.environment.Source inputSource = sourceResolver.resolve("");
+	String publicationPath = inputSource.getSystemId();
+	// Remove "file:" protocol
+	publicationPath = publicationPath.substring(5); 
+        
+        if (publicationPath.endsWith("/"))
+            publicationPath = publicationPath.substring(0, publicationPath.length()-1);
+
+        int lastSlashIndex = publicationPath.lastIndexOf("/");
+        String publicationId = publicationPath.substring(lastSlashIndex + 1);
+
+        publicationPath = publicationPath.substring(0, lastSlashIndex + 1);
+
+        String publicationPrefix = PublishingEnvironment.PUBLICATION_PREFIX;
+        String contextPath = publicationPath.replaceAll(publicationPrefix, "");
+        
+        Request request = ObjectModelHelper.getRequest(objectModel);
+
+	if (request == null) {
+	    getLogger().error ("No request object");
+	    return null;
+	}
+	
+        publicationPath = PublishingEnvironment.getPublicationPath(contextPath, publicationId);
+        PublishingEnvironment environment = new PublishingEnvironment(publicationPath);
+        
+        // set parameters using the request parameters
+        log.debug("\n<parameters>");
+        for (Enumeration e = request.getParameterNames(); e.hasMoreElements(); ) {
+            String name = (String) e.nextElement();
+            log.debug("\n  Parameter: " + name + " = " + request.getParameter(name));
+        }
+        log.debug("\n</parameters>");
+        
+        String articlePathParameter = request.getParameter(ARTICLE_FILE);
+        String newDossierId = request.getParameter(DOSSIER_ID);
+        
+        String articlePath =
+            publicationPath +
+            environment.getAuthoringPath() +
+            File.separator +
+            articlePathParameter;
+        
+        String dossierBasePath =
+            publicationPath +
+            environment.getAuthoringPath() +
+            File.separator +
+            "dossiers" +
+            File.separator;
+        
+        String newDossierPath = dossierBasePath + newDossierId + File.separator + "index.xml";
+        
+        File articleFile = new File(articlePath);
+        log.debug("Article file: " + articleFile.getPath());
+        
+        SAXReader reader = new SAXReader();
+        Document articleDocument = reader.read(articleFile);
+
+        DocumentFactory factory = DocumentFactory.getInstance();
+        
+        try {
+            
+            //------------------------------------
+            // article file
+            //------------------------------------
+            
+            StringTokenizer tokenizer = new StringTokenizer(articlePathParameter, "/");
+            String articleChannel = tokenizer.nextToken();
+            String articleSection = tokenizer.nextToken();
+            String articleYear = tokenizer.nextToken();
+            String articleDirectory = tokenizer.nextToken();
+            
+            // create backup
+            backupFile(articlePath);
+
+            // on we go
+            
+            XPath articleXPath = factory.createXPath("/NewsML/NewsItem/NewsComponent/ContentItem/DataContent/nitf/body/body.head");
+            List parentNodes = articleXPath.selectNodes(articleDocument);
+            Element parent = null;
+            if (parentNodes.size() > 0) {
+                parent = (Element) parentNodes.get(0);
+                log.debug(
+                    "\n--------------------------" +
+                    "\n- Parent element: " + parent.getName() +
+                    "\n--------------------------");
+
+            }
+            else {
+                log.error(
+                    "\n--------------------------" +
+                    "\n- Parent node not found!" +
+                    "\n--------------------------");
+            }
+            
+            Element dossierElement = parent.element("dossier");
+
+            String oldDossierId = "none";
+            if (dossierElement != null)
+                oldDossierId = dossierElement.attribute("id").getStringValue();
+            
+            boolean replace = !oldDossierId.equals(newDossierId);
+            
+            // ----------------------------------------------------
+            // do we need to replace it at all?
+            // ----------------------------------------------------
+            if (replace) {
+                
+                // ----------------------------------------------------
+                // does a previous dossier entry exist?
+                // ----------------------------------------------------
+                if (dossierElement != null) {
+
+                    parent.remove(dossierElement);
+
+                    // ----------------------------------------------------
+                    // remove from old dossier file
+                    // ----------------------------------------------------
+                    
+                    String oldDossierPath = dossierBasePath + oldDossierId + File.separator + "index.xml";
+                    File oldDossierFile = new File(oldDossierPath);
+                    
+                    Document oldDossierDocument = reader.read(oldDossierFile);
+
+                    XPath dossierXPath = factory.createXPath("/dossier/articles");
+                    List oldArticlesElements = dossierXPath.selectNodes(oldDossierDocument);
+                    Element oldArticlesElement = null;
+                    if (oldArticlesElements.size() > 0) {
+                        oldArticlesElement = (Element) oldArticlesElements.get(0);
+                        log.debug(
+                            "\n--------------------------" +
+                            "\n- Old Dossier Parent element: " + oldArticlesElement.getName() +
+                            "\n--------------------------");
+
+                    }
+                    else {
+                        log.error(
+                            "\n--------------------------" +
+                            "\n- Old Dossier Parent node not found!" +
+                            "\n--------------------------");
+                    }
+
+                    List articleElements = oldArticlesElement.elements("article");
+                    Element elementToRemove = null;
+                    for (Iterator i = articleElements.iterator(); i.hasNext(); ) {
+                        Element articleElement = (Element) i.next();
+                        if (articleElement.attribute("channel").getStringValue().equals(articleChannel) &&
+                            articleElement.attribute("section").getStringValue().equals(articleSection) &&
+                            articleElement.attribute("year")   .getStringValue().equals(articleYear) &&
+                            articleElement.attribute("dir")    .getStringValue().equals(articleDirectory)) {
+                            elementToRemove = articleElement;
+                        }
+                    }
+
+                    oldArticlesElement.remove(elementToRemove);
+                    writeDocument(oldDossierPath, oldDossierDocument);
+                    
+                    // ----------------------------------------------------
+                    // removing done
+                    // ----------------------------------------------------
+                }
+            
+                
+                if (!newDossierId.equals("none")) {
+                    
+                    // ----------------------------------------------------
+                    // insert new dossier element
+                    // ----------------------------------------------------
+                    
+                    dossierElement = factory.createElement("dossier");
+                    dossierElement.add(factory.createAttribute(
+                            dossierElement, "id", newDossierId));
+                    parent.add(dossierElement);
+
+                    //------------------------------------
+                    // new dossier file
+                    //------------------------------------
+
+                    // create backup
+                    backupFile(newDossierPath);
+
+                    File newDossierFile = new File(newDossierPath);
+                    log.debug("New dossier file: " + newDossierFile.getPath());
+                    Document newDossierDocument = reader.read(newDossierFile);
+
+                    XPath dossierXPath = factory.createXPath("/dossier/articles");
+                    List articlesElements = dossierXPath.selectNodes(newDossierDocument);
+                    Element articlesElement = null;
+                    if (articlesElements.size() > 0) {
+                        articlesElement = (Element) articlesElements.get(0);
+                        log.debug(
+                            "\n--------------------------" +
+                            "\n- Parent element: " + articlesElement.getName() +
+                            "\n--------------------------");
+
+                    }
+                    else {
+                        log.error(
+                            "\n--------------------------" +
+                            "\n- Parent node not found!" +
+                            "\n--------------------------");
+                    }
+
+                    Element articleElement = factory.createElement("article");
+                    articleElement.add(factory.createAttribute(articleElement, "channel", articleChannel));
+                    articleElement.add(factory.createAttribute(articleElement, "section", articleSection));
+                    articleElement.add(factory.createAttribute(articleElement, "year", articleYear));
+                    articleElement.add(factory.createAttribute(articleElement, "dir", articleDirectory));
+                    articlesElement.add(articleElement);
+
+                    writeDocument(newDossierPath, newDossierDocument);
+                }
+
+                // finally save article file
+                writeDocument(articlePath, articleDocument);
+            }
+
+        }
+        catch(Exception e) {
+            log.error("Updating article failed: ", e);
+        }
+        
+        //------------------------------------------------------------
+        // get session
+        //------------------------------------------------------------
+        
+	Session session = request.getSession(true);
+	if (session == null) {
+	    getLogger().error("No session object");
+	    return null;
+	}
+	
+        //------------------------------------------------------------
+	// Return referer
+        //------------------------------------------------------------
+
+        String parent_uri = (String)session.getAttribute("org.wyona.cms.cocoon.acting.DossierAction.parent_uri");
+	HashMap actionMap = new HashMap();
+	actionMap.put("parent_uri", parent_uri);
+	session.removeAttribute("org.wyona.cms.cocoon.acting.DossierAction.parent_uri");
+	return actionMap;
+    }
+    
+    protected void backupFile(String path) {
+        try {
+            SAXReader reader = new SAXReader();
+            Document document = reader.read(new File(path));
+            String backupPath = path.replaceAll(".xml", "_backup.xml");
+            writeDocument(backupPath, document);
+        }
+        catch(Exception e) {
+            log.error("Backup failed: ", e);
+        }
+    }
+    
+    protected void writeDocument(String path, Document document) {
+        try {
+            File file = new File(path);
+            file.createNewFile();
+
+            OutputFormat format = OutputFormat.createPrettyPrint();
+            format.setEncoding("UTF-8");
+            XMLWriter writer = new XMLWriter(new FileOutputStream(file), format);
+            writer.write(document);
+            writer.close();
+        }
+        catch(Exception e) {
+            log.error("Writing document failed: ", e);
+        }
+    }
+    
+}
