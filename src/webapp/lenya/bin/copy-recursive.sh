476dc38c0d7f1c9e4ed663bace8d13a95992e3b8
michicms <dev-null@apache.org>
2002-11-20 23:15:34 +0000

    sftp shell script
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@38493 13f79535-47bb-0310-9956-ffa450edef68

--- src/webapp/lenya/bin/copy-recursive.sh
+++ src/webapp/lenya/bin/copy-recursive.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-SOURCE_DIRECTORY=/home/michiii
+SOURCE_DIRECTORY=/home/michiii/tmp
 
-DESTINATION_DIRECTORY=/home/michiii/tmp
+DESTINATION_DIRECTORY=/home/michiii/backup
 
 #########################################################
 # FUNCTIONS
@@ -11,26 +11,34 @@ DESTINATION_DIRECTORY=/home/michiii/tmp
 
 browseDirectory(){
   CURRENT_DIRECTORY=$1
-  echo "Browsing Directory: $CURRENT_DIRECTORY"
+  DEST_DIR=$2
+  ##echo "DEBUG: Browsing Directory: $CURRENT_DIRECTORY"
 
   FILES_AND_DIRS=`ls $CURRENT_DIRECTORY`
   NUMBER_OF_FILES_AND_DIRS=`ls $CURRENT_DIRECTORY | wc -l`
 
-  echo "Number of files and dirs: $NUMBER_OF_FILES_AND_DIRS"
+  ##echo "DEBUG: Number of files and dirs: $NUMBER_OF_FILES_AND_DIRS"
   if [ $NUMBER_OF_FILES_AND_DIRS = 0 ];then
-    echo "No Files or Dirs within directory"
+    ##echo "DEBUG: No Files or Dirs within directory"
     return
   fi
 
   for FILE_OR_DIR in $FILES_AND_DIRS; do
+    ##echo "DEBUG: File or Dir: $FILE_OR_DIR"
     if [ -d $CURRENT_DIRECTORY/$FILE_OR_DIR ]; then
       echo "Directory: $CURRENT_DIRECTORY/$FILE_OR_DIR"
-      browseDirectory $CURRENT_DIRECTORY/$FILE_OR_DIR
-    fi
-    if [ -f $CURRENT_DIRECTORY/$FILE_OR_DIR ]; then
-      echo "File: $CURRENT_DIRECTORY/$FILE_OR_DIR"
+      browseDirectory $CURRENT_DIRECTORY/$FILE_OR_DIR $DEST_DIR
+      CURRENT_DIRECTORY=`dirname $CURRENT_DIRECTORY`
     else
-      echo "Exception: Neither File nor Directory: $CURRENT_DIRECTORY/$FILE_OR_DIR"
+      if [ -f $CURRENT_DIRECTORY/$FILE_OR_DIR ]; then
+        echo "File: $CURRENT_DIRECTORY/$FILE_OR_DIR"
+
+        cp $CURRENT_DIRECTORY/$FILE_OR_DIR $DEST_DIR/.
+
+        echo $DEST_DIR
+      else
+       echo "Exception: Neither File nor Directory: $CURRENT_DIRECTORY/$FILE_OR_DIR"
+      fi
     fi
   done
   }
@@ -40,5 +48,5 @@ browseDirectory(){
 # MAIN
 #########################################################
 
-
-browseDirectory $SOURCE_DIRECTORY
+CURRENT_DIRECTORY=$SOURCE_DIRECTORY
+browseDirectory $CURRENT_DIRECTORY $DESTINATION_DIRECTORY
