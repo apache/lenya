381b43f6c5e99acdbbf63984008c5b257c6e9e85
michicms <dev-null@apache.org>
2002-11-30 12:57:49 +0000

    FUNCTION downloadRSS implemented
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@38540 13f79535-47bb-0310-9956-ffa450edef68

--- src/webapp/lenya/pubs/oscom/bin/wget-rss.sh
+++ src/webapp/lenya/pubs/oscom/bin/wget-rss.sh
@@ -2,47 +2,42 @@
 
 PARENT_DIR=`dirname $0`
 echo $PARENT_DIR
-PORT=48080
+PORT=8080
+
+
+
+###########
+#
+# FUNCTIONS
+#
+###########
+
+downloadRSS(){
+  RSS_FILE=$1
+
+  wget -t 1 -O $RSS_FILE.tmp http://localhost:$PORT/wyona-cms/oscom/http/$RSS_FILE
+  ERROR=`grep -l "xmlns:error" $RSS_FILE.tmp`
+  if [ $ERROR ];then
+    echo "ERROR: http://localhost:$PORT/wyona-cms/oscom/http/$RSS_FILE"
+    rm $RSS_FILE.tmp
+  else
+    echo "NO ERROR"
+    mv $RSS_FILE.tmp $RSS_FILE
+  fi
+  }
+
+###########
+#
+# MAIN
+#
+###########
 
 cd $PARENT_DIR
 cd ../docs/publication/live/rss-rdf
 
-wget -t 1 -O slashdot.rdf.tmp http://localhost:$PORT/wyona-cms/oscom/http/slashdot.rdf
-ERROR=`grep -l "xmlns:error" slashdot.rdf.tmp`
-if [ $ERROR ];then
-  echo "ERROR: http://localhost:$PORT/wyona-cms/oscom/http/slashdot.rdf"
-  rm slashdot.rdf.tmp
-else
-  echo "NO ERROR"
-  mv slashdot.rdf.tmp slashdot.rdf
-fi
 
-wget -t 1 -O fm.rdf.tmp http://localhost:$PORT/wyona-cms/oscom/http/fm.rdf
-ERROR=`grep -l "xmlns:error" fm.rdf.tmp`
-if [ $ERROR ];then
-  echo "ERROR: http://localhost:$PORT/wyona-cms/oscom/http/fm.rdf"
-  rm fm.rdf.tmp
-else
-  echo "NO ERROR"
-  mv fm.rdf.tmp fm.rdf
-fi
 
-wget -t 1 -O cmsinfo.rdf.tmp http://localhost:$PORT/wyona-cms/oscom/http/cmsinfo.rdf
-ERROR=`grep -l "xmlns:error" cmsinfo.rdf.tmp`
-if [ $ERROR ];then
-  echo "ERROR: http://localhost:$PORT/wyona-cms/oscom/http/cmsinfo.rdf"
-  rm cmsinfo.rdf.tmp
-else
-  echo "NO ERROR"
-  mv cmsinfo.rdf.tmp cmsinfo.rdf
-fi
-
-wget -t 1 -O cmswatch.xml.tmp http://localhost:$PORT/wyona-cms/oscom/http/cmswatch.xml
-ERROR=`grep -l "xmlns:error" cmswatch.xml.tmp`
-if [ $ERROR ];then
-  echo "ERROR: http://localhost:$PORT/wyona-cms/oscom/http/cmswatch.xml"
-  rm cmswatch.xml.tmp
-else
-  echo "NO ERROR"
-  mv cmswatch.xml.tmp cmswatch.xml
-fi
+downloadRSS slashdot.rdf
+downloadRSS fm.rdf
+downloadRSS cmsinfo.rdf
+downloadRSS cmswatch.xml
