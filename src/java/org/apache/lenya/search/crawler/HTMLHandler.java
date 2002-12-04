4918d534bf7ffdf12690f7b4c2b221b9dbc04d8c
michicms <dev-null@apache.org>
2002-12-04 23:23:32 +0000

    Iterative HTML Crawler
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@38548 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/search/crawler/HTMLHandler.java
@@ -0,0 +1,286 @@
+package org.wyona.search.crawler;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.text.html.parser.ParserDelegator;
+import javax.swing.text.html.HTMLEditorKit.ParserCallback;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.html.HTML.Tag;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import javax.swing.text.html.HTML;
+
+
+/**
+ * 
+ */
+public final class HTMLHandler extends ParserCallback implements ContentHandler{
+    
+    // Content
+    private String title;
+    private String description;
+    private String keywords;
+    private String categories;
+    private long published;
+    private String href;
+    private String author;
+    private StringBuffer contents;
+    private ArrayList links;
+    
+    // Robot Instructions
+    private boolean robotIndex;
+    private boolean robotFollow;
+    
+    private static final char space = ' ';
+    private char state;
+    private static final char NONE = 0;
+    private static final char TITLE = 1;
+    private static final char HREF = 2;
+    private static final char SCRIPT = 3;
+    
+    private SimpleDateFormat dateFormatter;
+    private static ParserDelegator pd = new ParserDelegator();
+    
+    /**
+     *		Constructor - initializes variables
+     */
+    public HTMLHandler() {
+        
+        contents = new StringBuffer();
+        links = new ArrayList();
+        published = -1;
+        
+        // 1996.07.10 15:08:56 PST
+        dateFormatter = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss z");
+    }
+    /**
+     * Parse Content. [24] 320:1
+     */
+    public String getAuthor() {
+        return author;
+    }
+    /**
+     * Return categories (from META tags)
+     */
+    public String getCategories() {
+        return this.categories;
+    }
+    /**
+     *	Return contents
+     */
+    public String getContents() {
+        return this.contents.toString();
+    }
+    /**
+     *	Return description (from META tags)
+     */
+    public String getDescription() {
+        return this.description;
+    }
+    /**
+     *	Return META HREF
+     */
+    public String getHREF() {
+        return this.href;
+    }
+    /**
+     * Return keywords (from META tags)
+     */
+    public String getKeywords() {
+        return this.keywords;
+    }
+    /**
+     * Return links
+     */
+    public List getLinks() {
+        return links;
+    }
+    /**
+     *	Return published date (from META tag)
+     */
+    public long getPublished() {
+        return this.published;
+    }
+    /**
+     * Return boolean true if links are to be followed
+     */
+    public boolean getRobotFollow() {
+        return this.robotFollow;
+    }
+    /**
+     * Return boolean true if this is to be indexed
+     */
+    public boolean getRobotIndex() {
+        return this.robotIndex;
+    }
+    /**
+     *		Return page title
+     */
+    public String getTitle() {
+        return this.title;
+    }
+    /**
+     *		Handle Anchor <A HREF="~"> tags
+     */
+    public void handleAnchor(MutableAttributeSet attribs) {
+        String href = new String();
+        href = (String) attribs.getAttribute(HTML.Attribute.HREF);
+        if (href == null)
+            return;
+        links.add(href);
+        state = HREF;
+    }
+    /**
+     *		Closing tag
+     */
+    public void handleEndTag(Tag tag, int pos) {
+        if (state == NONE)
+            return;
+        // In order of precedence == > && > ||
+        if (state == TITLE && tag.equals(HTML.Tag.TITLE)) {
+            state = NONE;
+            return;
+        }
+        if (state == HREF && tag.equals(HTML.Tag.A)) {
+            //links.add(linktext);
+            state = NONE;
+            return;
+        }
+        if (state == SCRIPT && tag.equals(HTML.Tag.SCRIPT)) {
+            state = NONE;
+            return;
+        }
+    }
+    /**
+     *		Handle META tags
+     */
+    public void handleMeta(MutableAttributeSet attribs) {
+        String name = new String();
+        String content = new String();
+        name = (String) attribs.getAttribute(HTML.Attribute.NAME);
+        content = (String) attribs.getAttribute(HTML.Attribute.CONTENT);
+        if (name == null || content == null)
+            return;
+        name = name.toUpperCase();
+        if (name.equals("DESCRIPTION")) {
+            description = content;
+            return;
+        }
+        if (name.equals("KEYWORDS")) {
+            keywords = content;
+            return;
+        }
+        if (name.equals("CATEGORIES")) {
+            categories = content;
+            return;
+        }
+        if (name.equals("PUBLISHED")) {
+            try {
+                published = dateFormatter.parse(content).getTime();
+            } catch(ParseException e) {e.printStackTrace();}
+            return;
+        }
+        if (name.equals("HREF")) {
+            href = content;
+            return;
+        }
+        if (name.equals("AUTHOR")) {
+            author = content;
+            return;
+        }
+        if (name.equals("ROBOTS")) {
+            
+            if (content.indexOf("noindex") != -1) robotIndex = false;
+            if (content.indexOf("nofollow") != -1) robotFollow = false;
+            
+            author = content;
+            return;
+        }
+    }
+    /**
+     *	Handle standalone tags
+     */
+    public void handleSimpleTag(Tag tag, MutableAttributeSet attribs, int pos) {
+        if (tag.equals(HTML.Tag.META)) {
+            handleMeta(attribs);
+        }
+    }
+    /**
+     *		Opening tag
+     */
+    public void handleStartTag(Tag tag, MutableAttributeSet attribs, int pos) {
+        if (tag.equals(HTML.Tag.TITLE)) {
+            state = TITLE;
+        } else if (tag.equals(HTML.Tag.A)) {
+            handleAnchor(attribs);
+        } else if (tag.equals(HTML.Tag.SCRIPT)) {
+            state = SCRIPT;
+        }
+    }
+    /**
+     *		Handle page text
+     */
+    public void handleText(char[] text, int pos) {
+        switch (state) {
+            case NONE :
+                contents.append(text);
+                contents.append(space);
+                break;
+            case TITLE :
+                title = new String(text);
+                break;
+            case HREF :
+                contents.append(text);
+                contents.append(space);
+                //linktext = new String(text);
+                break;
+        }
+    }
+    /**
+     * Parse Content.
+     */
+    public void parse(InputStream in) {
+        
+        try {
+            reset();
+            
+            pd.parse(new BufferedReader(new InputStreamReader(in)), this, true);
+            
+            //System.out.println("Title: " + getTitle());
+            //System.out.println("Author: " + getAuthor());
+            //System.out.println("Published " + getPublished());
+            //System.out.println("Keywords: " + getKeywords());
+            //System.out.println("Description: " + getDescription());
+            //System.out.println("Content: " + getContents());
+            
+        } catch (Exception e) {e.printStackTrace();}
+        
+    }
+    /**
+     *	Return contents
+     */
+    private void reset() {
+        title = null;
+        description = null;
+        keywords = null;
+        categories = null;
+        href = null;
+        author = null;
+        
+        contents.setLength(0);
+        links = new ArrayList();
+        published = -1;
+        
+        // Robot Instructions
+        robotIndex = true;
+        robotFollow = true;
+        
+        state = NONE;
+        
+    }
+}
---
+++ src/java/org/apache/lenya/search/crawler/IterativeHTMLCrawler.java
@@ -0,0 +1,359 @@
+package org.wyona.search.crawler;
+
+import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ *
+ */
+public class IterativeHTMLCrawler{
+  java.util.Vector urlsToCrawl;
+  java.util.TreeSet urlsToCrawlLowerCase;
+
+  String url_list_file="url_file.txt";
+  String html_dump_directory="html_dump";
+
+
+  private String rootURL;
+  //private String currentURLPath;
+  //private java.util.Stack linkQueue;
+  //private java.util.TreeSet links;
+  private String[] scopeURL;
+/**
+ *
+ */
+  public static void main(String[] args){
+    if(args.length != 4){
+      System.err.println("Usage: IterativeHTMLCrawler startURL scope url_list_file html_dump_directory (http://127.0.0.1:8080/index.html http://127.0.0.1:8080/ /home/wyona/url_list.txt /home/wyona/html_dump)");
+      return;
+      }
+ 
+    try{
+      new IterativeHTMLCrawler(args[2],args[3]).crawl(new URL(args[0]),args[1]);
+      }
+    catch(MalformedURLException e){
+      System.err.println(e);
+      }
+    }
+/**
+ *
+ */
+  public IterativeHTMLCrawler(String url_list_file,String html_dump_directory){
+    this.url_list_file=url_list_file;
+    this.html_dump_directory=html_dump_directory;
+    }
+/**
+ * 
+ */
+  public void crawl(URL start,String scope){
+    scopeURL=new String[1];
+    scopeURL[0]=scope;
+    //scopeURL[0]="http://www.wyona.com/";
+
+    String seedURL=start.toString();
+    this.rootURL = seedURL.substring(0, seedURL.indexOf("/", 8));    
+
+    urlsToCrawl=new java.util.Vector();
+    urlsToCrawlLowerCase=new java.util.TreeSet();
+
+    urlsToCrawl.add(start);
+    urlsToCrawlLowerCase.add(start.toString().toLowerCase());
+    dumpHTML(start);
+
+    int currentPosition=0;
+    while(currentPosition < urlsToCrawl.size()){
+      System.out.println(".crawl(): INFO: Current Position: "+currentPosition+", Current Array Size: "+urlsToCrawl.size());
+
+
+      URL currentURL=(URL)urlsToCrawl.elementAt(currentPosition);
+      String currentURLPath = currentURL.toString().substring(0, currentURL.toString().lastIndexOf("/"));
+      System.out.println(".crawl(): Current URL: "+currentURL.toString());
+
+      java.util.List urlsWithinPage=parsePage(currentURL.toString());
+      if(urlsWithinPage != null){
+        java.util.Iterator iterator=urlsWithinPage.iterator();
+        while(iterator.hasNext()){
+          String urlCandidate=(String)iterator.next();
+          if(filterURL(urlCandidate,currentURLPath,urlsToCrawlLowerCase)){
+            try{
+              URL urlToCrawl=new URL(parseHREF(urlCandidate,urlCandidate.toLowerCase(),currentURLPath)); //completeURL(currentURL,urlCandidate)  new URL(currentURLPath+"/"+urlCandidate);
+              System.out.println(".crawl(): ADDED: "+urlToCrawl); 
+              urlsToCrawl.add(urlToCrawl);
+              urlsToCrawlLowerCase.add(urlToCrawl.toString().toLowerCase());
+              dumpHTML(urlToCrawl);
+              }
+            catch(MalformedURLException e){
+              System.err.println(".crawl(): "+e);
+              }
+            }
+          }
+        }
+
+      currentPosition=currentPosition+1;
+      //currentPosition=currentPosition+10000;
+      }
+
+
+
+    try{
+    java.io.PrintWriter out=new java.io.PrintWriter(new java.io.FileOutputStream(url_list_file));
+    System.out.println("\n\n\n");
+    System.out.println(".crawl(): URLs to crawl:");
+    for(int i=0;i<urlsToCrawl.size();i++){
+      System.out.println(".crawl(): URL to crawl: "+(URL)urlsToCrawl.elementAt(i));
+      out.println(""+(URL)urlsToCrawl.elementAt(i));
+      }
+    out.close();
+      }
+    catch(java.io.FileNotFoundException e){
+      System.err.println(".crawl(): "+e);
+      }
+    }
+/**
+ * @return ok, 404
+ */
+  public java.util.List parsePage(String urlString){
+    System.out.println(".parsePage(): Parse page: "+urlString);
+    String status = "ok";
+        try {
+            URL currentURL = new java.net.URL(urlString);
+            String currentURLPath = urlString.substring(0, urlString.lastIndexOf("/"));
+            HttpURLConnection httpCon = (HttpURLConnection)currentURL.openConnection();
+
+            httpCon.setRequestProperty("User-Agent", "Wyona Lucene Crawler");
+
+/*
+            if (cookie != null) {
+                httpCon.setRequestProperty("Cookie", this.cookie);
+            }
+*/
+            httpCon.connect();
+
+
+            long lastModified = httpCon.getLastModified();
+
+/*
+            if (httpCon.getHeaderField("Set-Cookie") != null) {
+                cookie = stripCookie(httpCon.getHeaderField("Set-Cookie"));
+                if (reporting > 1) report.print(" got cookie : " + cookie);
+            }
+*/
+
+            if (httpCon.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                String contentType = httpCon.getContentType();
+                if (contentType.indexOf("text/html") != -1) {
+                    return handleHTML(httpCon,currentURLPath);
+                } else if (contentType.indexOf("application/pdf") != -1) {
+                    handlePDF(httpCon);
+                } else {
+                    status = "Not an excepted content type : " + contentType;
+                }
+            } else {
+                status = "bad";
+            }
+            httpCon.disconnect();
+        } catch (java.net.MalformedURLException mue) {
+            status = mue.toString();
+        } catch (java.net.UnknownHostException uh) {
+            status = uh.toString(); // Mark as a bad URL
+        } catch (java.io.IOException ioe) {
+            status = ioe.toString(); // Mark as a bad URL
+        } catch (Exception e) {
+            status = e.toString(); // Mark as a bad URL
+        }
+
+        //return status;
+        return null;
+    }
+/**
+ *
+ */
+  public java.util.List handleHTML(HttpURLConnection httpCon,String currentURLPath) throws java.io.IOException{
+    ContentHandler handler = new HTMLHandler();
+    handler.parse(httpCon.getInputStream());
+
+    if(handler.getRobotFollow()){
+      java.util.List links = handler.getLinks();
+      System.out.println(".handleHTML(): Number of links found : " + links.size());
+/*
+      for(int i = 0; i < links.size(); i++){
+        System.out.println(".handleHTML(): "+(String)links.get(i));
+        }
+*/
+      if(true){
+        return links;
+        }
+      }
+
+/*
+    if(handler.getRobotIndex()){
+      System.out.println(".handleHTML(): Index by Lucene: Crawler.indexLucene()");
+      }
+*/
+    return null;
+    }
+/**
+ *
+ */
+  public void handlePDF(HttpURLConnection httpCon){
+    System.err.println(".handlePDF(): Not handled yet!");
+    }
+/**
+ *
+ */
+  public boolean filterURL(String url,String currentURLPath,java.util.TreeSet links){
+    //System.out.println(".filterURL(): URL to filter: "+url);
+
+    String urlLowCase = url.toLowerCase();
+
+    if(!(urlLowCase.startsWith("http://") || urlLowCase.startsWith("https://"))){
+      url = parseHREF(url,urlLowCase,currentURLPath);
+      if(url != null) {
+        urlLowCase = url.toLowerCase();
+        }
+      }
+    //System.out.println(".filterURL(): "+urlLowCase);
+
+    if(url != null && inScope(urlLowCase)){
+      //System.out.println(".filterURL(): In scope: "+url);
+      if(!links.contains(urlLowCase)){
+        links.add(urlLowCase);
+        //linkQueue.push(url);
+        //System.out.println(".filterURL(): Added: "+url);
+        return true;
+        }
+      else{
+        //System.out.println(".filterURL(): Not Added (already added) : "+url);
+        }
+      }
+    else{
+      //System.out.println(".filterURL(): Not in scope: "+url);
+      }
+    return false;
+    }
+/**
+ *
+ */
+  public String parseHREF(String url, String urlLowCase,String currentURLPath){
+    //System.out.println(".parseHREF(): "+url);
+    //System.out.println(".parseHREF(): "+urlLowCase);
+    //System.out.println(".parseHREF(): "+currentURLPath);
+
+        // Looks for incomplete URL and completes them
+        if (urlLowCase.startsWith("/")) {
+            url = rootURL + url;
+        } else if (urlLowCase.startsWith("./")) {
+            url = currentURLPath + url.substring(1, url.length());
+        } else if (urlLowCase.startsWith("../")) {
+            int back = 1;
+            while (urlLowCase.indexOf("../", back*3) != -1) back++;
+            int pos = currentURLPath.length();
+            int count = back;
+            while (count-- > 0) {
+                pos = currentURLPath.lastIndexOf("/", pos) - 1;
+            }
+            url = currentURLPath.substring(0, pos+2) + url.substring(3*back, url.length());
+        } else if (urlLowCase.startsWith("javascript:")) {
+            // handle javascript:...
+            //url = parseJavaScript(url, urlLowCase);
+            System.err.println(".parseHREF(): parseJavaScript is not implemented yet");
+        } else if (urlLowCase.startsWith("#")) {
+            // internal anchor... ignore.
+            url = null;
+        } else if (urlLowCase.startsWith("mailto:")) {
+            // handle mailto:...
+            url = null;
+        } else {
+            url = currentURLPath + "/" + url;
+        }
+
+        // strip anchor if exists otherwise crawler may index content multiple times
+        // links to the same url but with unique anchors would be considered unique
+        // by the crawler when they should not be
+        //int i;
+        if (url != null) {
+            int i;
+            if ((i = url.indexOf("#")) != -1) {
+                url = url.substring(0,i);
+            }
+        }
+        return url;
+
+    }
+/**
+ *
+ */
+  public boolean inScope(String url){
+    //System.out.println(".inScope(): "+url);
+    for(int i = 0; i < scopeURL.length; i++){
+      if(url.startsWith(scopeURL[i])){
+        return true;
+        }
+      }
+    return false;
+    }
+/**
+ *
+ */
+  public URL completeURL(URL parent,String child) throws MalformedURLException{
+    System.out.println(".completeURL(): "+parent);
+    System.out.println(".completeURL(): "+child);
+    return parent;
+    }
+/**
+ *
+ */
+  public void dumpHTML(URL url){
+    System.out.println(".dumpHTML(): "+url);
+
+    try{
+      HttpURLConnection httpConnection=(HttpURLConnection)url.openConnection();
+      java.io.InputStream in=httpConnection.getInputStream();
+      byte[] buffer=new byte[1024];
+      int bytes_read;
+      java.io.ByteArrayOutputStream bufferOut=new java.io.ByteArrayOutputStream();
+      while((bytes_read=in.read(buffer)) != -1){
+        bufferOut.write(buffer,0,bytes_read);
+        }
+      byte[] sresponse=bufferOut.toByteArray();
+      httpConnection.disconnect();
+
+      saveToFile(html_dump_directory+url.getFile(),sresponse);
+      }
+    catch(Exception e){
+     System.err.println(".dumpHTML(): "+e);
+     }
+
+
+/*
+    org.wyona.net.WGet wget=new org.wyona.net.WGet();
+    byte[] response=wget.download(url);
+    wget.setDirectoryPrefix("/home/wmi/site85_html");
+    wget.saveToFile(url.getFile(),response);
+*/
+    }
+/**
+ *
+ */
+  public void saveToFile(String filename,byte[] bytes) throws FileNotFoundException, IOException{
+    System.out.println(".saveToFile(): "+filename);
+    File file=new File(filename);
+    if(filename.charAt(filename.length()-1) == '/'){
+      file=new File(filename+"index.html");
+      }
+    File parent=new File(file.getParent());
+    if(!parent.exists()){
+      System.out.println(".saveToFile(): Directory will be created: "+parent.getAbsolutePath());
+      parent.mkdirs();
+      }
+    //log.debug(".saveToFile(): Filename: "+file.getAbsolutePath());
+    FileOutputStream out=new FileOutputStream(file.getAbsolutePath());
+    out.write(bytes);
+    out.close();
+    }
+  }
