b60dcbcd3b621a599ac88f7b4095a3a6aef34cc6
michicms <dev-null@apache.org>
2003-01-15 00:40:56 +0000

    Slides improved
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@38652 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/lucene/IndexEnvironment.java
@@ -0,0 +1,83 @@
+package org.wyona.lucene;
+
+import org.apache.avalon.framework.configuration.Configurable;
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.configuration.DefaultConfigurationBuilder;
+import org.apache.log4j.Category;
+
+import java.io.File;
+
+/**
+ * @author Michael Wechner
+ */
+public class IndexEnvironment implements Configurable{
+  static Category log=Category.getInstance(IndexEnvironment.class);
+
+  private String configurationFilePath;
+
+  private String index_dir;
+  private String htdocs_dump_dir;
+/**
+ *
+ */
+  public static void main(String[] args){
+    if(args.length != 1){
+      System.err.println("Usage: org.wyona.lucene.IndexEnvironment lucene.xconf");
+      return;
+      }
+    IndexEnvironment ie=new IndexEnvironment(args[0]);
+    String parameter;
+
+    parameter=ie.getIndexDir();
+    System.out.println(parameter);
+    System.out.println(ie.resolvePath(parameter));
+
+    parameter=ie.getHTDocsDumpDir();
+    System.out.println(parameter);
+    System.out.println(ie.resolvePath(parameter));
+    }
+/**
+ *
+ */
+  public IndexEnvironment(String configurationFilePath){
+    this.configurationFilePath=configurationFilePath;
+
+    File configurationFile = new File(configurationFilePath);
+    try{
+      DefaultConfigurationBuilder builder=new DefaultConfigurationBuilder();
+      Configuration configuration = builder.buildFromFile(configurationFile);
+      configure(configuration);
+      }
+    catch(Exception e){
+      log.error("Cannot load publishing configuration! ",e);
+      }
+    }
+/**
+ *
+ */
+  public void configure(Configuration configuration) throws ConfigurationException{
+    index_dir=configuration.getChild("index-dir").getAttribute("src");
+    htdocs_dump_dir=configuration.getChild("htdocs-dump-dir").getAttribute("src");
+    }
+/**
+ *
+ */
+  public String getIndexDir(){
+    log.debug(".getIndexDir(): "+index_dir);
+    return index_dir;
+    }
+/**
+ *
+ */
+  public String getHTDocsDumpDir(){
+    log.debug(".getHTDocsDumpDir(): "+htdocs_dump_dir);
+    return htdocs_dump_dir;
+    }
+/**
+ *
+ */
+  public String resolvePath(String path){
+    return org.apache.avalon.excalibur.io.FileUtil.catPath(configurationFilePath,path);
+    }
+}
