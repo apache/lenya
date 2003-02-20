604149aabf9a9dd6e3b0466613c5619eb1ae1231
Gregor Rothfuss <gregor@apache.org>
2003-02-20 16:57:34 +0000

    start moving wyona menu out of ethz publication
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@38869 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/webapp/lenya/resources/misc/menu/menu.js
@@ -0,0 +1,132 @@
+
+
+function initialize()
+        //runs at onload of window
+        {
+        //obj = document.getElementById("navTop")
+        //obj.style.top = 75; //fixes a bug in the clipping animation
+       // ihtScreen = new screenObject();
+        if (document.getElementById("articleBody") != null) initArticle(); //lays out the article in columns(if one exists)
+        userEventsInit(); //set up all user events
+        document.onmousemove = mousePosition; //moz
+
+       // windowTimer = setInterval("placeNavBar()",20);
+
+       
+// window.onresize = windowResize;
+        }
+
+var mouseX = mouseY =0;
+
+function screenObject()
+        {
+
+        this.bottom = function(){
+                if (document.body.scrollHeight) return document.body.scrollHeight};
+
+        this.height = function() {
+                if (document.body.offsetHeight) return document.body.offsetHeight;}
+
+        this.visHeight = function() {
+                if (window.innerHeight) return window.innerHeight;
+                if (document.body.clientHeight) return document.body.clientHeight;}
+
+        this.width = function() {
+                if (document.body.offsetWidth) return document.body.offsetWidth;}
+
+        this.scrollTop = function() {
+                if(document.body.scrollTop) return document.body.scrollTop
+                if (window.pageYOffset) return window.pageYOffset;
+                else return 0;};
+
+
+        }
+
+function mousePosition(e)
+        {
+        if (e) event = e;   //for Netscape
+        mouseX = event.clientX;
+        mouseY = event.clientY;
+        }
+
+var allEvents = new Array()
+
+var activeMenu = null; //defines which menu is currently open
+
+function menuOver()
+        {
+        document.onclick = null;
+        if (document.all) event.cancelBubble=true
+        }
+function menuOut()
+        {
+        document.onclick = eventHideMenu;
+        if (document.all) event.cancelBubble=true
+        }
+
+function eventShowPopUp(target)
+        //this will display info pop ups, for example art event details
+
+        {
+        if (activeMenu != null) eventHideMenu()
+        activeMenu = target;
+        obj = document.getElementById(activeMenu)
+        obj.style.display = "block"
+        if (event) event.cancelBubble=true
+        }
+
+function eventShowArtPopUp(target)
+        //this will display info pop ups, for example art event details
+
+        {
+
+        if (activeMenu != null) eventHideMenu()
+        activeMenu = target;
+        obj = document.getElementById(activeMenu)
+        obj.style.display = "block"
+        obj.style.visibility = "visible"
+
+        obj.style.top = -20+mouseY+ihtScreen.scrollTop()+"px"
+        if (event) event.cancelBubble=true
+        }
+
+function eventShowMenu(e)
+        //turns on the display for nav menus
+        {
+        if (activeMenu != null) eventHideMenu()
+        activeMenu = "menu"+this.id.substring(3,this.id.length)
+        obj = document.getElementById(activeMenu)
+        if (obj) obj.style.visibility = "visible"
+        if (document.all) document.onclick = eventHideMenu;
+        event.cancelBubble=true
+
+        }
+
+function eventHideMenu()
+        //hides nav menus and pop ups
+        {
+        document.onclick = null;
+        if (activeMenu != null)
+                {
+                obj = document.getElementById(activeMenu)
+                obj.style.visibility = "hidden"
+                }
+        }
+
+menus = new Array("File","Search","Help","Debug")
+
+function userEventsInit()
+        {
+        //nav events
+                for (i=0; i < menus.length; i++)
+                {
+                obj = document.getElementById("nav"+menus[i])
+                obj.onclick = eventShowMenu;
+
+                obj = document.getElementById("menu"+menus[i])
+                obj.onmouseover = menuOver;
+                obj.onmouseout = menuOut;
+                }
+
+ }
+
---
+++ src/webapp/lenya/resources/misc/menu/netscape.js
@@ -0,0 +1,19 @@
+/*
+
+$RCSfile: netscape.js,v $ 
+$Author: gregor $  
+$Date: 2003/02/20 16:57:17 $  
+$Locker:  $ 
+$Revision: 1.1 $ 
+
+*/
+
+
+if (!document.all)
+	{
+	document.writeln('<style type="text/css">');
+	document.writeln('.c, .click {cursor:pointer}');
+	document.writeln('.noborder {border:1px solid}');
+	document.writeln('</style>');
+	}
+
