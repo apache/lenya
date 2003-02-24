4da90b71410e6fc8f4eb9ddc206a28c4643ac238
Michael Wechner <michi@apache.org>
2003-02-24 22:49:38 +0000

    OSCOM creators moved to OSCOM publication
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@38890 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/webapp/lenya/pubs/oscom/java/src/org/oscom/lenya/cms/CMSProjectCreator.java
@@ -0,0 +1,150 @@
+/*
+ * $Id: CMSProjectCreator.java,v 1.1 2003/02/24 22:49:25 michi Exp $
+ * <License>
+ * The Apache Software License
+ *
+ * Copyright (c) 2002 wyona. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ *    list of conditions and the following disclaimer in the documentation and/or
+ *    other materials provided with the distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this software must
+ *    display the following acknowledgment: "This product includes software developed
+ *    by wyona (http://www.wyona.org)"
+ *
+ * 4. The name "wyona" must not be used to endorse or promote products derived from
+ *    this software without prior written permission. For written permission, please
+ *    contact contact@wyona.org
+ *
+ * 5. Products derived from this software may not be called "wyona" nor may "wyona"
+ *    appear in their names without prior written permission of wyona.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following acknowledgment:
+ *    "This product includes software developed by wyona (http://www.wyona.org)"
+ *
+ * THIS SOFTWARE IS PROVIDED BY wyona "AS IS" WITHOUT ANY WARRANTY EXPRESS OR IMPLIED,
+ * INCLUDING THE WARRANTY OF NON-INFRINGEMENT AND THE IMPLIED WARRANTIES OF MERCHANTI-
+ * BILITY AND FITNESS FOR A PARTICULAR PURPOSE. wyona WILL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY YOU AS A RESULT OF USING THIS SOFTWARE. IN NO EVENT WILL wyona BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR LOST PROFITS EVEN IF wyona HAS
+ * BEEN ADVISED OF THE POSSIBILITY OF THEIR OCCURRENCE. wyona WILL NOT BE LIABLE FOR ANY
+ * THIRD PARTY CLAIMS AGAINST YOU.
+ *
+ * Wyona includes software developed by the Apache Software Foundation, W3C,
+ * DOM4J Project, BitfluxEditor and Xopus.
+ * </License>
+ */
+package org.oscom.wyona.cms;
+
+import org.apache.log4j.Category;
+
+import org.w3c.dom.Document;
+
+import org.wyona.cms.authoring.AbstractParentChildCreator;
+import org.wyona.xml.DocumentHelper;
+import org.wyona.xml.DOMUtil;
+
+import java.io.File;
+import java.net.URL;
+
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author Michael Wechner
+ * @version 2003.2.11
+ */
+public class CMSProjectCreator extends AbstractParentChildCreator {
+    static Category log = Category.getInstance(CMSProjectCreator.class);
+
+    /**
+     * Why do we have to overwrite this method?
+     *
+     * @param childType DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     *
+     * @throws Exception DOCUMENT ME!
+     */
+    public short getChildType(short childType) throws Exception {
+        return AbstractParentChildCreator.LEAF_NODE;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param childname DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     *
+     * @throws Exception DOCUMENT ME!
+     */
+    public String getChildName(String childname) throws Exception {
+        return childname;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param childId DOCUMENT ME!
+     * @param childType DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     *
+     * @throws Exception DOCUMENT ME!
+     */
+    public String generateTreeId(String childId, short childType)
+        throws Exception {
+        return childId;
+    }
+
+    /**
+     * Create the new editor project
+     *
+     * @param samplesDir DOCUMENT ME!
+     * @param parentDir DOCUMENT ME!
+     * @param childId DOCUMENT ME!
+     * @param childType DOCUMENT ME!
+     * @param childName DOCUMENT ME!
+     *
+     * @throws Exception DOCUMENT ME!
+     */
+    public void create(File samplesDir, File parentDir, String childId, short childType,
+        String childName) throws Exception {
+        // Set filenames
+        String id = generateTreeId(childId, childType);
+        String filename = parentDir + "/" + id + ".xml";
+        String doctypeSample = samplesDir + "/CMSProject.xml";
+
+        // Read sample file
+        log.debug(".create(): Try to read file: " + doctypeSample);
+
+        Document doc=DocumentHelper.readDocument(new URL("file:" + doctypeSample));
+
+
+        DOMUtil du = new DOMUtil();
+        du.setElementValue(doc, "/system/id",id);
+        du.setElementValue(doc, "/system/system_name",childName);
+
+        log.debug(".create(): system_name = "+du.getElementValue(doc.getDocumentElement(), new org.wyona.xml.XPath("system_name")));
+
+
+        // Create parent directory
+        File parent = new File(new File(filename).getParent());
+        if (!parent.exists()) {
+            parent.mkdirs();
+        }
+
+        // Write file
+        java.io.FileOutputStream out=new java.io.FileOutputStream(filename);
+        new org.wyona.xml.DOMWriter(out).printWithoutFormatting(doc);
+        out.close();
+    }
+}
---
+++ src/webapp/lenya/pubs/oscom/java/src/org/oscom/lenya/cms/EditorProjectCreator.java
@@ -0,0 +1,160 @@
+/*
+ * $Id: EditorProjectCreator.java,v 1.1 2003/02/24 22:49:25 michi Exp $
+ * <License>
+ * The Apache Software License
+ *
+ * Copyright (c) 2002 wyona. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ *    list of conditions and the following disclaimer in the documentation and/or
+ *    other materials provided with the distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this software must
+ *    display the following acknowledgment: "This product includes software developed
+ *    by wyona (http://www.wyona.org)"
+ *
+ * 4. The name "wyona" must not be used to endorse or promote products derived from
+ *    this software without prior written permission. For written permission, please
+ *    contact contact@wyona.org
+ *
+ * 5. Products derived from this software may not be called "wyona" nor may "wyona"
+ *    appear in their names without prior written permission of wyona.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following acknowledgment:
+ *    "This product includes software developed by wyona (http://www.wyona.org)"
+ *
+ * THIS SOFTWARE IS PROVIDED BY wyona "AS IS" WITHOUT ANY WARRANTY EXPRESS OR IMPLIED,
+ * INCLUDING THE WARRANTY OF NON-INFRINGEMENT AND THE IMPLIED WARRANTIES OF MERCHANTI-
+ * BILITY AND FITNESS FOR A PARTICULAR PURPOSE. wyona WILL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY YOU AS A RESULT OF USING THIS SOFTWARE. IN NO EVENT WILL wyona BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR LOST PROFITS EVEN IF wyona HAS
+ * BEEN ADVISED OF THE POSSIBILITY OF THEIR OCCURRENCE. wyona WILL NOT BE LIABLE FOR ANY
+ * THIRD PARTY CLAIMS AGAINST YOU.
+ *
+ * Wyona includes software developed by the Apache Software Foundation, W3C,
+ * DOM4J Project, BitfluxEditor and Xopus.
+ * </License>
+ */
+package org.oscom.wyona.cms;
+
+import org.apache.log4j.Category;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+import org.dom4j.io.SAXReader;
+
+import org.wyona.cms.authoring.AbstractParentChildCreator;
+
+import java.io.File;
+import java.io.FileWriter;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author Michael Wechner
+ * @version 2002.12.29
+ */
+public class EditorProjectCreator extends AbstractParentChildCreator {
+    static Category log = Category.getInstance(EditorProjectCreator.class);
+
+    /**
+     * Why do we have to overwrite this method?
+     *
+     * @param childType DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     *
+     * @throws Exception DOCUMENT ME!
+     */
+    public short getChildType(short childType) throws Exception {
+        return AbstractParentChildCreator.LEAF_NODE;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param childname DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     *
+     * @throws Exception DOCUMENT ME!
+     */
+    public String getChildName(String childname) throws Exception {
+        return childname;
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param childId DOCUMENT ME!
+     * @param childType DOCUMENT ME!
+     *
+     * @return DOCUMENT ME!
+     *
+     * @throws Exception DOCUMENT ME!
+     */
+    public String generateTreeId(String childId, short childType)
+        throws Exception {
+        return childId;
+    }
+
+    /**
+     * Create the new editor project
+     *
+     * @param samplesDir DOCUMENT ME!
+     * @param parentDir DOCUMENT ME!
+     * @param childId DOCUMENT ME!
+     * @param childType DOCUMENT ME!
+     * @param childName DOCUMENT ME!
+     *
+     * @throws Exception DOCUMENT ME!
+     */
+    public void create(File samplesDir, File parentDir, String childId, short childType,
+        String childName) throws Exception {
+        // Set filenames
+        String id = generateTreeId(childId, childType);
+        String filename = parentDir + "/" + id + ".xml";
+        String doctypeSample = samplesDir + "/EditorProject.xml";
+
+        // Read sample file
+        log.debug(".create(): Try to read file: " + doctypeSample);
+
+        Document doc = new SAXReader().read("file:" + doctypeSample);
+
+        // Replace id
+        Element eid = (Element) doc.selectSingleNode("/system/id");
+
+        if (eid != null) {
+            log.debug(eid.getPath() + " " + eid.getText());
+            eid.addText(id);
+            log.debug(eid.getPath() + " " + eid.getText());
+        }
+
+        // Replace name
+        Element ename = (Element) doc.selectSingleNode("/system/system_name");
+
+        if (ename != null) {
+            log.debug(ename.getPath() + " " + ename.getText());
+            ename.addText(childName);
+            log.debug(ename.getPath() + " " + ename.getText());
+        }
+
+        // Write file
+        File parent = new File(new File(filename).getParent());
+
+        if (!parent.exists()) {
+            parent.mkdirs();
+        }
+
+        FileWriter fileWriter = new FileWriter(filename);
+        doc.write(fileWriter);
+        fileWriter.close();
+    }
+}
