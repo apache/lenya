a3d3ee09e5bb7bc4bc75e9a38d27146a50dfdace
Christian Egli <egli@apache.org>
2003-02-26 16:28:08 +0000

    Removed dead code.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@38909 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/cocoon/generation/ProxyGenerator.java
+++ src/java/org/apache/lenya/cms/cocoon/generation/ProxyGenerator.java
@@ -1,5 +1,5 @@
 /*
- * $Id: ProxyGenerator.java,v 1.9 2003/02/20 13:40:40 gregor Exp $
+ * $Id: ProxyGenerator.java,v 1.10 2003/02/26 16:28:08 egli Exp $
  * <License>
  * The Apache Software License
  *
@@ -48,11 +48,9 @@ import org.apache.avalon.framework.parameters.ParameterException;
 import org.apache.avalon.framework.parameters.Parameterizable;
 import org.apache.avalon.framework.parameters.Parameters;
 
-import org.apache.cocoon.ProcessingException;
 import org.apache.cocoon.environment.Cookie;
 import org.apache.cocoon.environment.ObjectModelHelper;
 import org.apache.cocoon.environment.Request;
-import org.apache.cocoon.environment.SourceResolver;
 import org.apache.cocoon.environment.http.HttpRequest;
 
 import org.apache.commons.httpclient.HttpClient;
@@ -63,20 +61,17 @@ import org.apache.log4j.Category;
 
 import org.xml.sax.InputSource;
 
-//import org.apache.log4j.Category;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.AttributesImpl;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import java.util.Enumeration;
-import java.util.Map;
 
 
 /**
@@ -88,7 +83,6 @@ import java.util.Map;
 public class ProxyGenerator extends org.apache.cocoon.generation.ServletGenerator
     implements Parameterizable {
     static Category log = Category.getInstance(ProxyGenerator.class);
-    protected String src;
 
     // The URI of the namespace of this generator
     private String URI = "http://www.wyona.org/wyona-cms/proxygenerator/1.0";
@@ -105,40 +99,11 @@ public class ProxyGenerator extends org.apache.cocoon.generation.ServletGenerato
     }
 
     /**
-     * DOCUMENT ME!
-     *
-     * @param resolver DOCUMENT ME!
-     * @param objectModel DOCUMENT ME!
-     * @param src DOCUMENT ME!
-     * @param par DOCUMENT ME!
-     *
-     * @throws ProcessingException DOCUMENT ME!
-     * @throws SAXException DOCUMENT ME!
-     * @throws IOException DOCUMENT ME!
-     */
-    public void setup(SourceResolver resolver, Map objectModel, String src, Parameters par)
-        throws ProcessingException, SAXException, IOException {
-        super.setup(resolver, objectModel, src, par);
-
-        try {
-            //log.debug(".setup(): "+src);
-            this.src = src;
-        } catch (Exception e) {
-            //log.error(e);
-        }
-    }
-
-    /**
      * Generate XML data.
      *
      * @throws SAXException DOCUMENT ME!
      */
     public void generate() throws SAXException {
-        /*  NOTE: httpRequest not used anymore!
-            HttpRequest httpRequest
-                = (HttpRequest) objectModel.get(ObjectModelHelper.REQUEST_OBJECT);
-            log.debug(".generate(): server-port: "+httpRequest.getServerPort());
-        */
         Request request = (Request) objectModel.get(ObjectModelHelper.REQUEST_OBJECT);
 
         log.debug("\n----------------------------------------------------------------" +
@@ -157,18 +122,11 @@ public class ProxyGenerator extends org.apache.cocoon.generation.ServletGenerato
                 if (request instanceof HttpRequest) {
                     java.io.InputStream is = intercept(((HttpRequest) request).getInputStream());
                 }
-
-                //log.debug("HTTP method: "+submitMethod);
-            } else if (submitMethod.equals("GET")) {
-                //log.debug("HTTP method: "+submitMethod);
-            } else {
-                //log.debug("HTTP method: "+submitMethod);
             }
 
             URL url = createURL(request);
 
             // Forward "InputStream", Parameters, QueryString to Servlet
-            //log.debug(".generate(): Remote URL: "+url);
             org.apache.commons.httpclient.HttpMethod httpMethod = null;
 
             if (submitMethod.equals("POST")) {
@@ -197,12 +155,10 @@ public class ProxyGenerator extends org.apache.cocoon.generation.ServletGenerato
                 transferedCookies = new org.apache.commons.httpclient.Cookie[cookies.length];
 
                 for (int i = 0; i < cookies.length; i++) {
-                    //log.warn(".generate(): Original COOKIE: "+cookies[i].getPath()+" "+cookies[i].getName()+" "+cookies[i].getDomain()+" "+cookies[i].getValue());
                     boolean secure = false; // http: false, https: true
                     transferedCookies[i] = new org.apache.commons.httpclient.Cookie(url.getHost(),
                             cookies[i].getName(), cookies[i].getValue(), url.getFile(), null, secure);
 
-                    //log.warn(".generate(): Copied COOKIE: "+transferedCookies[i]);
                 }
             }
 
@@ -217,12 +173,6 @@ public class ProxyGenerator extends org.apache.cocoon.generation.ServletGenerato
             }
 
             // DEBUG cookies
-            /*
-                      org.apache.commons.httpclient.Cookie[] tcookies=httpClient.getState().getCookies();
-                      for(int i=0;i<tcookies.length;i++){
-                        log.warn(".generate(): Transfered COOKIE: "+tcookies[i]);
-                        }
-            */
             // Send request to servlet
             httpMethod.setRequestHeader("Content-type", "text/plain");
             httpMethod.setPath(url.getPath());
@@ -249,7 +199,6 @@ public class ProxyGenerator extends org.apache.cocoon.generation.ServletGenerato
 
             byte[] sresponse = httpMethod.getResponseBody();
 
-            //log.warn(".generate(): Response from remote server: "+new String(sresponse));
             httpClient.endSession();
 
             // Return XML
@@ -290,23 +239,19 @@ public class ProxyGenerator extends org.apache.cocoon.generation.ServletGenerato
             bufferOut.write(buffer, 0, bytes_read);
         }
 
-        //log.warn("Intercepted Input Stream:\n\n"+bufferOut.toString());
         return new ByteArrayInputStream(bufferOut.toByteArray());
     }
 
-    //  private URL createURL(HttpRequest request) throws MalformedURLException{
     private URL createURL(Request request) throws MalformedURLException {
         URL url = null;
 
         try {
-            url = new URL(this.src);
+            url = new URL(this.source);
             log.debug(".createURL(): " + url);
         } catch (MalformedURLException e) {
-            //log.warn(".createURL(): "+e);
-            url = new URL("http://127.0.0.1:" + request.getServerPort() + this.src);
+            url = new URL("http://127.0.0.1:" + request.getServerPort() + this.source);
 
             //FIXME
-            //url=new URL("http://192.168.0.219:"+request.getServerPort()+this.src);
             log.debug(".createURL(): Add localhost and port: " + url);
         }
 
