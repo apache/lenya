e232d58dff717b9d04c8ed2b514a96b4e5fdae86
Andreas Hartmann <andreas@apache.org>
2003-04-03 19:18:59 +0000

    implemented executeAntTarget()
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39122 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/task/AntTask.java
+++ src/java/org/apache/lenya/cms/task/AntTask.java
@@ -48,8 +48,20 @@ package org.lenya.cms.task;
 import org.lenya.cms.task.ExecutionException;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Iterator;
 
 import java.util.Map;
+import org.apache.avalon.framework.parameters.ParameterException;
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.ProjectHelper;
+import org.apache.tools.ant.XmlLogger;
+import org.lenya.cms.publishing.PublishingEnvironment;
+import org.lenya.util.NamespaceMap;
 
 
 /**
@@ -58,9 +70,11 @@ import java.util.Map;
  *
  * <ul>
  *   <li><code><strong>publication-id</strong></code>: the publication ID</li>
- *   <li><code><strong>buildfile</strong></code>: the location of the build file
- *       relative to the publication directory</li>
- *   <li><code><strong>target</strong></code>: the build target</li>
+ *   <li><code><strong>buildfile</strong> (optional)</code>: the location of the build file
+ *       relative to the publication directory. If this parameter is
+ *       not provided, the file is loaded from {@link #DEFAULT_BUILDFILE}.</li>
+ *   <li><code><strong>target</strong> (optional)</code>: the build target. If this parameter
+         is not provided, the default target is executed.</li>
  *   <li><code><strong>ant.*</strong></code>: the command-line parameters for Ant</li>
  *   <li><code><strong>properties.*</strong></code>: the project properties</li>
  * </ul>
@@ -79,8 +93,62 @@ public class AntTask
      * @param properties A map mapping the project properties to their values.
      */
     public void executeAntTarget(File buildFile, String target, Map arguments,
-        Map properties) {
+        Map properties) throws ExecutionException {
+            
+        Project project = new Project();
+        project.setCoreLoader(getClass().getClassLoader());
+        
+        Throwable error = null;
+        
+        try {
+            File logFile = new File(buildFile.getParentFile(), "ant-log.xml");
+            project.setUserProperty("XmlLogger.file", logFile.getAbsolutePath());
+            XmlLogger logger = new XmlLogger();
+            project.addBuildListener(logger);
+            project.fireBuildStarted();
+            /*
+            try {
+                File logFile = new File(buildFile.getParentFile(), "ant-log.xml");
+                logFile.createNewFile();
+                logger.setOutputPrintStream(new PrintStream(new FileOutputStream(logFile)));
+            }
+            catch (Exception e) {
+                throw new ExecutionException("Could not create log file: " + e);
+            }
+             */
+            
+            project.init();
+            project.setBaseDir(buildFile.getParentFile());
+            ProjectHelper helper = ProjectHelper.getProjectHelper();
+            helper.parse(project, buildFile);
+            
+            for (Iterator keys = properties.keySet().iterator(); keys.hasNext(); ) {
+                String key = (String) keys.next();
+                project.setUserProperty(key, (String) properties.get(key));
+            }
+            
+            if (target == null) {
+                target = project.getDefaultTarget();
+            }
+            
+            project.executeTarget(target);
+            
+        }
+        catch (BuildException e) {
+            error = e;
+            throw new ExecutionException(e);
+        }
+        finally {
+            project.fireBuildFinished(error);
+        }
+        
     }
+    
+    public static final String BUILDFILE = "buildfile";
+    public static final String TARGET = "target";
+    public static final String ANT_PREFIX = "ant";
+    public static final String PROPERTIES_PREFIX = "properties";
+    public static final String DEFAULT_BUILDFILE = "config/tasks.xml";
 
     /**
      * Execute the task. All parameters must have been set with parameterize().
@@ -90,5 +158,36 @@ public class AntTask
      * @throws ExecutionException when the execution of the task failed.
      */
     public void execute(String servletContextPath) throws ExecutionException {
+        
+        File buildFile;
+        String target;
+        Map arguments;
+        Map properties;
+        
+        try {
+            String publicationId = getParameters().getParameter(PARAMETER_PUBLICATION_ID);
+
+            String relativeBuildFilePath = getParameters().getParameter("buildfile", DEFAULT_BUILDFILE);
+            relativeBuildFilePath = relativeBuildFilePath.replace('/', File.separatorChar);
+
+            PublishingEnvironment environment
+                = new PublishingEnvironment(servletContextPath, publicationId);
+            File publicationDirectory = environment.getPublicationDirectory();
+            buildFile = new File(publicationDirectory, relativeBuildFilePath);
+
+            target = getParameters().getParameter(TARGET, null);
+
+            Map parametersMap = Parameters.toProperties(getParameters());
+            NamespaceMap antMap = new NamespaceMap(parametersMap, ANT_PREFIX);
+            arguments = antMap.getMap();
+            NamespaceMap propertiesMap = new NamespaceMap(parametersMap, PROPERTIES_PREFIX);
+            properties = propertiesMap.getMap();
+        }
+        catch (ParameterException e) {
+            throw new ExecutionException(e);
+        }
+        
+        executeAntTarget(buildFile, target, arguments, properties);
+        
     }
 }
