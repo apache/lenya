5cd8452537492f0bd589c50cf31de5bfe6d833f7
Gregor Rothfuss <gregor@apache.org>
2003-04-11 16:12:50 +0000

    menu code supports menus now :)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39231 13f79535-47bb-0310-9956-ffa450edef68

--- src/webapp/lenya/resources/misc/menu2/menu.js
+++ src/webapp/lenya/resources/misc/menu2/menu.js
@@ -9,39 +9,48 @@ function LUI() {
 
 
 LUI.prototype = {
+  _activeMenu:null,
   init:function() {
     //internal variables
     this._menurow = document.getElementById('menurow');
     this._page = document.getElementById('page');
     this._visibility = false;
-    this._selTabID = 'Admin';
+    this._selTabID = 'Authoring';
 
     //create tabs
     this._tabs = document.getElementById('tabs');
     var tabTplStr = document.getElementById('tabTemplate').innerHTML;
     var s = '';
     for (var i=0; i<tabDef.length; i++) {
-      var name = tabDef[i].name;
-      var selected = ((this._selTabID==tabDef[i].name)?'selected':'');
-      var icon = (tabDef[i].icon?tabDef[i].icon:'/lenya/lenya/menu2/media/nix.gif');
-      var href = tabDef[i].href;
-      s += tabTplStr.replace(/\$name/g, name).replace(/\$selected/g, selected).replace(/\$icon/g, icon).replace(/\$href/g, href);
+      var hash = {
+        name:tabDef[i].name,
+        selected:((this._selTabID==tabDef[i].name)?'selected':''),
+        icon:(tabDef[i].icon?tabDef[i].icon:'/lenya/lenya/menu2/media/nix.gif'),
+        href:tabDef[i].href
+      };
+      s += this._replaceHash(tabTplStr, hash);
     }
     this._tabs.innerHTML = s;
 
     //create menubar
-    this._menubar = document.getElementById('menucontent');
-    var menuTplStr = document.getElementById('menuTemplate').innerHTML;
+    this._menubar = document.getElementById('menubar');
+    var menubuttonTplStr = document.getElementById('menubuttonTemplate').innerHTML;
     var s = '';
     for (var i=0; i<menubarDef.length; i++) {
-      var name = menubarDef[i].name;
-      var icon = (menubarDef[i].icon?menubarDef[i].icon:'/lenya/lenya/menu2/media/nix.gif');
-      s += menuTplStr.replace(/\$name/g, name).replace(/\$icon/g, icon);
+      var hash = {
+        name:menubarDef[i].name,
+        icon:(menubarDef[i].icon?menubarDef[i].icon:'/lenya/lenya/menu2/media/nix.gif')
+      };
+      s += this._replaceHash(menubuttonTplStr, hash);
     }
     this._menubar.innerHTML = s;
 
     //actions
     this.showMenu();
+
+    //attach onclick handler
+    if (document.all) document.body.attachEvent('onclick', function() { lui._doClickBody(window.event); });
+    else document.body.addEventListener('click', function(evt) { lui._doClickBody(evt); }, false);
   },
   showMenu:function() {
     this._setMenuVisibility(true);
@@ -82,8 +91,63 @@ LUI.prototype = {
 
     if (tabHref) window.frames['page'].location = tabHref;
   },
-  _doClickMenu:function(menuID) {
-alert('we should now open the menu having id ' + menuID);
+  _doClickMenuButton:function(menuID) {
+    //find menu definition
+    var menuDef = null;
+    for (var i=0; i<menubarDef.length; i++) {
+      if (menubarDef[i].name == menuID) {
+        menuDef = menubarDef[i];
+        break;
+      }
+    }
+    if (!menuDef) return;
+
+    //create menu
+    var s = '';
+    var menuitemTplStr = document.getElementById('menuitemTemplate').innerHTML;
+    for (var i=0; i< menuDef.items.length; i++) {
+      var hash = {
+        name:menuDef.items[i].name,
+        href:menuDef.items[i].href
+      };
+      s += this._replaceHash(menuitemTplStr, hash);
+    }
+
+    //draw the menu
+    var menuEl = document.getElementById('menu_' + menuID);
+    menuEl.innerHTML = s;
+    this._showMenu(menuEl);
+  },
+  
+  _replaceHash:function(s, hash) {
+    for (var n in hash) s = s.replace(new RegExp('\\$' + n, 'g'), hash[n]);
+    return s;
+  },
+  _doClickMenuItem:function(menuitemID, href) {
+    this._hideMenu();
+    window.frames['page'].location = href;
+  },
+  _showMenu:function(menuEl) {
+    this._hideMenu();
+    menuEl.className = 'menuvisible';
+    menuEl.style.display = 'block';
+    this._activeMenu = menuEl;
+    document.getElementById('mask').style.display = 'block';
+  },
+  _hideMenu:function() {
+    if (this._activeMenu) {
+      this._activeMenu.style.display = 'none';
+      this._activeMenu = null;
+    }
+    document.getElementById('mask').style.display = 'none';
+  },
+  _doClickBody:function(evt) {
+    if (!this._activeMenu) return;
+    var el = evt.srcElement || evt.target;
+    while (el && (el.id != 'menurow')) el = el.parentNode;
+    if (el) return;
+
+    this._hideMenu();
   }
 }
 
