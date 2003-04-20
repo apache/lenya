1aeca59db26be7654fb5e022371b349619e56479
Michael Wechner <michi@apache.org>
2003-04-20 21:14:30 +0000

    first useful junit test suite
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39303 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/lenya/cms/ac/IdentityTestCase.java
+++ src/test/org/apache/lenya/cms/ac/IdentityTestCase.java
@@ -1,165 +1,76 @@
-/*
-
- ============================================================================
-                   The Apache Software License, Version 1.1
- ============================================================================
-
- Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
-
- Redistribution and use in source and binary forms, with or without modifica-
- tion, are permitted provided that the following conditions are met:
-
- 1. Redistributions of  source code must  retain the above copyright  notice,
-    this list of conditions and the following disclaimer.
-
- 2. Redistributions in binary form must reproduce the above copyright notice,
-    this list of conditions and the following disclaimer in the documentation
-    and/or other materials provided with the distribution.
-
- 3. The end-user documentation included with the redistribution, if any, must
-    include  the following  acknowledgment:  "This product includes  software
-    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
-    Alternately, this  acknowledgment may  appear in the software itself,  if
-    and wherever such third-party acknowledgments normally appear.
-
- 4. The names "Apache Cocoon" and  "Apache Software Foundation" must  not  be
-    used to  endorse or promote  products derived from  this software without
-    prior written permission. For written permission, please contact
-    apache@apache.org.
-
- 5. Products  derived from this software may not  be called "Apache", nor may
-    "Apache" appear  in their name,  without prior written permission  of the
-    Apache Software Foundation.
-
- THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
- INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
- APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
- INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
- DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
- OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
- ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
- (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
- THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
- This software  consists of voluntary contributions made  by many individuals
- on  behalf of the Apache Software  Foundation and was  originally created by
- Stefano Mazzocchi  <stefano@apache.org>. For more  information on the Apache
- Software Foundation, please see <http://www.apache.org/>.
-
-*/
 package org.apache.lenya.cms.ac;
 
-import java.io.File;
-import java.io.InputStream;
-import java.util.Properties;
-
-import junit.swingui.TestRunner;
+import junit.framework.Test;
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.textui.TestRunner;
 
-/*
-import org.apache.avalon.excalibur.testcase.ExcaliburTestCase;
-import org.apache.avalon.framework.activity.Disposable;
-import org.apache.avalon.framework.activity.Initializable;
-import org.apache.avalon.framework.context.DefaultContext;
-import org.apache.cocoon.Constants;
-import org.apache.cocoon.environment.commandline.CommandlineContext;
-import org.apache.cocoon.util.IOUtils;
-import org.apache.excalibur.xml.EntityResolver;
-import org.xml.sax.InputSource;
-*/
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
 
+import java.io.FileInputStream;
+
+import org.w3c.dom.Document;
 
 import org.lenya.cms.ac.Identity;
 
 /**
- * A test case for cms/ac/Identity
+ * A test case testing org.lenya.cms.ac.Identity
  *
  * @author <a href="mailto:gregor@apache.org">Gregor J. Rothfuss</a>
  * @author <a href="mailto:michi@apache.org">Michael Wechner</a>
- * @version CVS $Id: IdentityTestCase.java,v 1.2 2003/04/17 16:47:06 michi Exp $
+ * @version CVS $Id: IdentityTestCase.java,v 1.3 2003/04/20 21:14:30 michi Exp $
  */
-public final class IdentityTestCase extends TestCase
-//public final class IdentityTestCase extends ExcaliburTestCase implements Initializable, Disposable
-{
+public final class IdentityTestCase extends TestCase {
+    private Identity identity = null;
+    private Document doc = null;
 
     /**
-     * Constructor for the IdentityTestCase object
+     * The main program for the IdentityTestCase class
      *
-     * @since
+     * @param args The command line arguments
      */
-    public IdentityTestCase() {
-        //this("IdentityTestCase");
-    }
-
-
-    /**
-     * The main program for the IdentityTestCase class.
-     *
-     * @param  args           The command line arguments
-     * @exception  Exception  Description of Exception
-     * @since
-     */
-    public static void main(final String[] args) throws Exception {
-        final String[] testCaseName = {IdentityTestCase.class.getName()};
-        TestRunner.main(testCaseName);
-
-        //Identity id = new Identity("asda", null);
+    public static void main(String[] args) {
+        TestRunner.run(suite());
     }
 
     /**
-     * The JUnit setup method. Lookup the resolver role.
      *
-     * @exception  Exception  Description of Exception
-     * @since
      */
-    public void setUp() throws Exception {
-        super.setUp();
+    public static Test suite() {
+        TestSuite suite = new TestSuite();
+        suite.addTest(new IdentityTestCase("testUsername"));
+        suite.addTest(new IdentityTestCase("testEncryptedPassword"));
+        return suite;
     }
 
     /**
-     * Invoked before components are created.
      *
-     * @exception  Exception  Description of Exception
-     * @since
      */
-    public void initialize() throws Exception {
-    }
-
-    /**
-     * Invoked after components have been disposed.
-     *
-     * @since
-     */
-    public void dispose() {
-    }
-
-    /**
-     * The teardown method for JUnit
-     *
-     * @exception  Exception  Description of Exception
-     * @since
-     */
-    public void tearDown() throws Exception {
-        super.tearDown();
-
-/*
-        if (Identity != null) {
-//            manager.release(Identity);
-            Identity = null;
+     public IdentityTestCase(String test) {
+         super(test);
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            doc = db.parse(new FileInputStream("/home/michi/src/lenya/src/webapp/lenya/pubs/oscom/content/ac/passwd/lenya.iml"));
+            identity = new Identity(doc);
+        } catch (Exception e) {
+            System.err.println(e);
         }
-*/
-    }
-
+     }
 
     /**
-     * JUnit test case:
-     * Ask for an entity using deliberately non-existent publicId and systemId
-     *
-     * @exception  Exception  Description of Exception
-     * @since
+     * Test if username exists
      */
-    public void testGetPassword() throws Exception {
+    public void testUsername() {
+        assertTrue(identity.getUsername().equals("lenya"));
+    }
+
+    /**
+     * Test if encrypted password exists
+     */
+    public void testEncryptedPassword() throws Exception {
+        assertTrue(identity.getPassword(doc).equals("8e07dafd13495561db9063ebe4db4b27"));
     }
 }
 
