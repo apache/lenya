3ccbfd6eaf06f91cb8feddadba83cd8d9a92547e
Chevrier Edith <edith@apache.org>
2003-05-14 13:53:57 +0000

    Ant Task Publisher for default publication
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39573 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/ant/TreePublisher.java
+++ src/java/org/apache/lenya/cms/ant/TreePublisher.java
@@ -1,18 +1,58 @@
 /*
- * TreePublisher.java
+ * $Id: TreePublisher.java,v 1.2 2003/05/14 13:53:57 edith Exp $
+ * <License>
+ * The Apache Software License
  *
- * Created on 7. Mai 2003, 18:00
+ * Copyright (c) 2002 lenya. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ *    list of conditions and the following disclaimer in the documentation and/or
+ *    other materials provided with the distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this software must
+ *    display the following acknowledgment: "This product includes software developed
+ *    by lenya (http://www.lenya.org)"
+ *
+ * 4. The name "lenya" must not be used to endorse or promote products derived from
+ *    this software without prior written permission. For written permission, please
+ *    contact contact@lenya.org
+ *
+ * 5. Products derived from this software may not be called "lenya" nor may "lenya"
+ *    appear in their names without prior written permission of lenya.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following acknowledgment:
+ *    "This product includes software developed by lenya (http://www.lenya.org)"
+ *
+ * THIS SOFTWARE IS PROVIDED BY lenya "AS IS" WITHOUT ANY WARRANTY EXPRESS OR IMPLIED,
+ * INCLUDING THE WARRANTY OF NON-INFRINGEMENT AND THE IMPLIED WARRANTIES OF MERCHANTI-
+ * BILITY AND FITNESS FOR A PARTICULAR PURPOSE. lenya WILL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY YOU AS A RESULT OF USING THIS SOFTWARE. IN NO EVENT WILL lenya BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR LOST PROFITS EVEN IF lenya HAS
+ * BEEN ADVISED OF THE POSSIBILITY OF THEIR OCCURRENCE. lenya WILL NOT BE LIABLE FOR ANY
+ * THIRD PARTY CLAIMS AGAINST YOU.
+ *
+ * Lenya includes software developed by the Apache Software Foundation, W3C,
+ * DOM4J Project, BitfluxEditor and Xopus.
+ * </License>
  */
-
 package org.apache.lenya.cms.ant;
 
 import org.apache.lenya.cms.publishing.PublishingException;
+import org.apache.lenya.cms.publication.DefaultSiteTree;
+import org.apache.lenya.cms.publication.SiteTreeNode;
+
 import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.Task;
 
 /**
+ * Ant task to publish the tree, adding a node for 
+ * the new published document in the live tree
  *
- * @author  edith
  */
 public class TreePublisher
     extends PublicationTask {
@@ -21,40 +61,96 @@ public class TreePublisher
     public TreePublisher() {
     }
     
-    private String absoluteTreeLivePath;
+    private String documentid;
 
-    protected String getAbsoluteTreeLivePath() {
-        return absoluteTreeLivePath;
+    /**
+     * Returns the document id
+     */
+    protected String getDocumentid() {
+        return documentid;
+    }
+
+    /**
+     * Sets the document id
+     */
+    public void setDocumentid(String documentid) {
+        this.documentid = documentid;
     }
     
-    public void setAbsoluteTreeLivePath(String absoluteTreeLivePath) {
-        this.absoluteTreeLivePath = absoluteTreeLivePath;
+    private String absolutetreeauthoringpath;
+    
+    /**
+     * Returns the absolute authoring path for the tree
+     */
+    protected String getAbsolutetreeauthoringpath() {
+        return absolutetreeauthoringpath;
     }
     
-    private String absoluteTreeAuthoringPath;
-    
-    protected String getAbsoluteTreeAuthoringPath() {
-        return absoluteTreeAuthoringPath;
+    /**
+     * Sets the absolute authoring path for the tree
+     */
+    public void setAbsolutetreeauthoringpath(String absolutetreeauthoringpath) {
+        this.absolutetreeauthoringpath = absolutetreeauthoringpath;
     }
     
-    protected void setAbsoluteTreeAuthoringPath() {
-        this.absoluteTreeAuthoringPath = absoluteTreeAuthoringPath;
+    private String absolutetreelivepath;
+
+    /**
+     * Returns the absolute live path for the tree
+     */
+    protected String getAbsolutetreelivepath() {
+        return absolutetreelivepath;
     }
-    
-    public void publish(String absoluteTreeAuthoringPath, String absoluteTreeLivePath)
+
+    /**
+     * Sets the absolute live path for the tree
+     */
+    public void setAbsolutetreelivepath(String absolutetreelivepath) {
+        this.absolutetreelivepath= absolutetreelivepath;
+    }
+
+    /**
+     * adds a node for the published document in the live tree 
+     * @param documentid The id of the published document
+     * @param absolutetreeauthoringpath The path for the tree in authoring
+     * @param absolutetreelivepath The path for the tree in live
+     */
+    public void publish(String documentid, String absolutetreeauthoringpath, String absolutetreelivepath)
         throws PublishingException {
+
+      DefaultSiteTree authoringTree = null;
+      DefaultSiteTree liveTree = null;
+
+      try {
+        authoringTree = new DefaultSiteTree(absolutetreeauthoringpath);
+        liveTree = new DefaultSiteTree(absolutetreelivepath);
+        SiteTreeNode documentNode = authoringTree.getNode(documentid);
+        if (documentNode!= null) {
+          liveTree.addNode(documentNode); 
+        } else {
+          throw new PublishingException("No node found for the document "+documentid);
+        } 
+        liveTree.serialize(); 
+      } catch (Exception e) {
+        throw new PublishingException("Couldn't publish the tree :"+ absolutetreelivepath , e);
+      }
     }
     
+    /**
+     * Executes the task
+     */
     public void execute()
         throws BuildException {
             
         try {
-            log("Absolute Tree Authoring Path: " + getAbsoluteTreeAuthoringPath());
-            log("Absolute Tree Live Path: " + getAbsoluteTreeLivePath());
+            log("document id: " + getDocumentid());
+            log("Absolute Tree Authoring Path: " + getAbsolutetreeauthoringpath());
+            log("Absolute Tree Live Path: " + getAbsolutetreelivepath());
 
             publish (
-                getAbsoluteTreeAuthoringPath(),
-                getAbsoluteTreeLivePath());
+                getDocumentid(), 
+                getAbsolutetreeauthoringpath(),
+                getAbsolutetreelivepath());
         } catch (Exception e) {
             throw new BuildException(e);
         }
