09365fc83cf2247cbd306c55684d7bbfdf456584
Andreas Hartmann <andreas@apache.org>
2003-05-28 15:28:08 +0000

    formatting changed added toString()
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39710 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/workflow/impl/TransitionImpl.java
+++ src/java/org/apache/lenya/cms/workflow/impl/TransitionImpl.java
@@ -19,64 +19,64 @@ import org.apache.lenya.cms.workflow.Transition;
  *
  * @author  andreas
  */
-public class TransitionImpl
-    implements Transition {
-    
+public class TransitionImpl implements Transition {
+
     /** Creates a new instance of TransitionImpl */
     public TransitionImpl(State source, State destination) {
-        
+
         assert source != null;
         assert destination != null;
-        
+
         this.source = source;
         this.destination = destination;
     }
-    
+
     private List actions = new ArrayList();
-    
+
     public Action[] getActions() {
         return (Action[]) actions.toArray(new Action[actions.size()]);
     }
-    
+
     public void addAction(Action action) {
         assert action != null;
         actions.add(action);
     }
-    
+
     private List conditions = new ArrayList();
-    
+
     public Condition[] getConditions() {
-        return (Condition[]) conditions.toArray(new Condition[conditions.size()]);
+        return (Condition[]) conditions.toArray(
+            new Condition[conditions.size()]);
     }
-    
+
     public void addCondition(Condition condition) {
         assert condition != null;
         conditions.add(condition);
     }
-    
+
     private Event event;
-    
+
     public Event getEvent() {
         return event;
     }
-    
+
     public void setEvent(Event event) {
         this.event = event;
         assert event != null;
     }
-    
+
     private State source;
-    
+
     public State getSource() {
         return source;
     }
-    
+
     private State destination;
-    
+
     public State getDestination() {
         return destination;
     }
-    
+
     /** Returns if the transition can fire in a certain situation.
      *
      */
@@ -90,5 +90,31 @@ public class TransitionImpl
         }
         return canFire;
     }
-    
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#toString()
+     */
+    public String toString() {
+        
+        String string = getEvent().getCommand() + " [";
+        Condition conditions[] = getConditions();
+        for (int i = 0; i < conditions.length; i++) {
+            if (i > 0) string += ", ";
+            string += conditions[i].toString();
+        }
+        
+        string += "]";
+        
+        Action actions[] = getActions();
+        if (actions.length > 0) {
+            string += " / ";
+            for (int i = 0; i < actions.length; i++) {
+                if (i > 0) string += ", ";
+                string += actions[i].toString();
+            }
+        }
+        
+        return string;
+    }
+
 }
