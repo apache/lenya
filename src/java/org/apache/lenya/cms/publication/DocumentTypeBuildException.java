47c50d9e9e41580a9a9a2e0799f0a3a784898490
Andreas Hartmann <andreas@apache.org>
2003-05-28 14:47:26 +0000

    completed Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39696 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/publication/DocumentTypeBuildException.java
+++ src/java/org/apache/lenya/cms/publication/DocumentTypeBuildException.java
@@ -1,27 +1,83 @@
 /*
- * DocumentTypeBuildException.java
+ * $Id
+ * <License>
+ * The Apache Software License
  *
- * Created on 9. April 2003, 10:12
+ * Copyright (c) 2002 lenya. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ *    list of conditions and the following disclaimer in the documentation and/or
+ *    other materials provided with the distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this software must
+ *    display the following acknowledgment: "This product includes software developed
+ *    by lenya (http://www.lenya.org)"
+ *
+ * 4. The name "lenya" must not be used to endorse or promote products derived from
+ *    this software without prior written permission. For written permission, please
+ *    contact contact@lenya.org
+ *
+ * 5. Products derived from this software may not be called "lenya" nor may "lenya"
+ *    appear in their names without prior written permission of lenya.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following acknowledgment:
+ *    "This product includes software developed by lenya (http://www.lenya.org)"
+ *
+ * THIS SOFTWARE IS PROVIDED BY lenya "AS IS" WITHOUT ANY WARRANTY EXPRESS OR IMPLIED,
+ * INCLUDING THE WARRANTY OF NON-INFRINGEMENT AND THE IMPLIED WARRANTIES OF MERCHANTI-
+ * BILITY AND FITNESS FOR A PARTICULAR PURPOSE. lenya WILL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY YOU AS A RESULT OF USING THIS SOFTWARE. IN NO EVENT WILL lenya BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR LOST PROFITS EVEN IF lenya HAS
+ * BEEN ADVISED OF THE POSSIBILITY OF THEIR OCCURRENCE. lenya WILL NOT BE LIABLE FOR ANY
+ * THIRD PARTY CLAIMS AGAINST YOU.
+ *
+ * Lenya includes software developed by the Apache Software Foundation, W3C,
+ * DOM4J Project, BitfluxEditor and Xopus.
+ * </License>
  */
-
 package org.apache.lenya.cms.publication;
 
 /**
+ * @author andreas
  *
- * @author  andreas
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
  */
-public class DocumentTypeBuildException
-    extends Exception {
-    
-    /** Creates a new instance of DocumentTypeBuildException */
+public class DocumentTypeBuildException extends Exception {
+
+    /**
+     * 
+     */
     public DocumentTypeBuildException() {
-        super(MESSAGE);
+        super();
     }
-    
-    public DocumentTypeBuildException(Throwable cause) {
-        super(MESSAGE, cause);
+
+    /**
+     * @param arg0
+     */
+    public DocumentTypeBuildException(String arg0) {
+        super(arg0);
     }
-    
-    public static final String MESSAGE = "Failed to build document type: ";
-    
+
+    /**
+     * @param arg0
+     * @param arg1
+     */
+    public DocumentTypeBuildException(String arg0, Throwable arg1) {
+        super(arg0, arg1);
+    }
+
+    /**
+     * @param arg0
+     */
+    public DocumentTypeBuildException(Throwable arg0) {
+        super(arg0);
+    }
+
 }
