4d19a88fd658513562c8fd812dd82669246efa1c
Andreas Hartmann <andreas@apache.org>
2003-05-28 15:02:52 +0000

    added some new workflow classes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39702 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/workflow/BooleanVariableInstance.java
@@ -0,0 +1,26 @@
+/*
+ * BooleanVariableInstance.java
+ *
+ * Created on 27. Mai 2003, 12:36
+ */
+
+package org.apache.lenya.cms.workflow;
+
+/**
+ *
+ * @author  andreas
+ */
+public interface BooleanVariableInstance {
+
+    /**
+     * Sets the value of this variable.
+     * @param value A boolean value.
+     */
+    void setValue(boolean value);
+
+    /**
+     * Returns the value of this variable.
+     * @return A boolean value.
+     */
+    boolean getValue();
+}
---
+++ src/java/org/apache/lenya/cms/workflow/WorkflowBuildException.java
@@ -0,0 +1,37 @@
+/*
+ * WorkflowBuildException.java
+ *
+ * Created on 8. April 2003, 20:01
+ */
+
+package org.apache.lenya.cms.workflow;
+
+/**
+ *
+ * @author  andreas
+ */
+public class WorkflowBuildException
+    extends Exception {
+        
+    /** Creates a new instance of WorkflowBuildException */
+    public WorkflowBuildException() {
+        super(MESSAGE);
+    }
+    
+    /** Creates a new instance of WorkflowBuildException */
+    public WorkflowBuildException(String message) {
+        super(message);
+    }
+    
+    public WorkflowBuildException(Throwable cause) {
+        super(MESSAGE, cause);
+    }
+    
+    public WorkflowBuildException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    protected static final String MESSAGE = "Workflow building failed: ";
+    
+    
+}
