627b032481168497f20c0f20f612b9ce188b0930
Andreas Hartmann <andreas@apache.org>
2003-05-28 15:03:30 +0000

    added workflow UI classes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39703 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/workflow/ui/CommandFilterImpl.java
@@ -0,0 +1,43 @@
+/*
+ * Created on 27.05.2003
+ *
+ * To change the template for this generated file go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+package org.apache.lenya.cms.workflow.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lenya.cms.workflow.Situation;
+import org.apache.lenya.cms.workflow.Transition;
+import org.apache.lenya.cms.workflow.WorkflowInstance;
+
+/**
+ * @author andreas
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class CommandFilterImpl implements CommandFilter {
+
+    /* (non-Javadoc)
+     * @see org.apache.lena.cms.menu.MenuFilter#getExecutableItems(org.apache.lenya.cms.workflow.Situation)
+     */
+    public String[] getExecutableCommands(
+        WorkflowInstance instance,
+        Situation situation) {
+
+        List commands = new ArrayList();
+
+        Transition transitions[] = instance.getExecutableTransitions(situation);
+        for (int i = 0; i < transitions.length; i++) {
+            String command = transitions[i].getEvent().getCommand();
+            commands.add(command);
+        }
+
+        return (String[]) commands.toArray(new String[commands.size()]);
+
+    }
+
+}
