85df45ecea863eb557cc42022450a42772518cbc
Andreas Hartmann <andreas@apache.org>
2003-06-02 17:25:00 +0000

    - using HashMap to store publications - added getPublication(String, String) method
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39781 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/publication/PublicationFactory.java
+++ src/java/org/apache/lenya/cms/publication/PublicationFactory.java
@@ -6,6 +6,7 @@
 
 package org.apache.lenya.cms.publication;
 
+import java.util.HashMap;
 import java.util.Map;
 import org.apache.cocoon.environment.Context;
 import org.apache.cocoon.environment.ObjectModelHelper;
@@ -23,6 +24,8 @@ public final class PublicationFactory {
     
     private static Category log = Category.getInstance(PublicationFactory.class);
     
+    private static Map idToPublication = new HashMap();
+    
     /**
      * Creates a new publication.
      * The publication ID is resolved from the request URI.
@@ -30,6 +33,7 @@ public final class PublicationFactory {
      * @param objectModel The object model of the Cocoon component.
      */
     public static Publication getPublication(Map objectModel) {
+        assert objectModel != null;
         
         Request request = ObjectModelHelper.getRequest(objectModel);
         String uri = request.getRequestURI();
@@ -47,7 +51,23 @@ public final class PublicationFactory {
         
         Context context = ObjectModelHelper.getContext(objectModel);
         String servletContextPath = context.getRealPath("");
-        return new Publication(publicationId, servletContextPath);
+        
+        return getPublication(publicationId, servletContextPath);
+    }
+    
+    public static Publication getPublication(String id, String servletContextPath) {
+        assert id != null;
+        assert servletContextPath != null;
+        Publication publication;
+        if (idToPublication.containsKey(id)) {
+            publication = (Publication) idToPublication.get(id);
+        }
+        else {
+            publication = new Publication(id, servletContextPath);
+            idToPublication.put(id, publication);
+        }
+        assert publication != null;
+        return publication;
     }
     
 }
