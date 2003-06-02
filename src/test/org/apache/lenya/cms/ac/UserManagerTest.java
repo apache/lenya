1b51dd8f7a886cd8d9f2887aea80f87170cf634b
Christian Egli <egli@apache.org>
2003-06-02 09:30:42 +0000

    Added skeletal versions of Unit Test cases.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39774 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/lenya/cms/ac/UserManagerTest.java
@@ -0,0 +1,114 @@
+/*
+ * $Id: UserManagerTest.java,v 1.1 2003/06/02 09:30:42 egli Exp $
+ * <License>
+ * The Apache Software License
+ *
+ * Copyright (c) 2003 Wyona. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgment: "This product
+ *    includes software developed by Wyona (http://www.wyona.com)"
+ *
+ * 4. The name "Lenya" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact contact@wyona.com
+ *
+ * 5. Products derived from this software may not be called "Lenya" nor
+ *    may "Lenya" appear in their names without prior written permission
+ *    of Wyona.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment: "This product includes software developed by Wyona
+ *    (http://www.wyona.com)"
+ *
+ * THIS SOFTWARE IS PROVIDED BY Wyona "AS IS" WITHOUT ANY WARRANTY EXPRESS
+ * OR IMPLIED, INCLUDING THE WARRANTY OF NON-INFRINGEMENT AND THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * Wyona WILL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY YOU AS A RESULT
+ * OF USING THIS SOFTWARE. IN NO EVENT WILL lenya BE LIABLE FOR ANY SPECIAL,
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OR LOST PROFITS EVEN IF Wyona HAS BEEN
+ * ADVISED OF THE POSSIBILITY OF THEIR OCCURRENCE. Wyona WILL NOT BE LIABLE
+ * FOR ANY THIRD PARTY CLAIMS AGAINST YOU.
+ *
+ * Lenya includes software developed by the Apache Software Foundation, W3C,
+ * DOM4J Project, BitfluxEditor and Xopus.
+ * </License>
+ */
+ 
+package org.apache.lenya.cms.ac;
+
+import org.apache.lenya.cms.publication.Publication;
+
+import junit.framework.TestCase;
+
+/**
+ * @author egli
+ * 
+ * 
+ */
+public class UserManagerTest extends TestCase {
+
+	/**
+	 * Constructor for UserManagerTest.
+	 * @param arg0
+	 */
+	public UserManagerTest(String arg0) {
+		super(arg0);
+	}
+
+	public static void main(String[] args) {
+		junit.textui.TestRunner.run(UserManagerTest.class);
+	}
+
+	/*
+	 * @see TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/*
+	 * @see TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	final public void testInstance() {
+		Publication publication = new Publication("default", "/home/egli/build/jakarta-tomcat-4.1.21-LE-jdk14/webapps/lenya/");
+		UserManager manager = null;
+		try {
+			manager = (UserManager)UserManager.instance(publication);
+		} catch (AccessControlException e) {}
+		assertNotNull(manager);
+	}
+
+	final public void testGetUsers() {
+		//TODO Implement getUsers().
+	}
+
+	final public void testAdd() {
+		//TODO Implement add().
+	}
+
+	final public void testRemove() {
+		//TODO Implement remove().
+	}
+
+	final public void testGetUser() {
+		//TODO Implement getUser().
+	}
+
+}
