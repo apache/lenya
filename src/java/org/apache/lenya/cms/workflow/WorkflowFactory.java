4332318f3f580b85a6e06bdc417522f8b29b93a8
Andreas Hartmann <andreas@apache.org>
2003-06-02 17:29:11 +0000

    added WorkflowFactory
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39787 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/workflow/WorkflowFactory.java
@@ -0,0 +1,110 @@
+/*
+ * WorkflowFactory.java
+ *
+ * Created on 8. April 2003, 18:08
+ */
+
+package org.apache.lenya.cms.workflow;
+
+import java.io.File;
+
+import org.apache.lenya.cms.ac.User;
+import org.apache.lenya.cms.publication.Document;
+import org.apache.lenya.cms.publication.DocumentType;
+import org.apache.lenya.cms.publication.DocumentTypeBuilder;
+import org.apache.lenya.cms.publication.Publication;
+import org.apache.lenya.workflow.*;
+import org.apache.lenya.workflow.Workflow;
+import org.apache.lenya.workflow.impl.WorkflowBuilder;
+import org.apache.lenya.workflow.impl.WorkflowImpl;
+import org.apache.lenya.xml.DocumentHelper;
+import org.apache.lenya.xml.NamespaceHelper;
+import org.w3c.dom.Element;
+
+/**
+ *
+ * @author andreas
+ */
+public class WorkflowFactory {
+
+    public static final String WORKFLOW_DIRECTORY =
+        "config/workflow".replace('/', File.separatorChar);
+
+    /** Creates a new instance of WorkflowFactory */
+    protected WorkflowFactory() {
+    }
+
+    public static WorkflowFactory newInstance() {
+        return new WorkflowFactory();
+    }
+
+    public static final String WORKFLOW_ELEMENT = "workflow";
+    public static final String SRC_ATTRIBUTE = "src";
+    
+    /*
+     * Creates a new workflow instance.
+     */
+    public WorkflowInstance buildInstance(Document document) throws WorkflowException {
+        assert document != null;
+        return new WorkflowDocument(document);
+    }
+
+    protected static Workflow buildWorkflow(Publication publication, DocumentType documentType)
+        throws WorkflowException {
+            
+        assert publication != null;
+        assert documentType != null;
+
+        File doctypesDirectory =
+            new File(publication.getDirectory(), DocumentTypeBuilder.DOCTYPE_DIRECTORY);
+        File doctypeFile = new File(doctypesDirectory, documentType.getName() + ".xml");
+
+        Workflow workflow;
+
+        try {
+            org.w3c.dom.Document xmlDocument = DocumentHelper.readDocument(doctypeFile);
+
+            NamespaceHelper helper =
+                new NamespaceHelper(
+                    DocumentType.NAMESPACE,
+                    DocumentType.DEFAULT_PREFIX,
+                    xmlDocument);
+            Element root = xmlDocument.getDocumentElement();
+
+            Element workflowElement =
+                (Element) root.getElementsByTagNameNS(
+                    DocumentType.NAMESPACE,
+                    WORKFLOW_ELEMENT).item(
+                    0);
+
+            String source = workflowElement.getAttribute(SRC_ATTRIBUTE);
+            assert source != null;
+
+            File publicationDirectory = publication.getDirectory();
+            File workflowDirectory = new File(publicationDirectory, WORKFLOW_DIRECTORY);
+            File workflowFile = new File(workflowDirectory, source);
+
+            workflow = WorkflowBuilder.buildWorkflow(workflowFile);
+        } catch (Exception e) {
+            throw new WorkflowException(e);
+        }
+        return workflow;
+    }
+
+    /* 
+     * Creates a new workflow situation.
+     */
+    public Situation buildSituation(User user) throws WorkflowException {
+        assert user != null;
+        return new CMSSituation(user);
+    }
+    
+    /**
+     * Creates an event object for a given command.
+     */
+    public Event buildEvent(WorkflowInstance instance, String command) throws WorkflowException {
+        WorkflowImpl workflow = (WorkflowImpl) instance.getWorkflow();
+        return workflow.getEvent(command);
+    }
+    
+}
