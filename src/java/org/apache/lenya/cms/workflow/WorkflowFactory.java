5e862d3bd408baee4c0cb509b6f18cee070961bb
Andreas Hartmann <andreas@apache.org>
2003-06-04 09:14:13 +0000

    - added hasWorkflow() - added buildSituation(ObjectModel)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39818 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/workflow/WorkflowFactory.java
+++ src/java/org/apache/lenya/cms/workflow/WorkflowFactory.java
@@ -7,12 +7,20 @@
 package org.apache.lenya.cms.workflow;
 
 import java.io.File;
+import java.util.Map;
 
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.Session;
+import org.apache.lenya.cms.ac.AccessControlException;
+import org.apache.lenya.cms.ac.Identity;
 import org.apache.lenya.cms.ac.User;
+import org.apache.lenya.cms.ac.UserManager;
 import org.apache.lenya.cms.publication.Document;
 import org.apache.lenya.cms.publication.DocumentType;
 import org.apache.lenya.cms.publication.DocumentTypeBuilder;
 import org.apache.lenya.cms.publication.Publication;
+import org.apache.lenya.cms.publication.PublicationFactory;
 import org.apache.lenya.workflow.*;
 import org.apache.lenya.workflow.Workflow;
 import org.apache.lenya.workflow.impl.WorkflowBuilder;
@@ -46,6 +54,17 @@ public class WorkflowFactory {
     public WorkflowInstance buildInstance(Document document) throws WorkflowException {
         assert document != null;
         return new WorkflowDocument(document);
+    }
+    
+    /**
+     * Checks if a workflow is assigned to the document.
+     * This is done by looking for the workflow history file.
+     * @param document The document to check.
+     * @return <code>true</code> if the document has a workflow, <code>false</code> otherwise.
+     */
+    public boolean hasWorkflow(Document document) {
+        File historyFile = WorkflowDocument.getHistoryFile(document);
+        return historyFile.exists();
     }
 
     protected static Workflow buildWorkflow(Publication publication, DocumentType documentType)
@@ -96,6 +115,34 @@ public class WorkflowFactory {
     public Situation buildSituation(User user) throws WorkflowException {
         assert user != null;
         return new CMSSituation(user);
+    }
+    
+    public Situation buildSituation(Map objectModel) throws WorkflowException {
+        Request request = ObjectModelHelper.getRequest(objectModel);
+        Session session = request.getSession(true);
+
+        if (session == null) {
+            throw new WorkflowException("No session object available!");
+        }
+        
+        Identity identity = (Identity) session.getAttribute("org.apache.lenya.cms.ac.Identity");
+        
+        if (identity == null) {
+            throw new WorkflowException("No session object available!");
+        }
+        
+        String username = identity.getUsername();
+        
+        Publication publication = PublicationFactory.getPublication(objectModel);
+        
+        User user;
+        try {
+            user = UserManager.instance(publication).getUser(username);
+        } catch (AccessControlException e) {
+            throw new WorkflowException(e);
+        }
+        
+        return buildSituation(user);
     }
     
 }
