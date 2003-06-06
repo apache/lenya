789d12b4a1075142d763d3e1d13d17a7586a361f
Christian Egli <egli@apache.org>
2003-06-06 13:55:45 +0000

    Added actions for user administration.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39866 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/cocoon/acting/UserAdminAddAction.java
@@ -0,0 +1,131 @@
+/*
+ * $Id: UserAdminAddAction.java,v 1.1 2003/06/06 13:55:45 egli Exp $
+ * <License>
+ * The Apache Software License
+ *
+ * Copyright (c) 2003 Wyona. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgment: "This product
+ *    includes software developed by Wyona (http://www.wyona.com)"
+ *
+ * 4. The name "Lenya" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact contact@wyona.com
+ *
+ * 5. Products derived from this software may not be called "Lenya" nor
+ *    may "Lenya" appear in their names without prior written permission
+ *    of Wyona.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment: "This product includes software developed by Wyona
+ *    (http://www.wyona.com)"
+ *
+ * THIS SOFTWARE IS PROVIDED BY Wyona "AS IS" WITHOUT ANY WARRANTY EXPRESS
+ * OR IMPLIED, INCLUDING THE WARRANTY OF NON-INFRINGEMENT AND THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * Wyona WILL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY YOU AS A RESULT
+ * OF USING THIS SOFTWARE. IN NO EVENT WILL lenya BE LIABLE FOR ANY SPECIAL,
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OR LOST PROFITS EVEN IF Wyona HAS BEEN
+ * ADVISED OF THE POSSIBILITY OF THEIR OCCURRENCE. Wyona WILL NOT BE LIABLE
+ * FOR ANY THIRD PARTY CLAIMS AGAINST YOU.
+ *
+ * Lenya includes software developed by the Apache Software Foundation, W3C,
+ * DOM4J Project, BitfluxEditor and Xopus.
+ * </License>
+ */
+
+package org.apache.lenya.cms.cocoon.acting;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.acting.AbstractAction;
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.SourceResolver;
+import org.apache.lenya.cms.ac.AccessControlException;
+import org.apache.lenya.cms.ac.FileUser;
+import org.apache.lenya.cms.ac.Group;
+import org.apache.lenya.cms.ac.GroupManager;
+import org.apache.lenya.cms.publication.PageEnvelope;
+import org.apache.lenya.cms.publication.Publication;
+
+/**
+ * @author egli
+ * 
+ * 
+ */
+public class UserAdminAddAction
+	extends AbstractAction
+	implements UserAdminActionInterface {
+
+	public Map act(
+		Redirector redirector,
+		SourceResolver resolver,
+		Map objectModel,
+		String source,
+		Parameters parameters)
+		throws Exception {
+
+		Request request = ObjectModelHelper.getRequest(objectModel);
+		PageEnvelope envelope = null;
+		try {
+			envelope = new PageEnvelope(resolver, request);
+		} catch (Exception e) {
+			getLogger().error(e.getMessage(), e);
+			return null;
+		}
+		Map emptyMap = Collections.EMPTY_MAP;
+
+		Publication publication = envelope.getPublication();
+
+		String userId = request.getParameter(USER_ID);
+		String fullName = request.getParameter(FULL_NAME);
+		String email = request.getParameter(EMAIL);
+		String password = request.getParameter(PASSWORD);
+		String confirmPassword = request.getParameter(CONFIRM_PASSWORD);
+
+		if ((password != null) && (!password.equals(confirmPassword))) {
+			getLogger().info("Password not confirmed");
+			return null;
+		}
+
+		GroupManager manager = null;
+		try {
+			manager = GroupManager.instance(publication);
+		} catch (AccessControlException e) {
+			getLogger().error(e.getMessage(), e);
+			return null;
+		}
+		FileUser user =
+			new FileUser(publication, userId, fullName, email, password);
+		String[] groups = request.getParameterValues(GROUP);
+		for (int i = 0; i < groups.length; i++) {
+			Group group = (Group) manager.getGroup(groups[i]);
+			user.addGroup(group);
+		}
+		try {
+			user.save();
+		} catch (AccessControlException e) {
+			getLogger().error(e.getMessage(), e);
+			return null;
+		}
+		return emptyMap;
+	}
+
+}
---
+++ src/java/org/apache/lenya/cms/cocoon/acting/UserAdminDeleteAction.java
@@ -0,0 +1,120 @@
+/*
+ * $Id: UserAdminDeleteAction.java,v 1.1 2003/06/06 13:55:44 egli Exp $
+ * <License>
+ * The Apache Software License
+ *
+ * Copyright (c) 2003 Wyona. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this
+ *    software must display the following acknowledgment: "This product
+ *    includes software developed by Wyona (http://www.wyona.com)"
+ *
+ * 4. The name "Lenya" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact contact@wyona.com
+ *
+ * 5. Products derived from this software may not be called "Lenya" nor
+ *    may "Lenya" appear in their names without prior written permission
+ *    of Wyona.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following
+ *    acknowledgment: "This product includes software developed by Wyona
+ *    (http://www.wyona.com)"
+ *
+ * THIS SOFTWARE IS PROVIDED BY Wyona "AS IS" WITHOUT ANY WARRANTY EXPRESS
+ * OR IMPLIED, INCLUDING THE WARRANTY OF NON-INFRINGEMENT AND THE IMPLIED
+ * WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * Wyona WILL NOT BE LIABLE FOR ANY DAMAGES SUFFERED BY YOU AS A RESULT
+ * OF USING THIS SOFTWARE. IN NO EVENT WILL lenya BE LIABLE FOR ANY SPECIAL,
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OR LOST PROFITS EVEN IF Wyona HAS BEEN
+ * ADVISED OF THE POSSIBILITY OF THEIR OCCURRENCE. Wyona WILL NOT BE LIABLE
+ * FOR ANY THIRD PARTY CLAIMS AGAINST YOU.
+ *
+ * Lenya includes software developed by the Apache Software Foundation, W3C,
+ * DOM4J Project, BitfluxEditor and Xopus.
+ * </License>
+ */
+
+package org.apache.lenya.cms.cocoon.acting;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.acting.AbstractAction;
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.SourceResolver;
+import org.apache.lenya.cms.ac.AccessControlException;
+import org.apache.lenya.cms.ac.User;
+import org.apache.lenya.cms.ac.UserManager;
+import org.apache.lenya.cms.publication.PageEnvelope;
+import org.apache.lenya.cms.publication.Publication;
+
+/**
+ * @author egli
+ * 
+ * 
+ */
+public class UserAdminDeleteAction
+	extends AbstractAction
+	implements UserAdminActionInterface {
+
+	/* (non-Javadoc)
+	 * @see org.apache.cocoon.acting.Action#act(org.apache.cocoon.environment.Redirector, org.apache.cocoon.environment.SourceResolver, java.util.Map, java.lang.String, org.apache.avalon.framework.parameters.Parameters)
+	 */
+	public Map act(
+		Redirector redirector,
+		SourceResolver resolver,
+		Map objectModel,
+		String source,
+		Parameters parameters)
+		throws Exception {
+
+		Request request = ObjectModelHelper.getRequest(objectModel);
+		PageEnvelope envelope = null;
+		try {
+			envelope = new PageEnvelope(resolver, request);
+		} catch (Exception e) {
+			getLogger().error(e.getMessage(), e);
+			return null;
+		}
+		Map emptyMap = Collections.EMPTY_MAP;
+
+		Publication publication = envelope.getPublication();
+
+		String userId = request.getParameter(USER_ID);
+
+		UserManager manager = null;
+		try {
+			manager = UserManager.instance(publication);
+		} catch (AccessControlException e) {
+			getLogger().error(e.getMessage(), e);
+			return null;
+		}
+		User user = manager.getUser(userId);
+
+		try {
+			user.delete();
+		} catch (AccessControlException e) {
+			getLogger().error(e.getMessage(), e);
+			return null;
+		}
+
+		return emptyMap;
+	}
+
+}
