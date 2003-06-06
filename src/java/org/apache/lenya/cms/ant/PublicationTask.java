bd82410043ebc867c5e1e618b9a3cdb08700e6e3
Andreas Hartmann <andreas@apache.org>
2003-06-06 13:28:57 +0000

    added getPublication() added assertString()
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39854 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/ant/PublicationTask.java
+++ src/java/org/apache/lenya/cms/ant/PublicationTask.java
@@ -7,8 +7,12 @@
 package org.apache.lenya.cms.ant;
 
 import java.io.File;
+import java.io.IOException;
 
+import org.apache.lenya.cms.publication.Publication;
+import org.apache.lenya.cms.publication.PublicationFactory;
 import org.apache.lenya.cms.task.AntTask;
+import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 
 /**
@@ -16,32 +20,46 @@ import org.apache.tools.ant.Task;
  * It requires some project parameters that are set by the AntTask.
  * @author  <a href="mailto:andreas@apache.org"/>
  */
-public abstract class PublicationTask
-    extends Task {
-    
+public abstract class PublicationTask extends Task {
+
     /** Creates a new instance of PublicationTask */
     public PublicationTask() {
     }
-    
+
     /**
      * Returns the publication directory.
      */
     protected File getPublicationDirectory() {
         return new File(getProject().getProperty(AntTask.PUBLICATION_DIRECTORY));
     }
-    
+
     /**
      * Returns the publication ID.
      */
     protected String getPublicationId() {
         return getProject().getProperty(AntTask.PUBLICATION_ID);
     }
-    
+
     /**
      * Returns the servlet context (e.g., <code>tomcat/webapp/lenya</code>)
      */
     protected File getServletContext() {
         return new File(getProject().getProperty(AntTask.SERVLET_CONTEXT_PATH));
     }
-    
+
+    protected Publication getPublication()
+        throws BuildException {
+        try {
+            return PublicationFactory.getPublication(
+                getPublicationId(),
+                getServletContext().getCanonicalPath());
+        } catch (IOException e) {
+            throw new BuildException(e);
+        }
+    }
+
+    protected void assertString(String string) {
+        assert string != null && !string.equals(""); 
+    }
+
 }
