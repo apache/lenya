8dc1267e44e6ce94275f4d53746a8f23f1721982
Andreas Hartmann <andreas@apache.org>
2003-06-13 18:15:22 +0000

    using className instead of color (changing CSS class)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39980 13f79535-47bb-0310-9956-ffa450edef68

--- src/webapp/lenya/resources/javascript/tabs.js
+++ src/webapp/lenya/resources/javascript/tabs.js
@@ -1,4 +1,4 @@
-// $Id: tabs.js,v 1.1 2003/06/04 15:08:44 gregor Exp $
+// $Id: tabs.js,v 1.2 2003/06/13 18:15:22 andreas Exp $
 
 function Tab(i){
 	// Variables for customisation:
@@ -7,15 +7,17 @@ function Tab(i){
 	var colourOfActiveTab = "#cccc99";
 	var colourOfInactiveLink = "#333333";
 	var colourOfActiveLink = "#ffffcc";
+	
+	var classOfInactiveTab = "lenya-tablink";
+	var classOfActiveTab = "lenya-tablink-active";
+	
 	// end variables
 	if (document.getElementById){
 		for (f=1;f<numberOfTabs+1;f++){
 			document.getElementById('contentblock'+f).style.display='none';
-			document.getElementById('link'+f).style.background=colourOfInactiveTab;
-			document.getElementById('link'+f).style.color=colourOfInactiveLink;
+			document.getElementById('link'+f).className=classOfInactiveTab;
 		}
 		document.getElementById('contentblock'+i).style.display='block';
-		document.getElementById('link'+i).style.background=colourOfActiveTab;
-		document.getElementById('link'+i).style.color=colourOfActiveLink;
+		document.getElementById('link'+i).className=classOfActiveTab;
 	}
 }
