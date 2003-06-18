1a190d04e510ac8ce2d17260efb29b63107b5e8b
Andreas Hartmann <andreas@apache.org>
2003-06-18 13:30:56 +0000

    added correct licence header
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@39995 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/org/apache/lenya/cms/task/AntTaskTest.java
+++ src/test/org/apache/lenya/cms/task/AntTaskTest.java
@@ -1,9 +1,47 @@
 /*
- * AntTaskTestCase.java
+ * $Id
+ * <License>
+ * The Apache Software License
  *
- * Created on 28. April 2003, 12:28
+ * Copyright (c) 2002 lenya. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this
+ *    list of conditions and the following disclaimer in the documentation and/or
+ *    other materials provided with the distribution.
+ *
+ * 3. All advertising materials mentioning features or use of this software must
+ *    display the following acknowledgment: "This product includes software developed
+ *    by lenya (http://www.lenya.org)"
+ *
+ * 4. The name "lenya" must not be used to endorse or promote products derived from
+ *    this software without prior written permission. For written permission, please
+ *    contact contact@lenya.org
+ *
+ * 5. Products derived from this software may not be called "lenya" nor may "lenya"
+ *    appear in their names without prior written permission of lenya.
+ *
+ * 6. Redistributions of any form whatsoever must retain the following acknowledgment:
+ *    "This product includes software developed by lenya (http://www.lenya.org)"
+ *
+ * THIS SOFTWARE IS PROVIDED BY lenya "AS IS" WITHOUT ANY WARRANTY EXPRESS OR IMPLIED,
+ * INCLUDING THE WARRANTY OF NON-INFRINGEMENT AND THE IMPLIED WARRANTIES OF MERCHANTI-
+ * BILITY AND FITNESS FOR A PARTICULAR PURPOSE. lenya WILL NOT BE LIABLE FOR ANY DAMAGES
+ * SUFFERED BY YOU AS A RESULT OF USING THIS SOFTWARE. IN NO EVENT WILL lenya BE LIABLE
+ * FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR LOST PROFITS EVEN IF lenya HAS
+ * BEEN ADVISED OF THE POSSIBILITY OF THEIR OCCURRENCE. lenya WILL NOT BE LIABLE FOR ANY
+ * THIRD PARTY CLAIMS AGAINST YOU.
+ *
+ * Lenya includes software developed by the Apache Software Foundation, W3C,
+ * DOM4J Project, BitfluxEditor and Xopus.
+ * </License>
  */
-
+ 
 package org.apache.lenya.cms.task;
 
 import java.io.File;
@@ -31,7 +69,7 @@ import org.apache.lenya.cms.publication.Publication;
  * @author  andreas
  */
 public class AntTaskTest extends TestCase {
-    
+
     private static String taskId = "ant-test";
 
     /**
@@ -69,7 +107,7 @@ public class AntTaskTest extends TestCase {
         doTest(getTaskId());
         evaluateTest();
     }
-    
+
     /**
      * Returns the task ID of the task to test.
      * @return The task ID.
@@ -77,7 +115,7 @@ public class AntTaskTest extends TestCase {
     protected String getTaskId() {
         return taskId;
     }
-    
+
     /**
      * Tests an AntTask.
      * @param taskId The ID of the task to test.
@@ -85,10 +123,11 @@ public class AntTaskTest extends TestCase {
      * @throws IOException
      * @throws ParameterException
      */
-    protected void doTest(String taskId) throws ExecutionException, IOException, ParameterException {
-        
+    protected void doTest(String taskId)
+        throws ExecutionException, IOException, ParameterException {
+
         Publication publication = PublicationHelper.getPublication();
-         
+
         TaskManager manager = new TaskManager(publication.getDirectory().getCanonicalPath());
         AntTask task = (AntTask) manager.getTask(taskId);
 
@@ -97,7 +136,7 @@ public class AntTaskTest extends TestCase {
         task.parameterize(parameters);
 
         final GregorianCalendar beforeExecution = new GregorianCalendar();
-        
+
         task.execute(publication.getServletContext().getCanonicalPath());
 
         File logDirectory = new File(publication.getDirectory(), AntTask.LOG_PATH);
@@ -111,7 +150,7 @@ public class AntTaskTest extends TestCase {
         File logFile = logFiles[0];
 
     }
-    
+
     /**
      * Override this method to add your test evaluation code.
      */
