5eea0e8185eaf155501ed471e612a90fb1244e17
Chevrier Edith <edith@apache.org>
2003-07-22 13:56:19 +0000

    Ant task to delete a node of a tree
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@40384 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/ant/DeleteNodeTask.java
@@ -0,0 +1,149 @@
+/*
+$Id: DeleteNodeTask.java,v 1.1 2003/07/22 13:56:19 edith Exp $
+<License>
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Apache Lenya" and  "Apache Software Foundation"  must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation and was  originally created by
+ Michael Wechner <michi@apache.org>. For more information on the Apache Soft-
+ ware Foundation, please see <http://www.apache.org/>.
+
+ Lenya includes software developed by the Apache Software Foundation, W3C,
+ DOM4J Project, BitfluxEditor, Xopus, and WebSHPINX.
+</License>
+*/
+package org.apache.lenya.cms.ant;
+
+import org.apache.lenya.cms.publication.DefaultSiteTree;
+import org.apache.lenya.cms.publication.SiteTreeException;
+import org.apache.lenya.cms.publication.SiteTreeNode;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+
+
+/**
+ * Ant task to delete a node of a tree.
+ * @author edith
+ */
+public class DeleteNodeTask extends Task {
+    private String absolutetreepath;
+    private String documentid;
+
+    /**
+     * Creates a new instance of DeleteNodeTask
+     */
+    public DeleteNodeTask() {
+        super();
+    }
+
+    /**
+     * @return absolutetreepath The absolute path of the tree.
+     */
+    protected String getAbsolutetreepath() {
+        return absolutetreepath;
+    }
+
+    /**
+     * Set the value of the absolute path of the tree
+     * 
+     * @param string The absolute path of the tree.
+     */
+    public void setAbsolutetreepath(String string) {
+        absolutetreepath = string;
+    }
+
+    /**
+     * return the document-id corresponding to the node to delete
+     * @return string The document-id.
+     */
+    protected String getDocumentid() {
+        return documentid;
+    }
+
+    /**
+     * Set the value of the document-id corresponding to the node to delete
+     * 
+     * @param string The document-id.
+     */
+    public void setDocumentid(String string) {
+        documentid = string;
+    }
+
+    /**
+     * Delete a node of a tree.
+     * 
+     * @param documentid The id of the document corresponding to the node to delete.
+     * @param absolutetreepath The absolute path of the tree
+     * 
+     * @throws SiteTreeException if an error occurs
+     */
+    public void deleteNode(String documentid, String absolutetreepath)
+        throws SiteTreeException {
+		DefaultSiteTree tree = null;
+
+	  	try {
+			tree = new DefaultSiteTree(absolutetreepath);
+			SiteTreeNode node = tree.removeNode(documentid);
+			if (node == null) {
+				throw new SiteTreeException("Node " + node + " couldn't be removed");
+			} 
+			tree.save();
+		} catch (Exception e) {
+			throw new SiteTreeException(e);
+		}
+    }   
+    /** (non-Javadoc)
+     * @see org.apache.tools.ant.Task#execute()
+     */
+    public void execute() throws BuildException {
+        try {
+            log("document-id corresponding to the node: " + getDocumentid());
+            log("Absolute Tree Path: " + getAbsolutetreepath());
+			deleteNode(getDocumentid(), getAbsolutetreepath());
+        } catch (Exception e) {
+            throw new BuildException(e);
+        }
+    }
+}
