dbdaab3ffa09910cc9f18c8843b6538934ca5759
Chevrier Edith <edith@apache.org>
2003-08-08 09:48:55 +0000

    work in progress
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@40635 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/ant/SetIdentifier.java
@@ -0,0 +1,171 @@
+/*
+$Id: SetIdentifier.java,v 1.1 2003/08/08 09:48:55 edith Exp $
+<License>
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Apache Lenya" and  "Apache Software Foundation"  must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation and was  originally created by
+ Michael Wechner <michi@apache.org>. For more information on the Apache Soft-
+ ware Foundation, please see <http://www.apache.org/>.
+
+ Lenya includes software developed by the Apache Software Foundation, W3C,
+ DOM4J Project, BitfluxEditor, Xopus, and WebSHPINX.
+</License>
+*/
+package org.apache.lenya.cms.ant;
+
+import org.apache.lenya.cms.publication.DefaultDocumentBuilder;
+import org.apache.lenya.cms.publication.Document;
+import org.apache.lenya.cms.publication.DocumentBuildException;
+import org.apache.lenya.cms.publication.DocumentIdToPathMapper;
+import org.apache.lenya.cms.publication.DublinCore;
+import org.apache.lenya.cms.publication.Label;
+import org.apache.lenya.cms.publication.Publication;
+import org.apache.lenya.cms.publication.SiteTree;
+import org.apache.lenya.cms.publication.SiteTreeNode;
+import org.apache.lenya.cms.publication.SiteTreeNodeVisitor;
+import org.apache.tools.ant.BuildException;
+
+/**
+ * anttask visitor to set the document-id in the dc:identifier
+ * @author edith
+ *
+ */
+public class SetIdentifier
+	extends PublicationTask
+	implements SiteTreeNodeVisitor {
+
+	private String area;
+	private String documentid;
+
+	/**
+	 * 
+	 */
+	public SetIdentifier() {
+		super();
+	}
+
+	/**
+	 * @return String The area.
+	 */
+	public String getArea() {
+		return area;
+	}
+
+	/**
+	 * @return String The document-id.
+	 */
+	public String getDocumentid() {
+		return documentid;
+	}
+
+	/**
+	 * @param string The area.
+	 */
+	public void setArea(String string) {
+		area = string;
+	}
+
+	/**
+	 * @param string The document-id.
+	 */
+	public void setDocumentid(String string) {
+		documentid = string;
+	}
+
+	/** (non-Javadoc)
+	 * @see org.apache.lenya.cms.publication.SiteTreeNodeVisitor#visitSiteTreeNode(org.apache.lenya.cms.publication.SiteTreeNode)
+	 */
+	public void visitSiteTreeNode(SiteTreeNode node) {
+		Publication publication = getPublication();
+		DocumentIdToPathMapper pathMapper = publication.getPathMapper();
+		Label[] labels = node.getLabels();
+		for (int i = 0; i < labels.length; i++) {
+			String language = labels[i].getLanguage();
+			String parentid = node.getAbsoluteParentId();
+			String documentid = parentid + "/" + node.getId();
+			String url =
+				"/"
+					+ publication.getId()
+					+ "/"
+					+ area
+					+ documentid
+					+ "_"
+					+ language
+					+ ".html";
+			log("url : "+url); 
+            Document document = null;
+			try {
+				document =
+					DefaultDocumentBuilder.getInstance().buildDocument(
+						publication,
+						url);
+			} catch (DocumentBuildException e) {
+				e.printStackTrace();
+			}
+            DublinCore dublincore = document.getDublinCore();
+            dublincore.setIdentifier(documentid);
+		}
+
+	}
+
+	/** 
+	 * @see org.apache.tools.ant.Task#execute()
+	 **/
+	public void execute() throws BuildException {
+		try {
+			log("document-id " + this.getDocumentid());
+			log("area " + this.getArea());
+
+			Publication publication = getPublication();
+			SiteTree tree = publication.getSiteTree(area);
+			SiteTreeNode node = tree.getNode(documentid);
+
+			node.acceptSubtree(this);
+		} catch (Exception e) {
+			throw new BuildException(e);
+		}
+	}
+
+}
