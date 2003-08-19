17ec13c3af5e3a1218ee65a600ab8fd91bb9e98e
Christian Egli <egli@apache.org>
2003-08-19 12:29:07 +0000

    Added Manager to keep track of resources for a given document.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@40810 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/publication/ResourcesManager.java
@@ -0,0 +1,106 @@
+/*
+$Id: ResourcesManager.java,v 1.1 2003/08/19 12:29:07 egli Exp $
+<License>
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Apache Lenya" and  "Apache Software Foundation"  must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation and was  originally created by
+ Michael Wechner <michi@apache.org>. For more information on the Apache Soft-
+ ware Foundation, please see <http://www.apache.org/>.
+
+ Lenya includes software developed by the Apache Software Foundation, W3C,
+ DOM4J Project, BitfluxEditor, Xopus, and WebSHPINX.
+</License>
+*/
+package org.apache.lenya.cms.publication;
+
+import java.io.File;
+import java.io.FilenameFilter;
+
+/**
+ * Manager for resources of a CMS document.
+ *
+ * @author <a href="mailto:egli@apache.org">Christian Egli</a>
+ */
+public class ResourcesManager {
+    
+    private File resourcesPath = null;
+    
+    public static final String RESOURCES_PREFIX = "resources";  
+    
+
+
+    /**
+     * Create a new instance of Resources.
+     * 
+     * @param document the document for which the resources are managed
+     */    
+    public ResourcesManager(Document document) {
+        File publicationPath = document.getPublication().getDirectory();
+        resourcesPath =
+            new File(
+                publicationPath,
+                RESOURCES_PREFIX
+                    + document.getId().replace('/', File.separatorChar));
+    }
+    
+    /**
+     * Get the path to the resources.
+     * 
+     * @return the path to the resources
+     */
+    public File getPath() {
+        return resourcesPath;
+    }
+    
+    /**
+     * Get all resources for the associated document.
+     * 
+     * @return all resources of the associated document
+     */
+    public File[] getResources() {
+        FilenameFilter filter = null;
+        return resourcesPath.listFiles(filter);
+    }
+}
