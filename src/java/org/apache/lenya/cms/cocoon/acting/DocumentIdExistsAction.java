dca4102435cb937dd59833d2fa238a8a0915f9cd
Christian Egli <egli@apache.org>
2003-09-18 08:10:54 +0000

    Added an action that checks for the existence of a document-id in the authoring site tree. This is used in the creation usecase. If a child-id is already taken redirect back to the creation screen and displey an error message.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@41282 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/cocoon/acting/DocumentIdExistsAction.java
@@ -0,0 +1,130 @@
+/*
+$Id: DocumentIdExistsAction.java,v 1.1 2003/09/18 08:10:54 egli Exp $
+<License>
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Apache Lenya" and  "Apache Software Foundation"  must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation and was  originally created by
+ Michael Wechner <michi@apache.org>. For more information on the Apache Soft-
+ ware Foundation, please see <http://www.apache.org/>.
+
+ Lenya includes software developed by the Apache Software Foundation, W3C,
+ DOM4J Project, BitfluxEditor, Xopus, and WebSHPINX.
+</License>
+*/
+package org.apache.lenya.cms.cocoon.acting;
+
+import org.apache.avalon.framework.parameters.ParameterException;
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.acting.AbstractAction;
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.SourceResolver;
+import org.apache.lenya.cms.publication.PageEnvelope;
+import org.apache.lenya.cms.publication.PageEnvelopeException;
+import org.apache.lenya.cms.publication.PageEnvelopeFactory;
+import org.apache.lenya.cms.publication.Publication;
+import org.apache.lenya.cms.publication.SiteTree;
+import org.apache.lenya.cms.publication.SiteTreeException;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Action that checks the sitetree if there is a node with the 
+ * current document-id. This is used to prevent creation of documents
+ * with non-unique document-ids
+ * 
+ * @author <a href="mailto:egli@apache.org">Christian Egli</a>
+ */
+public class DocumentIdExistsAction extends AbstractAction {
+
+    /**
+     * Check if there is a doument in the site tree with the given
+     * document-id.
+     * 
+     * If yes return an empty map, if not return null.
+     * 
+     * @param redirector a <code>Redirector</code> value
+     * @param resolver a <code>SourceResolver</code> value
+     * @param objectModel a <code>Map</code> value
+     * @param source a <code>String</code> value
+     * @param parameters a <code>Parameters</code> value
+     *
+     * @return an empty <code>Map</code> if there is a document 
+     * with the given document-id, null otherwiese
+     *
+     * @exception DocumentDoesNotExistException if there is no document with the specified document-id.
+     * @exception PageEnvelopeException if the PageEnvelope could not be created.
+     * @exception DocumentException if the language information could not be fetched from the document.
+     */
+
+    public static final String DOCUMENT_ID_PARAMETER_NAME = "document-id";
+
+    public Map act(
+        Redirector redirector,
+        SourceResolver resolver,
+        Map objectModel,
+        String source,
+        Parameters parameters)
+        throws PageEnvelopeException, SiteTreeException, ParameterException {
+
+        String documentId = parameters.getParameter(DOCUMENT_ID_PARAMETER_NAME);
+
+        if (documentId == null) {
+            return null;
+        }
+
+        PageEnvelope pageEnvelope =
+            PageEnvelopeFactory.getInstance().getPageEnvelope(objectModel);
+
+        SiteTree siteTree =
+            pageEnvelope.getPublication().getSiteTree(
+                Publication.AUTHORING_AREA);
+
+        if (siteTree.getNode(documentId) == null) {
+            return Collections.EMPTY_MAP;
+        }
+        return null;
+    }
+}
