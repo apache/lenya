6663ed729c284150106d375c5b5705e48862f242
Andreas Hartmann <andreas@apache.org>
2003-09-19 19:20:25 +0000

    added WorkflowInvokerAction
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@41315 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/cocoon/acting/WorkflowInvokerAction.java
@@ -0,0 +1,154 @@
+/*
+$Id: WorkflowInvokerAction.java,v 1.1 2003/09/19 19:20:25 andreas Exp $
+<License>
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Apache Lenya" and  "Apache Software Foundation"  must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation and was  originally created by
+ Michael Wechner <michi@apache.org>. For more information on the Apache Soft-
+ ware Foundation, please see <http://www.apache.org/>.
+
+ Lenya includes software developed by the Apache Software Foundation, W3C,
+ DOM4J Project, BitfluxEditor, Xopus, and WebSHPINX.
+</License>
+*/
+package org.apache.lenya.cms.cocoon.acting;
+
+import java.util.Collections;
+import java.util.Map;
+
+import org.apache.avalon.framework.parameters.Parameters;
+import org.apache.cocoon.acting.AbstractAction;
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.SourceResolver;
+import org.apache.lenya.cms.publication.DefaultDocumentBuilder;
+import org.apache.lenya.cms.publication.Document;
+import org.apache.lenya.cms.publication.PageEnvelope;
+import org.apache.lenya.cms.publication.PageEnvelopeFactory;
+import org.apache.lenya.cms.publication.Publication;
+import org.apache.lenya.cms.workflow.WorkflowFactory;
+import org.apache.lenya.workflow.Event;
+import org.apache.lenya.workflow.Situation;
+import org.apache.lenya.workflow.WorkflowInstance;
+
+/**
+ * Action to invoke a workflow transition independently from the request document URL.
+ * Parameters:
+ * <ul>
+ *   <li><strong>document-id:</strong> The document id.</li>
+ *   <li><strong>language:</strong> The language.</li>
+ *   <li><strong>event:</strong> The event to invoke.</li>
+ * </ul>
+ * 
+ * @author <a href="mailto:andreas@apache.org">Andreas Hartmann</a>
+ */
+public class WorkflowInvokerAction extends AbstractAction {
+
+    public static final String AREA = "area";
+    public static final String DOCUMENT_ID = "document-id";
+    public static final String LANGUAGE = "language";
+    public static final String EVENT = "event";
+
+    /**
+     * @see org.apache.cocoon.acting.Action#act(org.apache.cocoon.environment.Redirector, org.apache.cocoon.environment.SourceResolver, java.util.Map, java.lang.String, org.apache.avalon.framework.parameters.Parameters)
+     */
+    public Map act(
+        Redirector redirector,
+        SourceResolver resolver,
+        Map objectModel,
+        String source,
+        Parameters parameters)
+        throws Exception {
+
+        String area = parameters.getParameter(AREA);
+        String documentId = parameters.getParameter(DOCUMENT_ID);
+        String language = parameters.getParameter(LANGUAGE);
+        String eventName = parameters.getParameter(EVENT);
+
+        if (getLogger().isDebugEnabled()) {
+            getLogger().debug(getClass().getName() + " invoked.");
+            getLogger().debug("    Area:        [" + area + "]");
+            getLogger().debug("    Document ID: [" + documentId + "]");
+            getLogger().debug("    Language:    [" + language + "]");
+            getLogger().debug("    Event:       [" + eventName + "]");
+        }
+
+        PageEnvelope envelope = PageEnvelopeFactory.getInstance().getPageEnvelope(objectModel);
+        Publication publication = envelope.getPublication();
+        DefaultDocumentBuilder builder = DefaultDocumentBuilder.getInstance();
+        String url = builder.buildCanonicalUrl(publication, area, documentId, language);
+        Document document = builder.buildDocument(publication, url);
+        
+        WorkflowFactory factory = WorkflowFactory.newInstance();
+
+        if (factory.hasWorkflow(document)) {
+            
+            if (getLogger().isDebugEnabled()) {
+                getLogger().debug("    Invoking workflow event");
+            }
+            
+            WorkflowInstance instance = factory.buildInstance(document);
+            Situation situation = factory.buildSituation(objectModel);
+            Event[] events = instance.getExecutableEvents(situation);
+            Event event = null;
+
+            for (int i = 0; i < events.length; i++) {
+                if (events[i].getName().equals(eventName)) {
+                    event = events[i];
+                }
+            }
+
+            assert event != null;
+            instance.invoke(situation, event);
+        }
+        else {
+            if (getLogger().isDebugEnabled()) {
+                getLogger().debug("    Document has no workflow."); 
+            }
+        }
+
+        return Collections.EMPTY_MAP;
+    }
+
+}
