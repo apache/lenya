5bdfb3e96345c0a09e9ed027ad636b0959d7e32a
Chevrier Edith <edith@apache.org>
2003-09-30 13:22:53 +0000

    action to test the checkOut of a document
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@41369 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/cocoon/acting/ReservedCheckoutTestAction.java
@@ -0,0 +1,114 @@
+/*
+$Id: ReservedCheckoutTestAction.java,v 1.1 2003/09/30 13:22:53 edith Exp $
+<License>
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2003 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Apache Lenya" and  "Apache Software Foundation"  must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation and was  originally created by
+ Michael Wechner <michi@apache.org>. For more information on the Apache Soft-
+ ware Foundation, please see <http://www.apache.org/>.
+
+ Lenya includes software developed by the Apache Software Foundation, W3C,
+ DOM4J Project, BitfluxEditor, Xopus, and WebSHPINX.
+</License>
+*/
+package org.apache.lenya.cms.cocoon.acting;
+
+import org.apache.avalon.framework.parameters.Parameters;
+
+import org.apache.cocoon.environment.Redirector;
+import org.apache.cocoon.environment.SourceResolver;
+
+import org.apache.lenya.cms.rc.FileReservedCheckOutException;
+import org.apache.lenya.cms.rc.RCML;
+import org.apache.lenya.cms.rc.RCMLEntry;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * An action that tests if a document is already checked out by a given user.
+ * If it isn't, so a check out will be tried.
+ *
+ * @author edith
+ */
+
+public class ReservedCheckoutTestAction extends RevisionControllerAction {
+
+    /** (non-Javadoc)
+	 * @see org.apache.cocoon.acting.Action#act(org.apache.cocoon.environment.Redirector, org.apache.cocoon.environment.SourceResolver, java.util.Map, java.lang.String, org.apache.avalon.framework.parameters.Parameters)
+	 */
+	public Map act(Redirector redirector, SourceResolver resolver, Map objectModel, String src,
+        Parameters parameters) throws Exception {
+        super.act(redirector, resolver, objectModel, src, parameters);
+
+        HashMap actionMap = new HashMap();
+
+        try {
+            RCMLEntry entry =getRc().getRCML(getFilename()).getLatestEntry();
+
+			if ((entry == null) | (entry.getType() != RCML.co) | !entry.getIdentity().equals(getUsername())) {
+				//check out
+	            getRc().reservedCheckOut(getFilename(),getUsername());   
+			}
+		} catch (FileReservedCheckOutException e) {
+			actionMap.put("exception", "fileReservedCheckOutException");
+			actionMap.put("filename", getFilename());
+			actionMap.put("user", e.getCheckOutUsername());
+			actionMap.put("date", e.getCheckOutDate());
+			getLogger().warn("Document " + getFilename() + " already checked-out by " + e.getCheckOutUsername() + " since " + e.getCheckOutDate());
+
+			return actionMap;
+		} catch (Exception e) {
+			actionMap.put("exception", "genericException");
+			actionMap.put("filename", getFilename());
+			actionMap.put("message", "" + e);
+			getLogger().error(".act(): The document " + getFilename() + " couldn't be checked out: ", e);
+
+			return actionMap;
+		}
+        return null;
+    }
+}
