f5143c16781b613667e569eb48e480d141ea547d
Christian Egli <egli@apache.org>
2003-09-30 14:44:50 +0000

    Removed unused variable.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@41375 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/workflow/CMSHistory.java
+++ src/java/org/apache/lenya/cms/workflow/CMSHistory.java
@@ -63,21 +63,18 @@ import org.apache.lenya.workflow.impl.Version;
 import org.apache.lenya.workflow.impl.WorkflowInstanceImpl;
 import org.apache.lenya.xml.NamespaceHelper;
 
-import org.apache.log4j.Category;
-
 import org.w3c.dom.Element;
 
 import java.io.File;
 
 /**
  * @author andreas
- * @version $Id: CMSHistory.java,v 1.17 2003/09/17 15:17:22 michi Exp $
+ * @version $Id: CMSHistory.java,v 1.18 2003/09/30 14:44:50 egli Exp $
  *
  * To change the template for this generated type comment go to
  * Window>Preferences>Java>Code Generation>Code and Comments
  */
 public class CMSHistory extends History {
-    private static final Category log = Category.getInstance(CMSHistory.class);
     public static final String HISTORY_PATH = "content/workflow/history";
 
     /**
