ff9ce4574c560b39305827d2ffc81b8e9dce1ff1
Christian Egli <egli@apache.org>
2003-10-22 16:19:55 +0000

    Documentation fixes.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@41569 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/ant/DocumentIdToPath.java
+++ src/java/org/apache/lenya/cms/ant/DocumentIdToPath.java
@@ -49,99 +49,98 @@ import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Target;
 
 /**
- * ant task to get the file path from the document id
+ * Ant task to get the file path from the document id.
+ * 
  * @author edith
  */
 public class DocumentIdToPath extends PublicationTask {
-	private String area;
-	private String documentid;
-	private String propertyname;
-	/**
-	 * Creates a new instance of DocumentIdToPath
-	 */
-	public DocumentIdToPath() {
-		super();
-	}
+    private String area;
+    private String documentid;
+    private String propertyname;
+
+    /**
+     * Creates a new instance of DocumentIdToPath
+     */
+    public DocumentIdToPath() {
+        super();
+    }
+
+    /**
+     * @return Sting The directory for the area in the publication.
+     */
+    public String getArea() {
+        return area;
+    }
+
+    /**
+     * @return string The document id 
+     */
+    protected String getDocumentid() {
+        return documentid;
+    }
+
+    /**
+     * @return propertyname. The name of the property for the path.
+     */
+    public String getPropertyname() {
+        return propertyname;
+    }
+
+    /**
+     * @param string The area.
+     */
+    public void setArea(String string) {
+        area = string;
+    }
+
+    /**
+     * @param string The name of the property.
+     */
+    public void setPropertyname(String string) {
+        propertyname = string;
+    }
+
+    /**
+     * Set the value of the document id.
+     *   
+     * @param string The document id. 
+     */
+    public void setDocumentid(String string) {
+        documentid = string;
+    }
+
+    /**
+     * Gets the path from the document id and sets this value in the 
+     * property of the project with the name propertyname.   
     
-	/**
-	 * @return Sting The directory for the area in the publication.
-	 */
-	public String getArea() {
-		return area;
-	}
+     * @param area The area (ex authoring)
+     * @param documentid  The document id.
+     * @param propertyname The name of the property
+     */
+    public void compute(String area, String documentid, String propertyname) {
 
-	/**
-	 * @return string The document id 
-	 */
-	protected String getDocumentid() {
-		return documentid;
-	}
+        Publication publication = getPublication();
+        DocumentIdToPathMapper pathMapper = publication.getPathMapper();
+        String path = pathMapper.getPath(documentid, "");
+        log("path " + path);
 
-	/**
-	 * @return propertyname. The name of the property for the path.
-	 */
-	public String getPropertyname() {
-		return propertyname;
-	}
+        int index = path.lastIndexOf("/");
+        String dir = path.substring(0, index);
+        log("dir " + dir);
 
-	/**
-	 * @param string The area.
-	 */
-	public void setArea(String string) {
-		area = string;
-	}
+        Target target = getOwningTarget();
+        Project project = target.getProject();
+        project.setProperty(propertyname, dir);
+    }
 
-	/**
-	 * @param string The name of the property.
-	 */
-	public void setPropertyname(String string) {
-		propertyname = string;
-	}
-
-	/**
-	 * set the value of the document id  
-	 * @param string The document id. 
-	 */
-	public void setDocumentid(String string) {
-		documentid = string;
-	}
-
-	/**
-	 * Gets the path from the document id and sets this value in the 
-	 * property of the project with the name propertyname.   
-
-	 * @param area The area (ex authoring)
-	 * @param documentid  The document id.
-	 * @param propertyname The name of the property
-	 */
-	public void compute(String area, String documentid, String propertyname) { 
-
-	  Publication publication= getPublication();
-	  DocumentIdToPathMapper pathMapper=publication.getPathMapper();
-      String path = pathMapper.getPath(documentid, ""); 
-	  log("path " + path);
-
-	  int index = path.lastIndexOf("/");
-	  String dir = path.substring(0, index);
-	  log("dir " + dir);
-	  
-	  Target target=getOwningTarget();
-	  Project project=target.getProject();
-	  project.setProperty(propertyname, dir);
-	}
-
-	/** 
-	 * @see org.apache.tools.ant.Task#execute()
-	 **/
-	public void execute() throws BuildException {
-		try {
-			log("document-id " + getDocumentid());
-			log("area " + getArea());
-			log("property: " +getPropertyname());
-			compute(getArea(), getDocumentid(), getPropertyname());
-		} catch (Exception e) {
-			throw new BuildException(e);
-		}
-	}
+    /** 
+     * @see org.apache.tools.ant.Task#execute()
+     **/
+    public void execute() throws BuildException {
+        log("document-id " + getDocumentid());
+        log("area " + getArea());
+        log("property: " + getPropertyname());
+        compute(getArea(), getDocumentid(), getPropertyname());
+    }
 
 }
