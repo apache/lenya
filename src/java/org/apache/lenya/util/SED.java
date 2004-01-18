fec3da0592d2c49a0d1e7dc34d82d54b6554f374
Michael Wechner <michi@apache.org>
2004-01-18 12:22:41 +0000

    utility class similiar to UNIX sed added
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@42135 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/util/SED.java
@@ -0,0 +1,134 @@
+/*
+<License>
+
+ ============================================================================
+                   The Apache Software License, Version 1.1
+ ============================================================================
+
+ Copyright (C) 1999-2004 The Apache Software Foundation. All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without modifica-
+ tion, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of  source code must  retain the above copyright  notice,
+    this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+ 3. The end-user documentation included with the redistribution, if any, must
+    include  the following  acknowledgment:  "This product includes  software
+    developed  by the  Apache Software Foundation  (http://www.apache.org/)."
+    Alternately, this  acknowledgment may  appear in the software itself,  if
+    and wherever such third-party acknowledgments normally appear.
+
+ 4. The names "Apache Lenya" and  "Apache Software Foundation"  must  not  be
+    used to  endorse or promote  products derived from  this software without
+    prior written permission. For written permission, please contact
+    apache@apache.org.
+
+ 5. Products  derived from this software may not  be called "Apache", nor may
+    "Apache" appear  in their name,  without prior written permission  of the
+    Apache Software Foundation.
+
+ THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ FITNESS  FOR A PARTICULAR  PURPOSE ARE  DISCLAIMED.  IN NO  EVENT SHALL  THE
+ APACHE SOFTWARE  FOUNDATION  OR ITS CONTRIBUTORS  BE LIABLE FOR  ANY DIRECT,
+ INDIRECT, INCIDENTAL, SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL  DAMAGES (INCLU-
+ DING, BUT NOT LIMITED TO, PROCUREMENT  OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ OF USE, DATA, OR  PROFITS; OR BUSINESS  INTERRUPTION)  HOWEVER CAUSED AND ON
+ ANY  THEORY OF LIABILITY,  WHETHER  IN CONTRACT,  STRICT LIABILITY,  OR TORT
+ (INCLUDING  NEGLIGENCE OR  OTHERWISE) ARISING IN  ANY WAY OUT OF THE  USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ This software  consists of voluntary contributions made  by many individuals
+ on  behalf of the Apache Software  Foundation and was  originally created by
+ Michael Wechner <michi@apache.org>. For more information on the Apache Soft-
+ ware Foundation, please see <http://www.apache.org/>.
+
+ Lenya includes software developed by the Apache Software Foundation, W3C,
+ DOM4J Project, and WebSHPINX.
+</License>
+*/
+package org.apache.lenya.util;
+
+import org.apache.log4j.Category;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import java.nio.CharBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Similar to the UNIX sed
+ *
+ * @author Michael Wechner
+ * @version $Id: SED.java,v 1.1 2004/01/18 12:22:41 michi Exp $
+ */
+public class SED {
+    static Category log = Category.getInstance(SED.class);
+
+    /**
+     * Command Line Interface
+     *
+     * @param args DOCUMENT ME!
+     */
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Usage: org.apache.lenya.util.SED");
+            return;
+        }
+    }
+
+    /**
+     * Substitute prefix, e.g. ".*world.*" by "universe"
+     *
+     * @param file File which sed shall be applied
+     * @param prefixSubstitute Prefix which shall be replaced
+     * @param substituteReplacement Prefix which is going to replace the original
+     *
+     * @throws IOException DOCUMENT ME!
+     */
+    public static void replaceAll(File file, String substitute, String substituteReplacement) throws IOException {
+        log.debug("Replace " + substitute + " by " + substituteReplacement);
+
+        Pattern pattern = Pattern.compile(substitute);
+
+        // Open the file and then get a channel from the stream
+        FileInputStream fis = new FileInputStream(file);
+        FileChannel fc = fis.getChannel();
+
+        // Get the file's size and then map it into memory
+        int sz = (int)fc.size();
+        MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, sz);
+
+	// Decode the file into a char buffer
+        // Charset and decoder for ISO-8859-15
+        Charset charset = Charset.forName("ISO-8859-15");
+        CharsetDecoder decoder = charset.newDecoder();
+	CharBuffer cb = decoder.decode(bb);
+
+        Matcher matcher = pattern.matcher(cb);
+        String outString = matcher.replaceAll(substituteReplacement);
+        log.debug(outString);
+
+
+        FileOutputStream fos = new FileOutputStream(file.getAbsolutePath());
+        PrintStream ps =new PrintStream(fos);
+        ps.print(outString);
+        ps.close();
+        fos.close();
+    }
+}
