c4f26bfc77f15dc21bbafc52867d19eab11e4904
Christian Egli <egli@apache.org>
2004-03-26 18:59:59 +0000

    Insert ASL 2.0
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@42777 13f79535-47bb-0310-9956-ffa450edef68

--- lenya.bat
+++ lenya.bat
@@ -1,8 +1,22 @@
+rem Copyright 1999-2004 The Apache Software Foundation
+rem
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
+rem
+rem     http://www.apache.org/licenses/LICENSE-2.0
+rem
+rem Unless required by applicable law or agreed to in writing, software
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
+
 @echo off
 :: -----------------------------------------------------------------------------
 :: Lenya Win32 Shell Script
 ::
-:: $Id: lenya.bat,v 1.3 2004/03/04 18:40:41 gregor Exp $
+:: $Id: lenya.bat,v 1.4 2004/03/26 18:47:04 egli Exp $
 :: -----------------------------------------------------------------------------
 
 :: Configuration variables
--- lenya.sh
+++ lenya.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
+
+# Copyright 1999-2004 The Apache Software Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # -----------------------------------------------------------------------------
 # Lenya Unix Shell Script
 #
-# $Id: lenya.sh,v 1.3 2004/03/04 18:40:41 gregor Exp $
+# $Id: lenya.sh,v 1.4 2004/03/26 18:47:04 egli Exp $
 # -----------------------------------------------------------------------------
 
 # Configuration variables
