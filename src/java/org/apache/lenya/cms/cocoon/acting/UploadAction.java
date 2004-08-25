f240bc34fe7141396be4626d32404111882d8cab
Andreas Hartmann <andreas@apache.org>
2004-08-25 09:13:36 +0000

    refactoring: extracted some methods
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@43272 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/cocoon/acting/UploadAction.java
+++ src/java/org/apache/lenya/cms/cocoon/acting/UploadAction.java
@@ -15,11 +15,12 @@
  *
  */
 
-/* $Id: UploadAction.java,v 1.8 2004/08/23 14:38:30 andreas Exp $  */
+/* $Id: UploadAction.java,v 1.9 2004/08/25 09:13:36 andreas Exp $  */
 
 package org.apache.lenya.cms.cocoon.acting;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -50,16 +51,16 @@ import org.apache.lenya.xml.NamespaceHelper;
 import org.w3c.dom.Element;
 
 /**
- * The class <code>UploadAction</code> implements an action that allows for
- * asset and content upload. An upload consists of a file upload plus optionally a file creation
- * for the meta data of the asset.
- *
+ * The class <code>UploadAction</code> implements an action that allows for asset and content
+ * upload. An upload consists of a file upload plus optionally a file creation for the meta data of
+ * the asset.
+ * 
  * Also see org.apache.lenya.cms.authoring.UploadHelper
  */
 public class UploadAction extends AbstractConfigurableAction {
-	
-	private Document document;
-	private PageEnvelope pageEnvelope;
+
+    private Document document;
+    private PageEnvelope pageEnvelope;
 
     public static final String UPLOADASSET_PARAM_NAME = "properties.asset.data";
     public static final String UPLOADASSET_PARAM_PREFIX = "properties.asset.";
@@ -67,118 +68,58 @@ public class UploadAction extends AbstractConfigurableAction {
     public static final String UPLOADASSET_RETURN_FILESIZE = "file-size";
     public static final String UPLOADASSET_RETURN_MIMETYPE = "mime-type";
 
-	public static final String CONTENT_PREFIX = "content";
+    public static final String CONTENT_PREFIX = "content";
 
-    public static String FILE_NAME_REGEXP = "[-a-zA-Z0-9_.]+";
+    public static final String FILE_NAME_REGEXP = "[-a-zA-Z0-9_.]+";
 
     // optional parameters for meta data according to dublin core
-    public static final String[] DUBLIN_CORE_PARAMETERS =
-        {
-            "title",
-            "creator",
-            "subject",
-            "description",
-            "publisher",
-            "contributor",
-            "date",
-            "type",
-            "format",
-            "identifier",
-            "source",
-            "language",
-            "relation",
-            "coverage",
-            "rights" };
+    public static final String[] DUBLIN_CORE_PARAMETERS = { "title", "creator", "subject",
+            "description", "publisher", "contributor", "date", "type", "format", "identifier",
+            "source", "language", "relation", "coverage", "rights" };
 
     /**
-     * Retrieve the file from the request and store it in the
-     * corresponding directory, optionally create a meta file and
-     * optionally insert an image tag in the requesting document.
-     *
+     * Retrieve the file from the request and store it in the corresponding directory, optionally
+     * create a meta file and optionally insert an image tag in the requesting document.
+     * 
      * @param redirector a <code>Redirector</code> value
      * @param resolver a <code>SourceResolver</code> value
      * @param objectModel a <code>Map</code> value
      * @param source a <code>String</code> value
      * @param parameters a <code>Parameters</code> value
-     *
-     * @return a <code>Map</code> containing the referer or null if the 
-     * name of the file to be uploaded contains characters that are not allowed
-     * (@see FILE_NAME_REGEXP).
-     *
+     * 
+     * @return a <code>Map</code> containing the referer or null if the name of the file to be
+     *         uploaded contains characters that are not allowed (@see FILE_NAME_REGEXP).
+     * 
      * @exception Exception if an error occurs
      */
-    public Map act(
-        Redirector redirector,
-        SourceResolver resolver,
-        Map objectModel,
-        String source,
-        Parameters parameters)
-        throws Exception {
+    public Map act(Redirector redirector, SourceResolver resolver, Map objectModel, String source,
+            Parameters parameters) throws Exception {
 
-        HashMap results = new HashMap();
-
+        Map results = new HashMap();
         Request request = ObjectModelHelper.getRequest(objectModel);
-
-        pageEnvelope =
-            PageEnvelopeFactory.getInstance().getPageEnvelope(objectModel);
-        
+        pageEnvelope = PageEnvelopeFactory.getInstance().getPageEnvelope(objectModel);
         document = pageEnvelope.getDocument();
 
-        byte[] buf = new byte[4096];
         File assetFile;
 
-        for (Enumeration enum = request.getParameterNames();
-            enum.hasMoreElements();
-            ) {
-            String param = (String)enum.nextElement();
-            getLogger().debug(
-                param
-                    + ": "
-                    + request.getParameter(param)
-                    + " ["
-                    + request.get(param)
-                    + "]");
-        }
-        
+        logRequestParameters(request);
+
         // determine if the upload is an asset or a content upload
         String uploadType = request.getParameter("uploadtype");
 
         // make asset upload the default if it is not specified
-		if (uploadType == null) {
-			uploadType = "asset";
-		}
-
-        // optional parameters for the meta file which contains dublin
-        // core information.
-        HashMap dublinCoreParams = new HashMap();
-
-        for (int i = 0; i < DUBLIN_CORE_PARAMETERS.length; i++) {
-            String paramName = DUBLIN_CORE_PARAMETERS[i];
-            String paramValue =
-                request.getParameter(UPLOADASSET_PARAM_PREFIX + paramName);
-
-            if (paramValue == null) {
-                paramValue = "";
-            }
-
-            dublinCoreParams.put(paramName, paramValue);
+        if (uploadType == null) {
+            uploadType = "asset";
         }
 
-        Iterator iter = dublinCoreParams.keySet().iterator();
-
-        while (iter.hasNext()) {
-            String paramName = (String)iter.next();
-            getLogger().debug(
-                paramName + ": " + dublinCoreParams.get(paramName));
-        }
+        Map dublinCoreParams = getDublinCoreParameters(request);
 
         // upload the file to the uploadDir
-        Part part = (Part)request.get(UPLOADASSET_PARAM_NAME);
+        Part part = (Part) request.get(UPLOADASSET_PARAM_NAME);
 
         String fileName = part.getFileName();
-        if (!fileName.matches(FILE_NAME_REGEXP)
-            || FileUtil.getExtension(fileName).equals("")) {
-            // the file name contains characters which mean trouble 
+        if (!fileName.matches(FILE_NAME_REGEXP) || FileUtil.getExtension(fileName).equals("")) {
+            // the file name contains characters which mean trouble
             // and are therefore not allowed.
             getLogger().warn("The filename [" + fileName + "] is not valid for an asset.");
             return null;
@@ -189,76 +130,130 @@ public class UploadAction extends AbstractConfigurableAction {
         results.put(UPLOADASSET_RETURN_MIMETYPE, mimeType);
         results.put(UPLOADASSET_RETURN_FILESIZE, new Integer(fileSize));
 
-		// FIXME: write fileSize into dc meta data
+        // FIXME: write fileSize into dc meta data
         dublinCoreParams.put("format", mimeType);
         dublinCoreParams.put("extent", Integer.toString(fileSize));
 
         if (uploadType.equals("asset")) {
-        	ResourcesManager resourcesMgr =
-            	new ResourcesManager(document);
-        	    assetFile = new File(resourcesMgr.getPath(), part.getFileName());
+            ResourcesManager resourcesMgr = new ResourcesManager(document);
+            assetFile = new File(resourcesMgr.getPath(), part.getFileName());
 
-        	if (!resourcesMgr.getPath().exists()) {
-            	resourcesMgr.getPath().mkdirs();
-        	}
+            if (!resourcesMgr.getPath().exists()) {
+                resourcesMgr.getPath().mkdirs();
+            }
 
-	        // create an extra file containing the meta description for
-			// the asset.
-			File metaDataFile =
-				new File(resourcesMgr.getPath(), fileName + ".meta");
-			createMetaData(metaDataFile, dublinCoreParams);
+            // create an extra file containing the meta description for
+            // the asset.
+            File metaDataFile = new File(resourcesMgr.getPath(), fileName + ".meta");
+            createMetaData(metaDataFile, dublinCoreParams);
 
-        } 
+        }
         // must be a content upload then
         else {
-			assetFile = new File(document.getFile().getParent(), part.getFileName());
-            getLogger().debug("assetFile: "+assetFile);
+            assetFile = new File(document.getFile().getParent(), part.getFileName());
+            getLogger().debug("assetFile: " + assetFile);
         }
-        
-		assetFile.createNewFile();
-        
-        FileOutputStream out = new FileOutputStream(assetFile);
-        InputStream in = part.getInputStream();
-        int read = in.read(buf);
 
-        while (read > 0) {
-            out.write(buf, 0, read);
-            read = in.read(buf);
-        }
+        saveAsset(assetFile, part);
 
         return Collections.unmodifiableMap(results);
     }
 
     /**
+     * Saves the asset to a file.
+     * 
+     * @param assetFile The asset file.
+     * @param part The part of the multipart request.
+     * @throws Exception if an error occurs.
+     */
+    protected void saveAsset(File assetFile, Part part) throws Exception {
+        boolean created = assetFile.createNewFile();
+        if (!created) {
+            throw new RuntimeException("The file [" + assetFile + "] could not be created.");
+        }
+
+        byte[] buf = new byte[4096];
+        FileOutputStream out = new FileOutputStream(assetFile);
+        try {
+            InputStream in = part.getInputStream();
+            int read = in.read(buf);
+
+            while (read > 0) {
+                out.write(buf, 0, read);
+                read = in.read(buf);
+            }
+        } finally {
+            out.close();
+        }
+    }
+
+    /**
+     * Logs the request parameters.
+     * @param request The request.
+     */
+    protected void logRequestParameters(Request request) {
+        for (Enumeration enum = request.getParameterNames(); enum.hasMoreElements();) {
+            String param = (String) enum.nextElement();
+            getLogger().debug(
+                    param + ": " + request.getParameter(param) + " [" + request.get(param) + "]");
+        }
+    }
+
+    /**
+     * Retrieves optional parameters for the meta file which contains dublin core information from
+     * the request.
+     * @param request The request.
+     * @return A map.
+     */
+    protected Map getDublinCoreParameters(Request request) {
+        HashMap dublinCoreParams = new HashMap();
+
+        for (int i = 0; i < DUBLIN_CORE_PARAMETERS.length; i++) {
+            String paramName = DUBLIN_CORE_PARAMETERS[i];
+            String paramValue = request.getParameter(UPLOADASSET_PARAM_PREFIX + paramName);
+
+            if (paramValue == null) {
+                paramValue = "";
+            }
+
+            dublinCoreParams.put(paramName, paramValue);
+        }
+        
+        Iterator iter = dublinCoreParams.keySet().iterator();
+        while (iter.hasNext()) {
+            String paramName = (String) iter.next();
+            getLogger().debug(paramName + ": " + dublinCoreParams.get(paramName));
+        }
+        
+        return dublinCoreParams;
+    }
+
+    /**
      * Create the meta data file given the dublin core parameters.
-     *
+     * 
      * @param metaDataFile the file where the meta data file is to be created
      * @param dublinCoreParams a <code>Map</code> containing the dublin core values
-     *
-     * @exception IOException if an error occurs
+     * @throws TransformerConfigurationException if an error occurs.
+     * @throws TransformerException if an error occurs.
+     * @throws IOException if an error occurs
+     * @throws ParserConfigurationException if an error occurs.
      */
-    protected void createMetaData(File metaDataFile, HashMap dublinCoreParams)
-        throws
-            TransformerConfigurationException,
-            TransformerException,
-            IOException,
+    protected void createMetaData(File metaDataFile, Map dublinCoreParams)
+            throws TransformerConfigurationException, TransformerException, IOException,
             ParserConfigurationException {
 
-        assert(metaDataFile.getParentFile().exists());
+        assert (metaDataFile.getParentFile().exists());
 
-        NamespaceHelper helper =
-            new NamespaceHelper(
-                "http://purl.org/dc/elements/1.1/",
-                "dc",
+        NamespaceHelper helper = new NamespaceHelper("http://purl.org/dc/elements/1.1/", "dc",
                 "metadata");
 
-		Element root = helper.getDocument().getDocumentElement();
-		
+        Element root = helper.getDocument().getDocumentElement();
+
         Iterator iter = dublinCoreParams.keySet().iterator();
 
         while (iter.hasNext()) {
-            String tagName = (String)iter.next();
-            String tagValue = (String)dublinCoreParams.get(tagName);
+            String tagName = (String) iter.next();
+            String tagValue = (String) dublinCoreParams.get(tagName);
             root.appendChild(helper.createElement(tagName, tagValue));
         }
 
