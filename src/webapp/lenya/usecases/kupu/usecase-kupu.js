f7622d642e3b4ab80896a1ad2cbd1803e22795a0
Rolf Kulemann <roku@apache.org>
2004-08-27 21:29:51 +0000

    - Removed useless imports - Added lic - Docs
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@43291 13f79535-47bb-0310-9956-ffa450edef68

--- src/webapp/lenya/usecases/kupu/usecase-kupu.js
+++ src/webapp/lenya/usecases/kupu/usecase-kupu.js
@@ -1,17 +1,29 @@
+/*
+ * Copyright  1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
 importClass(Packages.java.util.ArrayList);
 
-importClass(Packages.org.apache.cocoon.components.ContextHelper);
-
 importClass(Packages.org.apache.lenya.cms.cocoon.flow.FlowHelper);
-importClass(Packages.org.apache.lenya.cms.publication.Publication);
 importClass(Packages.org.apache.lenya.cms.publication.DocumentHelper);
 importClass(Packages.org.apache.lenya.cms.publication.PublicationHelper);
 importClass(Packages.org.apache.lenya.cms.publication.ResourcesManager);
 
-importClass(Packages.java.io.BufferedOutputStream);
-importClass(Packages.java.io.FileOutputStream);
-
 /**
+ * Kupu usecase flow.
  * @version $Id$
  */
 
