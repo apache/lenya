4e9bf57ee2aa6576ad152c237c7fd2817428d5e8
Andreas Hartmann <andreas@apache.org>
2004-09-15 20:41:31 +0000

    removed reference to DOMUtil
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@46133 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/lucene/index/Index.java
+++ src/java/org/apache/lenya/lucene/index/Index.java
@@ -1,21 +1,17 @@
 /*
- * Copyright  1999-2004 The Apache Software Foundation
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
+ * Copyright 1999-2004 The Apache Software Foundation
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *  
  */
-
-/* $Id: Index.java,v 1.15 2004/07/29 05:49:06 michi Exp $  */
 
 package org.apache.lenya.lucene.index;
 
@@ -23,15 +19,19 @@ import java.io.File;
 import java.util.Date;
 
 import org.apache.lenya.lucene.IndexConfiguration;
-import org.apache.lenya.xml.DOMUtil;
 import org.apache.lenya.xml.DocumentHelper;
-import org.apache.lenya.xml.XPath;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
+/**
+ * Index.
+ *
+ * @version $Id:$
+ */
 public class Index {
     /**
      * Command line interface
-     *
+     * 
      * @param argv Lucene Index Configuration
      */
     public static void main(String[] argv) {
@@ -66,11 +66,12 @@ public class Index {
 
             Indexer indexer = (Indexer) ie.getIndexerClass().newInstance();
 
-            DOMUtil du = new DOMUtil();
             String path = argv[0];
-            
+
             Document config = DocumentHelper.readDocument(new File(path));
-            indexer.configure(du.getElement(config.getDocumentElement(), new XPath("indexer")), argv[0]);
+            Element indexerElement = DocumentHelper.getFirstChild(config.getDocumentElement(),
+                    null, "indexer");
+            indexer.configure(indexerElement, argv[0]);
 
             if (argv.length == 2) {
                 indexer.indexDocument(new File(argv[1]));
@@ -93,14 +94,18 @@ public class Index {
     }
 
     /**
-     * Get indexer from configuration
+     * Get indexer from configuration.
+     * @param luceneConfig The lucene configuration.
+     * @return An indexer.
+     * @throws Exception if an error occurs.
      */
     public static Indexer getIndexer(String luceneConfig) throws Exception {
         IndexConfiguration ie = new IndexConfiguration(luceneConfig);
         Indexer indexer = (Indexer) ie.getIndexerClass().newInstance();
-        DOMUtil du = new DOMUtil();
         Document config = DocumentHelper.readDocument(new File(luceneConfig));
-        indexer.configure(du.getElement(config.getDocumentElement(), new XPath("indexer")), luceneConfig);
+        Element indexerElement = DocumentHelper.getFirstChild(config.getDocumentElement(), null,
+                "indexer");
+        indexer.configure(indexerElement, luceneConfig);
 
         return indexer;
     }
