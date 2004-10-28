d4bba325e78adedcf9f69223523813af327ace03
Rolf Kulemann <roku@apache.org>
2004-10-28 19:35:43 +0000

    Fix for bug #31941
    git-svn-id: https://svn.apache.org/repos/asf/incubator/lenya/trunk@55887 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/cocoon/components/modules/input/SitetreeModule.java
+++ src/java/org/apache/lenya/cms/cocoon/components/modules/input/SitetreeModule.java
@@ -38,8 +38,9 @@ public class SitetreeModule extends AbstractPageEnvelopeModule {
 
     public static final String AUTHORING_NODE = "authoring-node";
     public static final String LIVE_NODE = "live-node";
-
-    protected static final String[] PARAMETER_NAMES = { AUTHORING_NODE, LIVE_NODE };
+    public static final String TRASH_NODE = "trash-node";
+    
+    protected static final String[] PARAMETER_NAMES = { AUTHORING_NODE, LIVE_NODE, TRASH_NODE };
 
     /**
      * @see org.apache.cocoon.components.modules.input.InputModule#getAttribute(java.lang.String,
@@ -65,7 +66,11 @@ public class SitetreeModule extends AbstractPageEnvelopeModule {
                 SiteTree liveTree = manager.getTree(Publication.LIVE_AREA);
                 value = liveTree.getNode(envelope.getDocument().getId());
             }
-
+            
+            if (name.equals(TRASH_NODE)) {
+                SiteTree authoringTree = publication.getSiteTree(Publication.TRASH_AREA);
+                value = authoringTree.getNode(envelope.getDocument().getId());
+            }
         } catch (Exception e) {
             throw new ConfigurationException("Obtaining value for [" + name + "] failed: ", e);
         }
