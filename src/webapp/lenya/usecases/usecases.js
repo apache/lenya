eb7af130a89c787e75cf9ba7e1c0c84c94922f69
Andreas Hartmann <andreas@apache.org>
2004-12-22 08:57:32 +0000

    initial checkin of java-based access control usecasees
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@123066 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/webapp/lenya/usecases/usecases.js
@@ -0,0 +1,71 @@
+
+function passRequestParameters(flowHelper, usecase) {
+	var names = cocoon.request.getParameterNames();
+	while (names.hasMoreElements()) {
+	    var name = names.nextElement();
+		if (!name.equals("lenya.usecase")
+			&& !name.equals("lenya.continuation")
+			&& !name.equals("submit")) {
+			
+			var value = flowHelper.getRequest(cocoon).get(name);
+			Packages.java.lang.System.out.println("name: " + name + "  value: " + value.getClass());
+			
+			var string = new Packages.java.lang.String();
+			if (string.getClass().isInstance(value)) {
+				usecase.setParameter(name, value);
+			}
+			else {
+				usecase.setPart(name, value);
+			}
+			
+		}
+	}
+}
+
+function executeUsecase() {
+	var usecaseName = cocoon.request.getParameter("lenya.usecase");
+	var usecaseResolver = cocoon.getComponent("org.apache.lenya.cms.usecase.UsecaseResolver");
+	var usecase = usecaseResolver.resolve(usecaseName);
+	
+	var flowHelper = new Packages.org.apache.lenya.cms.cocoon.flow.FlowHelper();
+	var situation = flowHelper.getSituation(cocoon);
+	var envelope = flowHelper.getPageEnvelope(cocoon);
+	var document = envelope.getDocument();
+	
+	passRequestParameters(flowHelper, usecase);
+	
+	var ready = false;
+	var success = false;
+	
+	while (!ready) {
+	
+		cocoon.sendPageAndWait("view/" + usecaseName, {
+		    "usecase" : usecase
+		});
+		
+		if (cocoon.request.getParameter("submit")) {
+		
+		    passRequestParameters(flowHelper, usecase);
+			
+			usecase.checkExecutionConditions();
+			if (usecase.getErrorMessages().isEmpty()) {
+				usecase.execute();
+				if (usecase.getErrorMessages().isEmpty()) {
+					usecase.checkPostconditions();
+					if (usecase.getErrorMessages().isEmpty()) {
+						ready = true;
+						success = true;
+					}
+				}
+			}
+		}
+		else if (cocoon.request.getParameter("cancel")) {
+			ready = true;
+		}
+	
+	}
+	
+	var url = envelope.getContext() + usecase.getTargetURL(success);
+	cocoon.redirectTo(url);
+	
+}
