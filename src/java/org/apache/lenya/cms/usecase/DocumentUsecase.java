e9852792408e7fa107e2d0c504720bef130c2f2f
Gregor Rothfuss <gregor@apache.org>
2004-12-25 05:36:38 +0000

    Improved javadocs for DocumentUsecase
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@123315 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/usecase/DocumentUsecase.java
+++ src/java/org/apache/lenya/cms/usecase/DocumentUsecase.java
@@ -23,8 +23,7 @@ import org.apache.lenya.workflow.Situation;
 
 /**
  * 
- * TODO To change the template for this generated type comment go to Window - Preferences - Java -
- * Code Style - Code Templates
+ * Extends AbstractUsecase with document helper methods
  */
 public class DocumentUsecase extends AbstractUsecase {
 
