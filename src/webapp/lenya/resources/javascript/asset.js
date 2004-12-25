42b2d7a3870d57ad816b249544186c0446c092d2
Gregor Rothfuss <gregor@apache.org>
2004-12-25 15:23:27 +0000

    Added license and file:// prefix to image preview javascript code
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@123339 13f79535-47bb-0310-9956-ffa450edef68

--- src/webapp/lenya/resources/javascript/asset.js
+++ src/webapp/lenya/resources/javascript/asset.js
@@ -1,3 +1,21 @@
+/*
+* Copyright 1999-2004 The Apache Software Foundation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/* $Id$ */
+
 /* Show a preview of an image to be uploaded */
 function imagepreview(obj) {
    prev = document.getElementById('preview');
@@ -14,7 +32,7 @@ function imagepreview(obj) {
      } 
    } 
    if (isimage) { 
-     prev.setAttribute('src',obj.value);
+     prev.setAttribute('src','file://' + obj.value);
      prev.style.visibility = 'visible';
    }
 }
