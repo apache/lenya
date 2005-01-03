4328e6a3348fb4e2a05ba81ea77324a91e32cfcd
Andreas Hartmann <andreas@apache.org>
2005-01-03 15:00:14 +0000

    fixed some style issues
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@123980 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/ac/AccessControl.java
+++ src/java/org/apache/lenya/cms/ac/AccessControl.java
@@ -16,54 +16,33 @@
  */
 package org.apache.lenya.cms.ac;
 
-import java.util.Map;
-
-import org.apache.lenya.cms.publication.Document;
-import org.apache.lenya.cms.publication.DocumentBuildException;
 import org.apache.lenya.cms.usecase.UsecaseException;
 import org.apache.lenya.cms.admin.AccessControlUsecase;
 
-import org.apache.avalon.framework.context.Context;
-import org.apache.avalon.framework.context.ContextException;
-import org.apache.avalon.framework.context.Contextualizable;
-import org.apache.cocoon.components.ContextHelper;
-import org.apache.cocoon.environment.ObjectModelHelper;
-
-import org.apache.lenya.ac.Accreditable;
-import org.apache.lenya.ac.Group;
-import org.apache.lenya.ac.GroupManager;
-import org.apache.lenya.ac.IPRange;
-import org.apache.lenya.ac.IPRangeManager;
 import org.apache.lenya.ac.Item;
 import org.apache.lenya.ac.Role;
-import org.apache.lenya.ac.RoleManager;
-import org.apache.lenya.ac.User;
-import org.apache.lenya.ac.UserManager;
-import org.apache.lenya.ac.impl.Credential;
 import org.apache.lenya.cms.ac.cocoon.PolicyHelper;
-import org.apache.lenya.cms.ac.cocoon.CredentialWrapper;
-import org.apache.lenya.cms.publication.Publication;
-import org.apache.lenya.xml.DocumentHelper;
-import org.apache.lenya.workflow.Situation;
 
 /**
  * Usecase to display the AccessControl tab in the site area for a document.
- * This is a mix-in class that ideally would inherit both from AccessControlUsecase and DocumentUsecase.
- * FIXME i just took the appropriate code from DocumentUsecase, maybe its possible to have a saner
- * inheritance?
+ * This is a mix-in class that ideally would inherit both from
+ * AccessControlUsecase and DocumentUsecase. FIXME i just took the appropriate
+ * code from DocumentUsecase, maybe its possible to have a saner inheritance?
  * 
  * @version $Id$
  */
 
 public class AccessControl extends AccessControlUsecase {
-	
-	private PolicyHelper helper = null;
-    private Document sourceDocument = null;
+
+    private PolicyHelper helper = null;
+
     private Item[] items = null;
+
     private static String[] types = { "user", "group", "iprange", "role" };
+
     private static String[] operations = { "add", "delete" };
 
-	/**
+    /**
      * Ctor.
      */
     public AccessControl() {
@@ -71,23 +50,9 @@ public class AccessControl extends AccessControlUsecase {
     }
 
     /**
-     * @see org.apache.lenya.cms.usecase.Usecase#setup(java.lang.String,
-     *      org.apache.lenya.workflow.Situation)
-     */
-    public void setup(String sourceUrl, Situation situation) {
-        super.setup(sourceUrl, situation);
-        try {
-            this.sourceDocument = getUnitOfWork().getIdentityMap().getFactory().getFromURL(
-                    sourceUrl);
-        } catch (DocumentBuildException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    /**
      * @see org.apache.lenya.cms.usecase.AbstractUsecase#doInitialize()
      */
-    protected void doInitialize() throws Exception {
+    protected void doInitialize() {
         super.doInitialize();
         try {
 
@@ -98,65 +63,60 @@ public class AccessControl extends AccessControlUsecase {
                     visitorRole = roles[i].getId();
                 }
             }
-            
+
             setParameter("visitorRole", visitorRole);
-            
-            String area = getParameterAsString("area");
-  			helper = new PolicyHelper(getLogger());
-  	        Map objectModel = ContextHelper.getObjectModel(getContext());
-  	        //FIXME expects the component manager
-  	        // helper.setup(objectModel, this.manager, area);
-            
+
+            this.helper = new PolicyHelper(getLogger());
+            //FIXME expects the component manager
+            // helper.setup(objectModel, this.manager, area);
+
             for (int i = 0; i < types.length; i++) {
-      				Item[] items = null;
-      				
-      				if (types[i].equals("user")) {
-      						items = getUserManager().getUsers();
-      				}
-      				else if (types[i].equals("group")) {
-      						items = getGroupManager().getGroups();
-      				}
-      				else if (types[i].equals("iprange")) {
-      						items = getIpRangeManager().getIPRanges();
-      				}
-      				else if (types[i].equals("role")) {
-      						items = getRoleManager().getRoles();
-      				}
-      				for (int j = 0; j < operations.length; j++) {
-      					if (getParameterAsString(operations[j] + "_credential_" + types[i]) != null) {
-      							String roleId = getParameterAsString("role_id");
-      							
-      							String accreditableId = getParameterAsString("accreditable_id");
-      							Item item = null;
-      							for (int k = 0; k < items.length; k++) {
-      							    if (accreditableId.equals(items[k].getId())) {
-      							        item = items[k];
-      							    }
-      							}
-      							
-      							Role role = getRoleManager().getRole(roleId);
-      							
-      							if (role == null) {
-      							    addErrorMessage("Role [" + roleId + "] does not exist!");
-      							}
-      							
-      							helper.manipulateCredential(item, role, operations[j]);
-      					}
-      			}
+                Item[] items = null;
+
+                if (types[i].equals("user")) {
+                    items = getUserManager().getUsers();
+                } else if (types[i].equals("group")) {
+                    items = getGroupManager().getGroups();
+                } else if (types[i].equals("iprange")) {
+                    items = getIpRangeManager().getIPRanges();
+                } else if (types[i].equals("role")) {
+                    items = getRoleManager().getRoles();
+                }
+                for (int j = 0; j < operations.length; j++) {
+                    if (getParameterAsString(operations[j] + "_credential_" + types[i]) != null) {
+                        String roleId = getParameterAsString("role_id");
+
+                        String accreditableId = getParameterAsString("accreditable_id");
+                        Item item = null;
+                        for (int k = 0; k < items.length; k++) {
+                            if (accreditableId.equals(items[k].getId())) {
+                                item = items[k];
+                            }
+                        }
+
+                        Role role = getRoleManager().getRole(roleId);
+
+                        if (role == null) {
+                            addErrorMessage("Role [" + roleId + "] does not exist!");
+                        }
+
+                        this.helper.manipulateCredential(item, role, operations[j]);
+                    }
+                }
             }
-        
+
         } catch (Exception e) {
-        	addErrorMessage("Could not read a value.");
-        	getLogger().error("Could not read value for AccessControl usecase. " + e.toString());
+            addErrorMessage("Could not read a value.");
+            getLogger().error("Could not read value for AccessControl usecase. " + e.toString());
         }
     }
-            
-     /**
+
+    /**
      * Validates the request parameters.
      * @throws UsecaseException if an error occurs.
      */
     void validate() throws UsecaseException {
-   }
+    }
 
     /**
      * @see org.apache.lenya.cms.usecase.AbstractUsecase#doCheckExecutionConditions()
@@ -170,36 +130,35 @@ public class AccessControl extends AccessControlUsecase {
      */
     protected void doExecute() throws Exception {
         super.doExecute();
-		if (getParameterAsString("change_ssl") != null) {
-			if (getParameterAsString("ssl") != null) {
-					helper.setUrlSSLProtected(true);
-			}
-			else {
-					helper.setUrlSSLProtected(false);
-			}
-		}
+        if (getParameterAsString("change_ssl") != null) {
+            if (getParameterAsString("ssl") != null) {
+                this.helper.setUrlSSLProtected(true);
+            } else {
+                this.helper.setUrlSSLProtected(false);
+            }
+        }
 
     }
 
     /**
-     * @see org.apache.lenya.cms.usecase.Usecase#setParameter(java.lang.String, java.lang.Object)
+     * @see org.apache.lenya.cms.usecase.Usecase#setParameter(java.lang.String,
+     *      java.lang.Object)
      */
     public void setParameter(String name, Object value) {
         super.setParameter(name, value);
     }
 
-    
     /**
      * @return PolicyHelper the policy helper
      */
     public PolicyHelper getHelper() {
-    	return this.helper;
+        return this.helper;
     }
 
     /**
      * @return Item the item
      */
     public Item[] getItems() {
-    	return this.items;
+        return this.items;
     }
 }
