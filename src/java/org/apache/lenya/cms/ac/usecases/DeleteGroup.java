8e15e1c834e621355bbc4d4354eea77092189117
Andreas Hartmann <andreas@apache.org>
2005-01-04 16:54:34 +0000

    group admin using usecase framework
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@124123 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/ac/usecases/DeleteGroup.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright  1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.lenya.cms.ac.usecases;
+
+import org.apache.lenya.ac.Group;
+
+/**
+ * Usecase to delete a group.
+ *
+ * @version $Id:$
+ */
+public class DeleteGroup extends AccessControlUsecase {
+
+    /**
+     * @see org.apache.lenya.cms.usecase.AbstractUsecase#doExecute()
+     */
+    protected void doExecute() throws Exception {
+        super.doExecute();
+
+        String groupId = getParameterAsString(GroupProfile.ID);
+        Group group = getGroupManager().getGroup(groupId);
+        if (group == null) {
+            throw new RuntimeException("Group [" + groupId + "] not found.");
+        }
+        
+        getGroupManager().remove(group);
+        group.delete();
+    }
+}
