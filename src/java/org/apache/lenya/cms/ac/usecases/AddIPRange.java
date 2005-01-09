b17274d02f2d40d9f39644611fe8414ccc660de5
Gregor Rothfuss <gregor@apache.org>
2005-01-09 23:04:50 +0000

    Added ASL 2.0 license and set SVN properties for some new Java files that did not have them
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@124746 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/ac/usecases/AddIPRange.java
+++ src/java/org/apache/lenya/cms/ac/usecases/AddIPRange.java
@@ -1,9 +1,22 @@
 /*
- * Created on 04.01.2005
+ * Copyright  1999-2004 The Apache Software Foundation
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  */
+
+/* $Id$  */
+
 package org.apache.lenya.cms.ac.usecases;
 
 import java.io.File;
@@ -17,8 +30,6 @@ import org.apache.lenya.cms.usecase.UsecaseException;
 
 /**
  * Usecase to add an IP range.
- * 
- * @version $Id:$
  */
 public class AddIPRange extends AccessControlUsecase {
 
--- src/java/org/apache/lenya/cms/usecase/WorkflowUsecase.java
+++ src/java/org/apache/lenya/cms/usecase/WorkflowUsecase.java
@@ -1,9 +1,22 @@
 /*
- * Created on 03.01.2005
+ * Copyright  1999-2004 The Apache Software Foundation
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  */
+
+/* $Id$  */
+
 package org.apache.lenya.cms.usecase;
 
 import java.util.Map;
@@ -18,10 +31,6 @@ import org.apache.lenya.workflow.WorkflowException;
 import org.apache.lenya.workflow.WorkflowInstance;
 
 /**
- * @author nobby
- *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
  */
 public class WorkflowUsecase extends AbstractUsecase {
 
