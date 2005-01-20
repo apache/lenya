0a52fce41e3a1901ff2d9a439f9765aec068623d
Andreas Hartmann <andreas@apache.org>
2005-01-20 15:23:35 +0000

    usecase framework used to change navigation title
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@125781 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/site/usecases/ChangeLabel.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright  1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.lenya.cms.site.usecases;
+
+import org.apache.lenya.cms.publication.Document;
+import org.apache.lenya.cms.site.SiteException;
+import org.apache.lenya.cms.site.SiteManager;
+import org.apache.lenya.cms.usecase.DocumentUsecase;
+
+/**
+ * Change the label of a document.
+ * 
+ * @version $Id:$
+ */
+public class ChangeLabel extends DocumentUsecase {
+
+    protected static final String LABEL = "label";
+    protected static final String DOCUMENT_ID = "documentId";
+
+    /**
+     * @see org.apache.lenya.cms.usecase.AbstractUsecase#initParameters()
+     */
+    protected void initParameters() {
+        super.initParameters();
+        Document document = getSourceDocument();
+        setParameter(DOCUMENT_ID, document.getId());
+        try {
+            SiteManager manager = document.getPublication().getSiteManager(document
+                    .getIdentityMap());
+            setParameter(LABEL, manager.getLabel(document));
+        } catch (SiteException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * @see org.apache.lenya.cms.usecase.AbstractUsecase#doExecute()
+     */
+    protected void doExecute() throws Exception {
+        super.doExecute();
+
+        Document document = getSourceDocument();
+        SiteManager manager = document.getPublication().getSiteManager(document.getIdentityMap());
+
+        String label = getParameterAsString(LABEL);
+        manager.setLabel(document, label);
+    }
+}
