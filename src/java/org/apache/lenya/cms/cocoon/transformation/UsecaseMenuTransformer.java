82849dbc471c36e0597e035058c67a1ac3831e94
Andreas Hartmann <andreas@apache.org>
2005-02-23 22:24:16 +0000

    check for usecase preconditions in UsecaseMenuTransformer
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@155091 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/cocoon/transformation/UsecaseMenuTransformer.java
+++ src/java/org/apache/lenya/cms/cocoon/transformation/UsecaseMenuTransformer.java
@@ -20,6 +20,7 @@
 package org.apache.lenya.cms.cocoon.transformation;
 
 import java.io.IOException;
+import java.util.Enumeration;
 import java.util.Map;
 
 import org.apache.avalon.framework.activity.Disposable;
@@ -30,6 +31,7 @@ import org.apache.cocoon.ProcessingException;
 import org.apache.cocoon.environment.ObjectModelHelper;
 import org.apache.cocoon.environment.Request;
 import org.apache.cocoon.environment.SourceResolver;
+import org.apache.cocoon.servlet.multipart.Part;
 import org.apache.cocoon.transformation.AbstractSAXTransformer;
 import org.apache.lenya.ac.AccessControlException;
 import org.apache.lenya.ac.AccessController;
@@ -42,14 +44,16 @@ import org.apache.lenya.cms.ac.usecase.UsecaseAuthorizer;
 import org.apache.lenya.cms.publication.Publication;
 import org.apache.lenya.cms.publication.PublicationException;
 import org.apache.lenya.cms.publication.PublicationFactory;
+import org.apache.lenya.cms.usecase.Usecase;
+import org.apache.lenya.cms.usecase.UsecaseResolver;
 import org.apache.lenya.util.ServletHelper;
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.AttributesImpl;
 
 /**
- * This transformer disables menu items (by removing the href attribute)
- * which are not allowed with respect to the usecase policies.
+ * This transformer disables menu items (by removing the href attribute) which
+ * are not allowed with respect to the usecase policies.
  */
 public class UsecaseMenuTransformer extends AbstractSAXTransformer implements Disposable {
 
@@ -66,42 +70,113 @@ public class UsecaseMenuTransformer extends AbstractSAXTransformer implements Di
      */
     public static final String USECASE_ATTRIBUTE = "usecase";
     /**
+     * Comment for <code>HREF_ATTRIBUTE</code> The href attribute
+     */
+    public static final String HREF_ATTRIBUTE = "href";
+    /**
      * <code>NAMESPACE</code> The usecase namespace
      */
     public static final String NAMESPACE = "http://apache.org/cocoon/lenya/usecase/1.0";
 
-    /** (non-Javadoc)
-     * @see org.xml.sax.ContentHandler#startElement(java.lang.String, java.lang.String, java.lang.String, org.xml.sax.Attributes)
+    /**
+     * (non-Javadoc)
+     * @see org.xml.sax.ContentHandler#startElement(java.lang.String,
+     *      java.lang.String, java.lang.String, org.xml.sax.Attributes)
      */
     public void startElement(String uri, String localName, String raw, Attributes attr)
-        throws SAXException {
+            throws SAXException {
 
         Attributes attributes = attr;
 
-        if (this.authorizer != null && localName.equals(ITEM_ELEMENT)) {
-            String usecase = attr.getValue(NAMESPACE, USECASE_ATTRIBUTE);
+        UsecaseResolver usecaseResolver = null;
+        try {
+            usecaseResolver = (UsecaseResolver) this.manager.lookup(UsecaseResolver.ROLE);
 
-            // filter item if usecase not allowed 
-            if (usecase != null) {
-                getLogger().debug("Found usecase [" + usecase + "]");
+            if (this.authorizer != null && localName.equals(ITEM_ELEMENT)) {
+                String usecaseName = attr.getValue(NAMESPACE, USECASE_ATTRIBUTE);
 
-                try {
-                    if (!this.authorizer.authorizeUsecase(usecase, this.roles, this.publication)) {
-                        getLogger().debug("Usecase not authorized");
-                        int hrefIndex = attributes.getIndex("href");
-                        if (hrefIndex > -1) {
-                            attributes = new AttributesImpl(attr);
-                            ((AttributesImpl) attributes).removeAttribute(hrefIndex);
+                // filter item if usecase not allowed
+                if (usecaseName != null) {
+                    if (getLogger().isDebugEnabled()) {
+                        getLogger().debug("Found usecase [" + usecaseName + "]");
+                    }
+                    if (!this.authorizer
+                            .authorizeUsecase(usecaseName, this.roles, this.publication)) {
+                        if (getLogger().isDebugEnabled()) {
+                            getLogger().debug("Usecase not authorized");
+                        }
+                        attributes = removeHrefAttribute(attr);
+                    }
+                }
+
+                if (usecaseResolver.isRegistered(usecaseName)) {
+                    Usecase usecase = null;
+                    try {
+                        usecase = usecaseResolver.resolve(usecaseName);
+                        if (attr.getValue(HREF_ATTRIBUTE) != null) {
+                            passRequestParameters(usecase, attr.getValue(HREF_ATTRIBUTE));
+                        }
+                        usecase.checkPreconditions();
+                        if (!usecase.getErrorMessages().isEmpty()) {
+                            if (getLogger().isDebugEnabled()) {
+                                getLogger().debug("Usecase preconditions not complied");
+                            }
+                            attributes = removeHrefAttribute(attr);
+                        }
+                    } finally {
+                        if (usecase != null) {
+                            usecaseResolver.release(usecase);
                         }
                     }
-                } catch (final AccessControlException e) {
-                    throw new SAXException(e);
                 }
+            }
+        } catch (final Exception e) {
+            throw new SAXException(e);
+        } finally {
+            if (usecaseResolver != null) {
+                this.manager.release(usecaseResolver);
             }
         }
 
         super.startElement(uri, localName, raw, attributes);
 
+    }
+
+    /**
+     * Removes the <code>href</code> attribute.
+     * @param attr The original attributes.
+     * @return An attributes object.
+     */
+    protected Attributes removeHrefAttribute(Attributes attr) {
+        Attributes attributes = attr;
+        int hrefIndex = attributes.getIndex(HREF_ATTRIBUTE);
+        if (hrefIndex > -1) {
+            attributes = new AttributesImpl(attr);
+            ((AttributesImpl) attributes).removeAttribute(hrefIndex);
+        }
+        return attributes;
+    }
+
+    /**
+     * Pass the request parameters from the <code>href</code> attribute to the
+     * usecase handler.
+     * @param usecase The usecase handler.
+     * @param href The value of the <code>href</code> attribute.
+     */
+    void passRequestParameters(Usecase usecase, String href) {
+        int questionMarkIndex = href.indexOf("?");
+        if (questionMarkIndex > -1) {
+            String queryString = href.substring(questionMarkIndex + 1);
+            String[] nameValuePairs = queryString.split("&");
+            for (int i = 0; i < nameValuePairs.length; i++) {
+                String[] pair = nameValuePairs[i].split("=");
+                if (pair.length == 2) {
+                    String name = pair[0];
+                    String value = pair[1];
+                    usecase.setParameter(name, value);
+                }
+            }
+        }
     }
 
     private UsecaseAuthorizer authorizer;
@@ -111,11 +186,13 @@ public class UsecaseMenuTransformer extends AbstractSAXTransformer implements Di
     private AccessControllerResolver acResolver;
 
     /**
-     * @see org.apache.cocoon.sitemap.SitemapModelComponent#setup(org.apache.cocoon.environment.SourceResolver, java.util.Map, java.lang.String, org.apache.avalon.framework.parameters.Parameters)
+     * @see org.apache.cocoon.sitemap.SitemapModelComponent#setup(org.apache.cocoon.environment.SourceResolver,
+     *      java.util.Map, java.lang.String,
+     *      org.apache.avalon.framework.parameters.Parameters)
      */
     public void setup(SourceResolver _resolver, Map _objectModel, String src, Parameters _parameters)
-        throws ProcessingException, SAXException, IOException {
-        
+            throws ProcessingException, SAXException, IOException {
+
         super.setup(_resolver, _objectModel, src, _parameters);
 
         getLogger().debug("Setting up transformer");
@@ -132,19 +209,17 @@ public class UsecaseMenuTransformer extends AbstractSAXTransformer implements Di
             PublicationFactory factory = PublicationFactory.getInstance(getLogger());
             this.publication = factory.getPublication(_objectModel);
 
-            this.serviceSelector =
-                (ServiceSelector) this.manager.lookup(AccessControllerResolver.ROLE + "Selector");
-            this.acResolver =
-                (AccessControllerResolver) this.serviceSelector.select(
-                    AccessControllerResolver.DEFAULT_RESOLVER);
+            this.serviceSelector = (ServiceSelector) this.manager
+                    .lookup(AccessControllerResolver.ROLE + "Selector");
+            this.acResolver = (AccessControllerResolver) this.serviceSelector
+                    .select(AccessControllerResolver.DEFAULT_RESOLVER);
             getLogger().debug("Resolved AC resolver [" + this.acResolver + "]");
 
             String webappUrl = ServletHelper.getWebappURI(_request);
             AccessController accessController = this.acResolver.resolveAccessController(webappUrl);
 
             if (accessController instanceof DefaultAccessController) {
-                DefaultAccessController defaultAccessController =
-                    (DefaultAccessController) accessController;
+                DefaultAccessController defaultAccessController = (DefaultAccessController) accessController;
                 Authorizer[] authorizers = defaultAccessController.getAuthorizers();
                 for (int i = 0; i < authorizers.length; i++) {
                     if (authorizers[i] instanceof UsecaseAuthorizer) {
