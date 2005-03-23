7a7347a116eff055ff49e99d69687f724defcd48
Andreas Hartmann <andreas@apache.org>
2005-03-23 15:28:47 +0000

    added Users and Groups usecases
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@158782 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/ac/usecases/Users.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright  1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.lenya.cms.ac.usecases;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.apache.cocoon.components.ContextHelper;
+import org.apache.cocoon.environment.Request;
+import org.apache.cocoon.environment.Session;
+import org.apache.lenya.ac.Identity;
+import org.apache.lenya.ac.User;
+import org.apache.lenya.ac.UserType;
+
+/**
+ * Manage users.
+ * 
+ * @version $Id:$
+ */
+public class Users extends AccessControlUsecase {
+
+    protected static final String USERS = "users";
+    protected static final String CURRENT_USER = "currentUser";
+    protected static final String USER_TYPES = "userTypes";
+
+    /**
+     * @see org.apache.lenya.cms.usecase.AbstractUsecase#initParameters()
+     */
+    protected void initParameters() {
+        super.initParameters();
+
+        User[] users = getUserManager().getUsers();
+        List userList = new ArrayList();
+        userList.addAll(Arrays.asList(users));
+        Collections.sort(userList);
+        setParameter(USERS, userList);
+        
+        Request request = ContextHelper.getRequest(getContext());
+        Session session = request.getSession(false);
+        if (session != null) {
+            Identity identity = (Identity) session.getAttribute(Identity.class.getName());
+            if (identity != null) {
+                setParameter(CURRENT_USER, identity.getUser());
+            }
+        }
+        
+        UserType[] types = getUserManager().getUserTypes();
+        setParameter(USER_TYPES, Arrays.asList(types));
+    }
+}
