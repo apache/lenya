88661b7050603f14ecd2f5a97c18f4f170691e4e
Joachim Wolfgang Kaltz <jwkaltz@apache.org>
2005-04-14 15:18:29 +0000

    [minor change] changed index from i to j in inner loop - I assume this was a typo
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@161302 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/site/tree/TreeSiteManager.java
+++ src/java/org/apache/lenya/cms/site/tree/TreeSiteManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright  1999-2004 The Apache Software Foundation
+ * Copyright  1999-2005 The Apache Software Foundation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -144,7 +144,7 @@ public class TreeSiteManager extends AbstractSiteManager implements Serviceable 
                         Document version = resource.getIdentityMap().get(resource.getPublication(),
                                 resource.getArea(),
                                 descendant.getAbsoluteId(),
-                                labels[i].getLanguage());
+                                labels[j].getLanguage());
                         resources.add(version);
                         if (getLogger().isDebugEnabled()) {
                             getLogger().debug("    Descendant: [" + version + "]");
