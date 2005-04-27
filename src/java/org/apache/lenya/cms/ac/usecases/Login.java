bfdda387ba4a1c1b6ae77eb03dea8feb37e40b1c
Joachim Wolfgang Kaltz <jwkaltz@apache.org>
2005-04-27 15:22:48 +0000

    [minor change] fixed to use text in dictionary
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@164992 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/ac/usecases/Login.java
+++ src/java/org/apache/lenya/cms/ac/usecases/Login.java
@@ -1,5 +1,5 @@
 /*
- * Copyright  1999-2004 The Apache Software Foundation
+ * Copyright  1999-2005 The Apache Software Foundation
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -72,7 +72,7 @@ public class Login extends AccessControlUsecase {
         if (getAccessController().authenticate(request)) {
         	setTargetURL(request.getRequestURI());
 		} else {
-        	addErrorMessage("Authentication failed.");
+        	addErrorMessage("Authentication failed");
         }
    }
 
