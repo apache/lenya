0f14574f6a4109eab15ddc5e5d9f75320653c42e
Andreas Hartmann <andreas@apache.org>
2005-05-13 14:38:40 +0000

    implemented 'view IP range' usecase
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@170027 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/ac/usecases/IPRange.java
+++ src/java/org/apache/lenya/cms/ac/usecases/IPRange.java
@@ -18,9 +18,28 @@ package org.apache.lenya.cms.ac.usecases;
 
 /**
  * Show information about an IP range.
- *
+ * 
  * @version $Id:$
  */
 public class IPRange extends AccessControlUsecase {
 
+    protected static final String IP_RANGE_ID = "ipRangeId";
+    protected static final String IP_RANGE = "ipRange";
+
+    /**
+     * @see org.apache.lenya.cms.usecase.Usecase#setParameter(java.lang.String, java.lang.Object)
+     */
+    public void setParameter(String name, Object value) {
+        super.setParameter(name, value);
+
+        if (name.equals(IP_RANGE_ID)) {
+            String ipRangeId = (String) value;
+            org.apache.lenya.ac.IPRange ipRange = getIpRangeManager().getIPRange(ipRangeId);
+            if (ipRange == null) {
+                addErrorMessage("iprange_no_such_iprange", new String[] { ipRangeId });
+            } else {
+                setParameter(IP_RANGE, ipRange);
+            }
+        }
+    }
 }
