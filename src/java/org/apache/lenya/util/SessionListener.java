42a5df9b711bf7f12c55962d09f1a34686089079
Jann Forrer <jann@apache.org>
2005-06-17 11:54:54 +0000

    Add SessionListener class
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@191133 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/util/SessionListener.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright  1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.apache.lenya.util;
+
+/**
+ * A Listener which store the session object in a HashMap on creation and 
+ * remove it if the session is destroyed.
+ * 
+ * @version $Id:$
+ */
+
+import javax.servlet.http.*;
+import java.util.WeakHashMap;
+
+public class SessionListener implements HttpSessionListener {
+
+	private static WeakHashMap allSessions = new WeakHashMap();
+
+	public void sessionCreated(HttpSessionEvent se) {
+
+		HttpSession session = se.getSession();
+		String sessionID = session.getId();
+		allSessions.put(sessionID,session);				
+	}
+
+	public void sessionDestroyed(HttpSessionEvent se) {
+		HttpSession session = se.getSession();
+		String sessionID = session.getId();
+		if (allSessions.containsKey(sessionID)) {
+			allSessions.remove(sessionID);
+		}
+	}
+
+	public WeakHashMap getAllSessions() {
+		return allSessions;
+	}	
+}
