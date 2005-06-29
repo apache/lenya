d6cfc1e5f77e2bb3be84c38250fb9dcad36eec30
Andreas Hartmann <andreas@apache.org>
2005-06-29 09:16:50 +0000

    added ASF header
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@202342 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/transaction/Identifiable.java
+++ src/java/org/apache/lenya/transaction/Identifiable.java
@@ -1,16 +1,25 @@
 /*
- * Created on 08.04.2005
+ * Copyright  1999-2005 The Apache Software Foundation
  *
- * TODO To change the template for this generated file go to
- * Window - Preferences - Java - Code Style - Code Templates
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
  */
 package org.apache.lenya.transaction;
 
 /**
- * @author nobby
+ * An identifiable object.
  *
- * TODO To change the template for this generated type comment go to
- * Window - Preferences - Java - Code Style - Code Templates
+ * @version $Id:$
  */
 public interface Identifiable {
 
