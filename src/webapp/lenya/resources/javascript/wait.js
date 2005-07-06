2f98c68e960f85e17c92527fe41c5e91735e8289
Chevrier Edith <edith@apache.org>
2005-07-06 13:58:04 +0000

    Please Wait Message build while publishing, submiting, deactivating, copying, cuting, pasting, archiving, deletein, restoring
    git-svn-id: https://svn.apache.org/repos/asf/lenya/branches/BRANCH_1_2_X@209463 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/webapp/lenya/resources/javascript/wait.js
@@ -0,0 +1,35 @@
+/*
+* Copyright 1999-2004 The Apache Software Foundation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/*
+ * "Please Wait" message  
+ */
+  function submit_wait(fm)
+    {
+      document.getElementById("pleasewaitScreen").style.visibility = "visible";
+      window.setTimeout('submit_form(fm)',1);
+    }
+
+  function submit_form(form)
+    {
+      form.submit();
+      document.getElementById("pleasewaitScreen").style.visibility = "hidden";
+    }
+
+  function onClick_wait()
+    {
+      document.getElementById("pleasewaitScreen").style.visibility = "visible";
+    }
