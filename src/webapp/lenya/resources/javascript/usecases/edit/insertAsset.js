e6541b72d83cfb25fc46ef20c3a5dad6eca2657e
Chevrier Edith <edith@apache.org>
2006-01-18 10:17:36 +0000

    Scaling of images when they are inserted in a doc. With the call of svg module, the scaling will be made with svg.
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@370106 13f79535-47bb-0310-9956-ffa450edef68

--- src/webapp/lenya/resources/javascript/usecases/edit/insertAsset.js
+++ src/webapp/lenya/resources/javascript/usecases/edit/insertAsset.js
@@ -15,7 +15,9 @@
       var src = document.forms['image'].assetName.value;
       var title = document.forms['image'].caption.value;
       var type = document.forms['image'].type.value;
-      var content = '<object xmlns="'+window.opener.XHTMLNS+'" href="'+link+'" title="'+title+'" type="'+type+'" data="'+nodeid + '/' + src+'">'+src+'</object>'; 
+      var height = document.forms['image'].height.value;
+      var width = document.forms['image'].width.value;
+      var content = '<object xmlns="'+window.opener.XHTMLNS+'" href="'+link+'" title="'+title+'" type="'+type+'" data="'+nodeid + '/' + src+'" height="'+height+'" width="'+width+'">'+src+'</object>'; 
       window.opener.bxe_insertContent(content,window.opener.bxe_ContextNode); 
       window.close();
    }
@@ -27,3 +29,31 @@
      document.forms['image'].assetSize.value = size;
      focus(); 
    } 
+
+   function insertData(name, caption, type, size, height, width) { 
+     var ratio = 1;
+     if (width != 0) {
+       ratio = height / width;
+     }
+     document.forms['image'].assetName.value = name;
+     document.forms['image'].caption.value = caption;
+     document.forms['image'].type.value = type;
+     document.forms['image'].assetSize.value = size;
+     document.forms['image'].height.value = height;
+     document.forms['image'].width.value = width;
+     document.forms['image'].ratio.value = ratio;
+     focus(); 
+   } 
+
+   function scaleHeight(width) {
+     var ratio = document.forms['image'].ratio.value;
+     document.forms['image'].height.value = width * ratio;
+     focus(); 
+   } 
+  
+   function scaleWidth(height) {
+     var ratio = document.forms['image'].ratio.value;
+     document.forms['image'].width.value = height * ratio;
+     focus(); 
+   } 
+  
