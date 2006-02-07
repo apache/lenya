aeb6b219a95e4ff751065479932eea216a7bd090
Josias Thony <josias@apache.org>
2006-02-07 14:36:53 +0000

    added input module to get the path of the lucene search index, instead of hardcoding it in the sitemap
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@375611 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/modules/lucene/java/src/org/apache/cocoon/components/modules/input/IndexPathModule.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright  1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.apache.cocoon.components.modules.input;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.cocoon.components.search.Index;
+import org.apache.cocoon.components.search.components.IndexManager;
+import org.apache.lucene.store.FSDirectory;
+
+/**
+ * This module returns the directory path of a search index given by its id,
+ * which normally is "pubid-area".
+ */
+public class IndexPathModule extends AbstractInputModule implements Serviceable {
+
+    protected ServiceManager serviceManager;
+    
+    public void service(ServiceManager manager) throws ServiceException {
+        this.serviceManager = manager;
+    }
+
+    /**
+     * @see org.apache.cocoon.components.modules.input.InputModule#getAttribute(java.lang.String,
+     *      org.apache.avalon.framework.configuration.Configuration, java.util.Map)
+     */
+    public Object getAttribute(String name, Configuration modeConf, Map objectModel)
+            throws ConfigurationException {
+
+        String value = null;
+        
+        IndexManager indexManager = null;
+        try {
+            indexManager = (IndexManager) this.serviceManager.lookup(IndexManager.ROLE);
+            
+            Index index = indexManager.getIndex(name);
+            
+            if (index == null) {
+                throw new ConfigurationException("no search index with id [" + name + "] found.");
+            }
+            
+            value = ((FSDirectory)index.getDirectory()).getFile().getCanonicalPath();
+
+            if (getLogger().isDebugEnabled()) {
+                getLogger().debug("resolved search index with id [" + name + "] to directory " + value);
+            }
+        } catch (Exception e) {
+            throw new ConfigurationException("Resolving attribute [" + name + "] failed: ", e);
+        } finally {
+            if (indexManager != null) {
+                this.serviceManager.release(indexManager);
+            }
+        }
+        return value;
+    }
+
+    /**
+     * @see org.apache.cocoon.components.modules.input.InputModule#getAttributeNames(org.apache.avalon.framework.configuration.Configuration,
+     *      java.util.Map)
+     */
+    public Iterator getAttributeNames(Configuration modeConf, Map objectModel)
+            throws ConfigurationException {
+        return Collections.EMPTY_SET.iterator();
+    }
+
+    /**
+     * @see org.apache.cocoon.components.modules.input.InputModule#getAttributeValues(java.lang.String,
+     *      org.apache.avalon.framework.configuration.Configuration, java.util.Map)
+     */
+    public Object[] getAttributeValues(String name, Configuration modeConf, Map objectModel)
+            throws ConfigurationException {
+        Object[] objects = { getAttribute(name, modeConf, objectModel) };
+
+        return objects;
+    }
+
+}
