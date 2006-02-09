4d32b5d5cc4e8758bcd831fbd7b8ed18b2a975eb
Thorsten Scherler <thorsten@apache.org>
2006-02-09 11:14:01 +0000

    formating changes
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@376262 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/metadata/usecases/Metadata.java
+++ src/java/org/apache/lenya/cms/metadata/usecases/Metadata.java
@@ -28,77 +28,81 @@ import org.apache.lenya.cms.usecase.UsecaseException;
  */
 public class Metadata extends SiteUsecase {
 
-    /**
-     * Ctor.
-     */
-    public Metadata() {
-        super();
-    }
+  /**
+   * Ctor.
+   */
+  public Metadata() {
+    super();
+  }
 
-    /**
-     * @see org.apache.lenya.cms.usecase.AbstractUsecase#getNodesToLock()
-     */
-    protected Node[] getNodesToLock() throws UsecaseException {
-        Node[] objects = { getSourceDocument().getRepositoryNode() };
-        return objects;
-    }
+  /**
+   * @see org.apache.lenya.cms.usecase.AbstractUsecase#getNodesToLock()
+   */
+  protected Node[] getNodesToLock() throws UsecaseException {
+    Node[] objects = { getSourceDocument().getRepositoryNode() };
+    return objects;
+  }
 
-    /**
-     * @see org.apache.lenya.cms.usecase.AbstractUsecase#initParameters()
-     */
-    protected void initParameters() {
-        super.initParameters();
+  /**
+   * @see org.apache.lenya.cms.usecase.AbstractUsecase#initParameters()
+   */
+  protected void initParameters() {
+    super.initParameters();
 
-        try {
-            MetaData meta = getSourceDocument().getMetaDataManager().getDublinCoreMetaData();
+    try {
+      MetaData meta = getSourceDocument().getMetaDataManager()
+          .getDublinCoreMetaData();
 
-            String[] keys = meta.getPossibleKeys();
-            for (int i = 0; i < keys.length; i++) {
-                String value = meta.getFirstValue(keys[i]);
-                if (value != null) {
-                    setParameter(keys[i], value);
-                }
-            }
-
-        } catch (Exception e) {
-            getLogger().error("Unable to load Dublin Core metadata.", e);
-            addErrorMessage("Unable to load Dublin Core metadata.");
+      String[] keys = meta.getPossibleKeys();
+      for (int i = 0; i < keys.length; i++) {
+        String value = meta.getFirstValue(keys[i]);
+        if (value != null) {
+          setParameter(keys[i], value);
         }
+      }
+
+    } catch (Exception e) {
+      getLogger().error("Unable to load Dublin Core metadata.", e);
+      addErrorMessage("Unable to load Dublin Core metadata.");
     }
+  }
 
-    /**
-     * Validates the request parameters.
-     * @throws UsecaseException if an error occurs.
-     */
-    void validate() throws UsecaseException {
-        // do nothing
+  /**
+   * Validates the request parameters.
+   * 
+   * @throws UsecaseException
+   *           if an error occurs.
+   */
+  void validate() throws UsecaseException {
+    // do nothing
+  }
+
+  /**
+   * @see org.apache.lenya.cms.usecase.AbstractUsecase#doCheckExecutionConditions()
+   */
+  protected void doCheckExecutionConditions() throws Exception {
+    validate();
+  }
+
+  /**
+   * @see org.apache.lenya.cms.usecase.AbstractUsecase#doExecute()
+   */
+  protected void doExecute() throws Exception {
+    super.doExecute();
+
+    MetaData meta = getSourceDocument().getMetaDataManager()
+        .getDublinCoreMetaData();
+
+    String[] keys = meta.getPossibleKeys();
+    for (int i = 0; i < keys.length; i++) {
+      String value = getParameterAsString(keys[i]);
+      if (value != null) {
+        meta.setValue(keys[i], value);
+      }
     }
+    meta.save();
 
-    /**
-     * @see org.apache.lenya.cms.usecase.AbstractUsecase#doCheckExecutionConditions()
-     */
-    protected void doCheckExecutionConditions() throws Exception {
-        validate();
-    }
-
-    /**
-     * @see org.apache.lenya.cms.usecase.AbstractUsecase#doExecute()
-     */
-    protected void doExecute() throws Exception {
-        super.doExecute();
-
-        MetaData meta = getSourceDocument().getMetaDataManager().getDublinCoreMetaData();
-
-        String[] keys = meta.getPossibleKeys();
-        for (int i = 0; i < keys.length; i++) {
-            String value = getParameterAsString(keys[i]);
-            if (value != null) {
-                meta.setValue(keys[i], value);
-            }
-        }
-        meta.save();
-
-        //TODO set workflow situation to edit here.
-    }
+    // TODO set workflow situation to edit here.
+  }
 
 }
