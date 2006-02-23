1e133fe658a1ecc156f0ce200d4919067de5cbb3
Michael Wechner <michi@apache.org>
2006-02-23 13:57:36 +0000

    jcrsource moved
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@380116 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/modules-optional/jcrsource/java/src/org/apache/lenya/cms/jcr/JCRNodeFactory.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright  1999-2005 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.lenya.cms.jcr;
+
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.lenya.cms.repository.Node;
+import org.apache.lenya.cms.repository.NodeFactory;
+import org.apache.lenya.cms.repository.Session;
+import org.apache.lenya.transaction.Identifiable;
+import org.apache.lenya.transaction.IdentityMap;
+
+/**
+ * JCR node factory.
+ */
+public class JCRNodeFactory extends AbstractLogEnabled implements NodeFactory, Serviceable {
+
+    public Identifiable build(IdentityMap map, String key) throws Exception {
+        return new JCRSourceNode(this.session, key, this.manager, getLogger());
+    }
+
+    public String getType() {
+        return Node.IDENTIFIABLE_TYPE;
+    }
+
+    private ServiceManager manager;
+
+    public void service(ServiceManager manager) throws ServiceException {
+        this.manager = manager;
+    }
+    
+    private Session session;
+
+    public void setSession(Session session) {
+        this.session = session;
+    }
+
+}
