3117f15cabfd4e78e4d2961ae2fb6639f990e518
Michael Wechner <michi@apache.org>
2006-03-13 15:30:26 +0000

    existsInAnyLanguage simplified because it is duplicating code
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@385571 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/publication/DefaultDocument.java
+++ src/java/org/apache/lenya/cms/publication/DefaultDocument.java
@@ -35,12 +35,15 @@ import org.apache.lenya.cms.metadata.MetaDataManager;
 import org.apache.lenya.cms.publication.util.DocumentVisitor;
 import org.apache.lenya.cms.repository.Node;
 import org.apache.lenya.cms.site.SiteManager;
+import org.apache.log4j.Category;
 
 /**
  * A typical CMS document.
  * @version $Id$
  */
 public class DefaultDocument extends AbstractLogEnabled implements Document {
+
+    private Category log = Category.getInstance(DefaultDocument.class);
 
     private DocumentIdentifier identifier;
     private String sourceURI;
@@ -134,11 +137,15 @@ public class DefaultDocument extends AbstractLogEnabled implements Document {
         List documentLanguages = new ArrayList();
         String[] allLanguages = getPublication().getLanguages();
 
+        log.debug("Number of languages of this publication: " + allLanguages.length);
+
         for (int i = 0; i < allLanguages.length; i++) {
             Document version;
             try {
+                log.debug("Try to create document: " + allLanguages[i] + " " + this);
                 version = getIdentityMap().getLanguageVersion(this, allLanguages[i]);
             } catch (DocumentBuildException e) {
+                log.warn(e.getMessage());
                 throw new DocumentException(e);
             }
             if (version.exists()) {
@@ -191,6 +198,9 @@ public class DefaultDocument extends AbstractLogEnabled implements Document {
         return this.extension;
     }
 
+    /**
+     * @see org.apache.lenya.cms.publication.Document#getSourceExtension()
+     */
     public String getSourceExtension() {
         String extension;
         try {
@@ -200,6 +210,7 @@ public class DefaultDocument extends AbstractLogEnabled implements Document {
             throw new RuntimeException(e);
         }
         if (extension == null) {
+            getLogger().warn("No source extension. The extension \"xml\" will be used as default!");
             extension = "xml";
         }
         return extension;
@@ -264,6 +275,20 @@ public class DefaultDocument extends AbstractLogEnabled implements Document {
     public boolean existsInAnyLanguage() throws DocumentException {
         boolean exists = false;
 
+        String[] languages = getLanguages();
+
+        if (languages.length > 0) {
+                log.warn("Document (" + this + ") exists in at least one language: " + languages.length);
+                String[] allLanguages = getPublication().getLanguages();
+                if (languages.length == allLanguages.length) log.warn("Document (" + this + ") exists even in all languages of this publication");
+                return true;
+            } else {
+                log.warn("Document (" + this + ") does NOT exist in any language");
+                return false;
+            }
+
+// NOTE: This seems to be unecessary because getLanguage() already creates these documents
+/*
         try {
             String[] languages = getLanguages();
             for (int i = 0; i < languages.length; i++) {
@@ -274,6 +299,7 @@ public class DefaultDocument extends AbstractLogEnabled implements Document {
             throw new DocumentException(e);
         }
         return exists;
+*/
     }
 
     protected DocumentIdentifier getIdentifier() {
@@ -341,16 +367,22 @@ public class DefaultDocument extends AbstractLogEnabled implements Document {
      * @see org.apache.lenya.cms.metadata.MetaDataOwner#getMetaDataManager()
      */
     public MetaDataManager getMetaDataManager() {
+        log.debug("getSourceURI(): " + getPublication().getSourceURI());
         if (this.metaDataManager == null) {
             SourceResolver resolver = null;
             RepositorySource source = null;
             try {
                 resolver = (SourceResolver) this.manager.lookup(SourceResolver.ROLE);
+
+                // TODO: This needs to be improved ...
                 String sourceUri = getPublication().getSourceURI() + "/content/" + getArea()
                         + getId() + "/index_" + getLanguage() + ".xml";
+
+                log.debug("Source URI: " + sourceUri);
                 source = (RepositorySource) resolver.resolveURI(sourceUri);
                 this.metaDataManager = source.getNode().getMetaDataManager();
             } catch (Exception e) {
+                log.warn(e.getMessage());
                 throw new RuntimeException(e);
             } finally {
                 if (resolver != null) {
