9aca8500a66b5dacd7162b5cc5156ce158e2dd79
Andreas Hartmann <andreas@apache.org>
2006-05-17 13:28:37 +0000

    Added missing UsecaseAuthorizer after separating API from implementation (sorry)
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@407253 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/ac/usecase/UsecaseAuthorizer.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright  1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+
+package org.apache.lenya.cms.ac.usecase;
+
+import org.apache.lenya.ac.AccessControlException;
+import org.apache.lenya.ac.Authorizer;
+import org.apache.lenya.ac.Role;
+import org.apache.lenya.cms.publication.Publication;
+
+/**
+ * Authorizer for usecases.
+ * @version $Id: UsecaseAuthorizer.java 392449 2006-04-07 23:20:38Z michi $
+ */
+public interface UsecaseAuthorizer extends Authorizer {
+
+    /**
+     * Authorizes a usecase.
+     * 
+     * @param usecase The usecase ID.
+     * @param roles The roles of the current identity.
+     * @param publication The publication.
+     * @param requestURI The request URI.
+     * @return A boolean value.
+     * @throws AccessControlException when something went wrong.
+     */
+    public boolean authorizeUsecase(String usecase, Role[] roles, Publication publication,
+            String requestURI) throws AccessControlException;
+}
