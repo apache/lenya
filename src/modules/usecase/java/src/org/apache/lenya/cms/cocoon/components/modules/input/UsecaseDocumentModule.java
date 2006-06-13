572e5cdd0e06a856ff567dafa76132ef5665ebc7
Andreas Hartmann <andreas@apache.org>
2006-06-13 13:46:54 +0000

    Added usecase resource type module - this allows to execute usecases as documents.
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@413894 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/modules/usecase/java/src/org/apache/lenya/cms/cocoon/components/modules/input/UsecaseDocumentModule.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright  1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.lenya.cms.cocoon.components.modules.input;
+
+import java.util.Map;
+
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.cocoon.components.modules.input.AbstractInputModule;
+import org.apache.cocoon.environment.ObjectModelHelper;
+import org.apache.cocoon.environment.Request;
+import org.apache.lenya.cms.cocoon.source.SourceUtil;
+import org.apache.lenya.cms.publication.Document;
+import org.apache.lenya.cms.publication.DocumentUtil;
+import org.apache.lenya.cms.site.usecases.CreateUsecaseDocument;
+
+/**
+ * Module to retrieve information from a usecase resource type document.
+ */
+public class UsecaseDocumentModule extends AbstractInputModule implements Serviceable {
+
+    protected static final String USECASE = "usecase";
+
+    public Object getAttribute(String name, Configuration modeConf, Map objectModel)
+            throws ConfigurationException {
+
+        Object value = null;
+        try {
+            if (name.equals(USECASE)) {
+                Request request = ObjectModelHelper.getRequest(objectModel);
+                Document doc = DocumentUtil.getCurrentDocument(this.manager, request);
+                org.w3c.dom.Document xmlDoc = SourceUtil.readDOM(doc.getSourceURI(), this.manager);
+                String usecaseName = xmlDoc.getDocumentElement().getAttribute(CreateUsecaseDocument.ATTRIBUTE_NAME);
+                value = usecaseName;
+            }
+        } catch (Exception e) {
+            throw new ConfigurationException("Could not obtain value: ", e);
+        }
+        return value;
+    }
+
+    protected ServiceManager manager;
+
+    public void service(ServiceManager manager) throws ServiceException {
+        this.manager = manager;
+    }
+
+}
---
+++ src/modules/usecase/java/src/org/apache/lenya/cms/site/usecases/CreateUsecaseDocument.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright  1999-2005 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.lenya.cms.site.usecases;
+
+import java.util.Arrays;
+
+import org.apache.lenya.cms.cocoon.source.SourceUtil;
+import org.apache.lenya.cms.usecase.UsecaseResolver;
+import org.apache.lenya.xml.NamespaceHelper;
+import org.w3c.dom.Element;
+
+/**
+ * <p>
+ * Create a usecase document. The document content looks like this:
+ * </p>
+ * 
+ * <pre>
+ *   &lt;usecase name=&quot;...&quot;/&gt;
+ * </pre>
+ */
+public class CreateUsecaseDocument extends CreateDocument {
+
+    protected static final String USECASE = "usecase";
+    protected static final String RESOURCE_TYPE_USECASE = "usecase";
+
+    /**
+     * The namespace for usecase document content.
+     */
+    public static final String NAMESPACE = "http://apache.org/lenya/usecase/1.0";
+
+    /**
+     * The local name of the usecase XML element.
+     */
+    public static final String ELEMENT_USECASE = "usecase";
+
+    /**
+     * The local name of the name attribute.
+     */
+    public static final String ATTRIBUTE_NAME = "name";
+    
+    protected void initParameters() {
+        super.initParameters();
+        setParameter(DOCUMENT_TYPE, RESOURCE_TYPE_USECASE);
+        setParameter(RESOURCE_TYPES, Arrays.asList(new String[0]));
+    }
+
+    protected void doCheckExecutionConditions() throws Exception {
+        super.doCheckExecutionConditions();
+
+        String usecaseName = getParameterAsString(USECASE);
+        if (usecaseName.equals("")) {
+            addErrorMessage("Please enter a usecase name.");
+        } else {
+            UsecaseResolver resolver = null;
+            try {
+                resolver = (UsecaseResolver) this.manager.lookup(UsecaseResolver.ROLE);
+                if (!resolver.isRegistered(getSourceURL(), usecaseName)) {
+                    addErrorMessage("The usecase '" + usecaseName + "' is not registered.");
+                }
+            } finally {
+                if (resolver != null) {
+                    this.manager.release(resolver);
+                }
+            }
+        }
+    }
+
+    protected void doExecute() throws Exception {
+        super.doExecute();
+
+        NamespaceHelper helper = new NamespaceHelper(NAMESPACE, "", ELEMENT_USECASE);
+        Element usecaseElement = helper.getDocument().getDocumentElement();
+        usecaseElement.setAttribute(ATTRIBUTE_NAME, getParameterAsString(USECASE));
+        SourceUtil.writeDOM(helper.getDocument(), getNewDocument().getSourceURI(), this.manager);
+
+    }
+
+}
