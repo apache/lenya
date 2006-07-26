0250a5db5bbc4454135c25207256d96d3ba3ec76
Andreas Hartmann <andreas@apache.org>
2006-07-26 11:23:46 +0000

    Throw exception instead of System.out when InetAddressUtilTest has errors
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@425705 13f79535-47bb-0310-9956-ffa450edef68

--- src/impl/test/org/apache/lenya/net/InetAddressUtilTest.java
+++ src/impl/test/org/apache/lenya/net/InetAddressUtilTest.java
@@ -1,21 +1,22 @@
 /*
-* Copyright 1999-2004 The Apache Software Foundation
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 1999-2004 The Apache Software Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.lenya.net;
 
 import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import junit.framework.TestCase;
 
@@ -23,23 +24,19 @@ import junit.framework.TestCase;
  * Inet address util test
  */
 public class InetAddressUtilTest extends TestCase {
-    
-    public void testInetAddressUtil() {
-        
+
+    public void testInetAddressUtil() throws UnknownHostException {
+
         String[] args = { "195.226.6.64", "255.255.255.0", "195.226.6.70" };
-        
+
         if (args.length != 3) {
             System.out.println("Usage: InetAddressUtilTest network subnet ip");
             return;
         }
 
-        try {
-            InetAddress network = InetAddress.getByName(args[0]); // "195.226.6.64");
-            InetAddress subnet = InetAddress.getByName(args[1]); // "255.255.255.0");
-            InetAddress ip = InetAddress.getByName(args[2]); // "195.226.6.70");
-            System.out.println(InetAddressUtil.contains(network, subnet, ip));
-        } catch(Exception e) {
-            System.err.println(e);
-        }
+        InetAddress network = InetAddress.getByName(args[0]); // "195.226.6.64");
+        InetAddress subnet = InetAddress.getByName(args[1]); // "255.255.255.0");
+        InetAddress ip = InetAddress.getByName(args[2]); // "195.226.6.70");
+        System.out.println(InetAddressUtil.contains(network, subnet, ip));
     }
 }
