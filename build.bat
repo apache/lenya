050266300e318d40981f3bceb105cd2d98c2feea
Thorsten Scherler <thorsten@apache.org>
2006-07-28 08:58:29 +0000

    ASF Bugzilla Bug 34191 - JDK 1.5 build fails with NoClassDefFoundError: org/apache/xpath/XPathAPI Applying patch from Michael Wohlfart to fix Windows Build (the last remaining issue with 1.5). Thx Michael and everyone else who help solving the issue.
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@426442 13f79535-47bb-0310-9956-ffa450edef68

--- build.bat
+++ build.bat
@@ -19,17 +19,22 @@ rem
 rem $Id: build.bat 54525 2004-10-11 09:39:04Z cziegeler $
 rem ----------------------------------------------------------------------------
 
+
+set COCOON_HOME=externals\cocoon_2_1_x
+set COCOON_ENDORSED_DIR=%COCOON_HOME%\lib\endorsed
+
 rem ----- Ignore system CLASSPATH variable
 set OLD_CLASSPATH=%CLASSPATH%
 set CLASSPATH=
 for %%i in (lib\*.jar) do call tools\bin\appendcp.bat %%i
+for %%i in (%COCOON_ENDORSED_DIR%\*.jar) do call tools\bin\appendcp.bat %%i
 
 rem ----- Use Ant shipped with Cocoon. Ignore installed in the system Ant
 set OLD_ANT_HOME=%ANT_HOME%
 set ANT_HOME=tools
 
-call %ANT_HOME%\bin\ant -Djava.endorsed.dirs=lib\endorsed -logger org.apache.tools.ant.NoBannerLogger -emacs %1 %2 %3 %4 %5 %6 %7 %8 %9
-
+call %ANT_HOME%\bin\ant -Djava.endorsed.dirs=%COCOON_ENDORSED_DIR% -logger org.apache.tools.ant.NoBannerLogger -emacs %1 %2 %3 %4 %5 %6 %7 %8 %9
+ 
 rem ----- Restore ANT_HOME and ANT_OPTS
 set ANT_HOME=%OLD_ANT_HOME%
 set OLD_ANT_HOME=
