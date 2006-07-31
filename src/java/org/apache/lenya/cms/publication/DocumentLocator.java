859f17423246cb7cc01a9b8a092ddab777b6d3b7
Andreas Hartmann <andreas@apache.org>
2006-07-31 13:31:17 +0000

    [minor change] added Javadocs
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@427115 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/publication/DocumentLocator.java
+++ src/java/org/apache/lenya/cms/publication/DocumentLocator.java
@@ -19,12 +19,22 @@ package org.apache.lenya.cms.publication;
 import java.util.Map;
 import java.util.WeakHashMap;
 
-import org.apache.lenya.cms.site.SiteNode;
-
+/**
+ * A DocumentLocator describes a document based on its path in the site structure. The actual
+ * document doesn't have to exist.
+ */
 public class DocumentLocator {
 
     private static Map locators = new WeakHashMap();
 
+    /**
+     * Returns a specific document locator.
+     * @param pubId The publication ID.
+     * @param area The area of the document.
+     * @param path The path of the document in the site structure.
+     * @param language The language of the document.
+     * @return A document locator.
+     */
     public static DocumentLocator getLocator(String pubId, String area, String path, String language) {
         String key = DocumentLocator.getKey(pubId, area, path, language);
         DocumentLocator locator = null;
@@ -53,33 +63,65 @@ public class DocumentLocator {
         this.language = language;
     }
 
+    /**
+     * @return The area of the document.
+     */
     public String getArea() {
         return area;
     }
 
+    /**
+     * @return The language of the document.
+     */
     public String getLanguage() {
         return language;
     }
 
+    /**
+     * @return The path of the document in the site structure.
+     */
     public String getPath() {
         return path;
     }
 
+    /**
+     * @return The publication ID.
+     */
     public String getPublicationId() {
         return pubId;
     }
 
+    /**
+     * Returns a locator with the same publication ID, area, and language, but a different path in
+     * the site structure.
+     * @param path The path.
+     * @return A document locator.
+     */
     public DocumentLocator getPathVersion(String path) {
         return DocumentLocator.getLocator(getPublicationId(), getArea(), path, getLanguage());
     }
 
+    /**
+     * Returns a descendant of this locator.
+     * @param relativePath The relative path which must not begin with a slash and must not be
+     *            empty.
+     * @return A document locator.
+     */
     public DocumentLocator getDescendant(String relativePath) {
         if (relativePath.length() == 0) {
             throw new IllegalArgumentException("The relative path must not be empty!");
         }
+        if (relativePath.startsWith("/")) {
+            throw new IllegalArgumentException("The relative path must not start with a slash!");
+        }
         return getPathVersion(getPath() + "/" + relativePath);
     }
 
+    /**
+     * Returns a child of this locator.
+     * @param step The relative path to the child, it must not contain a slash.
+     * @return A document locator.
+     */
     public DocumentLocator getChild(String step) {
         if (step.indexOf("/") > -1) {
             throw new IllegalArgumentException("The step [" + step + "] must not contain a slash!");
@@ -87,6 +129,10 @@ public class DocumentLocator {
         return getDescendant(step);
     }
 
+    /**
+     * Returns the parent of this locator.
+     * @return A document locator or <code>null</code> if this is the root locator.
+     */
     public DocumentLocator getParent() {
         int lastSlashIndex = getPath().lastIndexOf("/");
         if (lastSlashIndex > -1) {
@@ -97,6 +143,11 @@ public class DocumentLocator {
         }
     }
 
+    /**
+     * Returns the parent of this locator.
+     * @param defaultPath The path of the locator to return if this is the root locator.
+     * @return A document locator.
+     */
     public DocumentLocator getParent(String defaultPath) {
         DocumentLocator parent = getParent();
         if (parent != null) {
@@ -106,6 +157,12 @@ public class DocumentLocator {
         }
     }
 
+    /**
+     * Returns a locator with the same publication ID, area, and path, but with a different
+     * language.
+     * @param language The language.
+     * @return A document locator.
+     */
     public DocumentLocator getLanguageVersion(String language) {
         return DocumentLocator.getLocator(getPublicationId(), getArea(), getPath(), language);
     }
@@ -130,6 +187,12 @@ public class DocumentLocator {
         return getKey();
     }
 
+    /**
+     * Returns a locator with the same publication ID, path, and language, but with a different
+     * area.
+     * @param area The area.
+     * @return A document locator.
+     */
     public DocumentLocator getAreaVersion(String area) {
         return DocumentLocator.getLocator(getPublicationId(), area, getPath(), getLanguage());
     }
