9bfebe01c478893d63e22a66c7a8ec1f4d8db7bc
Andreas Hartmann <andreas@apache.org>
2006-08-03 19:39:32 +0000

    Added content janitor (cleans up empty directories)
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@428496 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/modules-core/janitor/java/src/org/apache/lenya/cms/janitor/ContentJanitor.java
@@ -0,0 +1,58 @@
+package org.apache.lenya.cms.janitor;
+
+import java.io.File;
+
+import org.apache.avalon.framework.activity.Startable;
+import org.apache.avalon.framework.logger.AbstractLogEnabled;
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.avalon.framework.thread.ThreadSafe;
+import org.apache.lenya.cms.cocoon.source.SourceUtil;
+import org.apache.lenya.cms.observation.ObservationRegistry;
+import org.apache.lenya.cms.observation.RepositoryEvent;
+import org.apache.lenya.cms.observation.RepositoryListener;
+import org.apache.lenya.cms.publication.Document;
+
+/**
+ * The content janitor cleans up empty directories after a document is removed.
+ */
+public class ContentJanitor extends AbstractLogEnabled implements Serviceable, Startable,
+        ThreadSafe, RepositoryListener {
+
+    public void documentChanged(RepositoryEvent event) {
+    }
+
+    public void documentRemoved(RepositoryEvent event) {
+        Document doc = event.getDocument();
+        File contentFile = doc.getPublication().getContentDirectory(doc.getArea());
+        String contentUri = contentFile.toURI().toString();
+        try {
+            SourceUtil.deleteEmptyCollections(contentUri, this.manager);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void start() throws Exception {
+        ObservationRegistry registry = null;
+        try {
+            registry = (ObservationRegistry) this.manager.lookup(ObservationRegistry.ROLE);
+            registry.registerListener(this);
+        } finally {
+            if (registry != null) {
+                this.manager.release(registry);
+            }
+        }
+    }
+
+    public void stop() throws Exception {
+    }
+
+    private ServiceManager manager;
+
+    public void service(ServiceManager manager) throws ServiceException {
+        this.manager = manager;
+    }
+
+}
