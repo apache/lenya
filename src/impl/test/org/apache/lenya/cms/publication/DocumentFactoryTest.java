33e8f61b3c97ff5daa5d0acfc40b936e6eeaa08c
Andreas Hartmann <andreas@apache.org>
2006-08-11 14:02:41 +0000

    Added DocumentFactoryTest
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@430792 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/impl/test/org/apache/lenya/cms/publication/DocumentFactoryTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright  1999-2004 The Apache Software Foundation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.lenya.cms.publication;
+
+import org.apache.lenya.ac.impl.AbstractAccessControlTest;
+import org.apache.lenya.cms.repository.RepositoryUtil;
+import org.apache.lenya.cms.repository.Session;
+
+/**
+ * Document factory test.
+ */
+public class DocumentFactoryTest extends AbstractAccessControlTest {
+
+    /**
+     * Tests the meta data.
+     * @throws Exception
+     */
+    public void testDocumentFactory() throws Exception {
+        Session session = RepositoryUtil.getSession(getManager(), getRequest());
+        DocumentFactory factoryA = DocumentUtil.createDocumentIdentityMap(getManager(), session);
+
+        Publication publication = PublicationUtil.getPublication(getManager(), "test");
+        
+        Document docA1 = factoryA.get(publication, Publication.AUTHORING_AREA, "/index", "en");
+        Document docA2 = factoryA.get(publication, Publication.AUTHORING_AREA, "/index", "en");
+        assertSame(docA1, docA2);
+        
+        DocumentFactory factoryB = DocumentUtil.createDocumentIdentityMap(getManager(), session);
+
+        Document docB1 = factoryB.get(publication, Publication.AUTHORING_AREA, "/index", "en");
+        
+        assertSame(docA1, docB1);
+    }
+}
