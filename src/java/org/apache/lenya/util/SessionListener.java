c10561b34d8a33d0df0064f6eb0eb86781a5988c
Gregor Rothfuss <gregor@apache.org>
2006-11-12 03:51:14 +0000

    change to new ASF source header
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@473861 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/util/SessionListener.java
+++ src/java/org/apache/lenya/util/SessionListener.java
@@ -1,9 +1,10 @@
 /*
- * Copyright  1999-2004 The Apache Software Foundation
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -21,7 +22,7 @@ package org.apache.lenya.util;
  * A Listener which store the session object in a HashMap on creation and 
  * remove it if the session is destroyed.
  * 
- * @version $Id:$
+ * @version $Id$
  */
 
 import javax.servlet.http.*;
