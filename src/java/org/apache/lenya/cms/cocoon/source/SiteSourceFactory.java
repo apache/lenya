09358773d0ecdb2b858ff427359bb3eecb82217b
Andreas Hartmann <andreas@apache.org>
2007-03-22 16:35:18 +0000

    Introduce separated SiteSource class to allow checking for existence
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@521330 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/cocoon/source/SiteSourceFactory.java
+++ src/java/org/apache/lenya/cms/cocoon/source/SiteSourceFactory.java
@@ -30,7 +30,6 @@ import org.apache.avalon.framework.context.Contextualizable;
 import org.apache.avalon.framework.logger.AbstractLogEnabled;
 import org.apache.avalon.framework.service.ServiceException;
 import org.apache.avalon.framework.service.ServiceManager;
-import org.apache.avalon.framework.service.ServiceSelector;
 import org.apache.avalon.framework.service.Serviceable;
 import org.apache.avalon.framework.thread.ThreadSafe;
 import org.apache.cocoon.components.ContextHelper;
@@ -39,18 +38,6 @@ import org.apache.cocoon.environment.Request;
 import org.apache.excalibur.source.Source;
 import org.apache.excalibur.source.SourceException;
 import org.apache.excalibur.source.SourceFactory;
-import org.apache.excalibur.source.SourceResolver;
-import org.apache.lenya.cms.publication.Document;
-import org.apache.lenya.cms.publication.DocumentException;
-import org.apache.lenya.cms.publication.DocumentFactory;
-import org.apache.lenya.cms.publication.DocumentUtil;
-import org.apache.lenya.cms.publication.Publication;
-import org.apache.lenya.cms.publication.URLInformation;
-import org.apache.lenya.cms.repository.RepositoryUtil;
-import org.apache.lenya.cms.repository.Session;
-import org.apache.lenya.cms.site.SiteManager;
-import org.apache.lenya.cms.site.SiteStructure;
-import org.apache.lenya.util.ServletHelper;
 
 /**
  * <p>
@@ -108,107 +95,9 @@ public class SiteSourceFactory extends AbstractLogEnabled implements SourceFacto
      */
     public Source getSource(String location, Map parameters) throws MalformedURLException,
             IOException, SourceException {
-        String area = null;
-        String pubId;
-
         Map objectModel = ContextHelper.getObjectModel(this.context);
         Request request = ObjectModelHelper.getRequest(objectModel);
-
-        String completePath = location.split("\\?")[0];
-        String queryString = null;
-        if (location.indexOf("?") > -1) {
-            queryString = location.split("\\?")[1];
-        }
-
-        String relativePath;
-        try {
-
-            final String scheme = completePath.split(":")[0] + ":";
-            final String absolutePath = completePath.substring(scheme.length());
-            if (absolutePath.startsWith("//")) {
-                final String fullPath = absolutePath.substring(2);
-                String[] steps = fullPath.split("/");
-                pubId = steps[0];
-                area = steps[1];
-                String prefix = pubId + "/" + area;
-                relativePath = fullPath.substring(prefix.length());
-            } else if (absolutePath.startsWith("/")) {
-                String webappUrl = ServletHelper.getWebappURI(request);
-                URLInformation info = new URLInformation(webappUrl);
-                pubId = info.getPublicationId();
-                area = info.getArea();
-                relativePath = absolutePath;
-            } else {
-                throw new MalformedURLException("The path [" + absolutePath
-                        + "] must start with at least one slash.");
-            }
-
-            DocumentFactory factory = DocumentUtil.getDocumentFactory(this.manager, request);
-            Publication pub = factory.getPublication(pubId);
-            String[] steps = relativePath.substring(1).split("/");
-
-            String language = steps[0];
-            String prefix = "/" + language;
-            String path = relativePath.substring(prefix.length());
-
-            ServiceSelector selector = null;
-            SiteManager siteManager = null;
-
-            Document doc;
-
-            try {
-                selector = (ServiceSelector) this.manager.lookup(SiteManager.ROLE + "Selector");
-                siteManager = (SiteManager) selector.select(pub.getSiteManagerHint());
-                SiteStructure structure = siteManager.getSiteStructure(factory, pub, area);
-                doc = structure.getNode(path).getLink(language).getDocument();
-            } finally {
-                if (selector != null) {
-                    if (siteManager != null) {
-                        selector.release(siteManager);
-                    }
-                    this.manager.release(selector);
-                }
-            }
-
-            if (queryString != null && queryString.length() > 0) {
-                return getFormatSource(doc, queryString);
-            } else {
-                String lenyaURL = doc.getSourceURI();
-                Session session = RepositoryUtil.getSession(this.manager, request);
-                return new RepositorySource(manager, lenyaURL, session, getLogger());
-            }
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
-    protected Source getFormatSource(Document doc, String queryString) throws DocumentException, ServiceException, MalformedURLException, IOException {
-        String name = queryString.split("=")[0];
-        String value = queryString.split("=")[1];
-
-        if (name.equals("format")) {
-            String format = value;
-            String formatBaseUri = doc.getResourceType().getFormatURI(format);
-            String formatUri = formatBaseUri + "/" + doc.getPublication().getId() + "/"
-                    + doc.getArea() + "/" + doc.getUUID() + "/" + doc.getLanguage();
-            
-            SourceResolver resolver = null;
-            try {
-                resolver = (SourceResolver) this.manager.lookup(SourceResolver.ROLE);
-                return resolver.resolveURI(formatUri);
-            }
-            finally {
-                if (resolver != null) {
-                    this.manager.release(resolver);
-                }
-            }
-            
-        } else {
-            throw new MalformedURLException("The parameter [" + name
-                    + "] is not supported.");
-        }
+        return new SiteSource(this.manager, request, location);
     }
 
     /**
