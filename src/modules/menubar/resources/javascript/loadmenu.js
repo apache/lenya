bf2f78df19341ee191a4a0b5cf16d37a52dc8032
Andreas Hartmann <andreas@apache.org>
2007-08-22 14:55:36 +0000

    Added menu module, e.g. for asynchronous menu loading
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@568637 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/modules/menubar/resources/javascript/loadmenu.js
@@ -0,0 +1,49 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+dojo.require("dojo.logging.*");
+dojo.require("dojo.event.*");
+dojo.require("dojo.io.*");
+
+function loadMenu(e) {
+	var menuFunction = {
+		url: MENU_URL + "&lenya.module=menubar",
+		load: function(type, data, evt) {
+			var placeholderElement = document.getElementById("lenyaMenuPlaceholder");
+			var menuElement = document.getElementById("lenyaMenuContainer");
+			menuElement.removeChild(placeholderElement);
+			menuElement.appendChild(data.documentElement);
+			initialize();
+		},
+		error: function(type, error) {
+		alert(error.message);
+			dojo.log.error(error.message);
+		},
+		mimetype: "text/xml",
+		method: "GET"
+	};
+	dojo.io.bind(menuFunction);
+}
+
+/*
+* Customize this method for event-based loading.
+*/
+function initAjax() {
+	dojo.event.connect(dojo.byId("dojotest"), "onclick", "loadMenu");
+}
+
+dojo.addOnLoad(loadMenu);
