a33c28cd5b51e9ae14b21b7277b6c6150338748b
Andreas Hartmann <andreas@apache.org>
2007-12-17 16:24:19 +0000

    Added serial version UID to World class
    git-svn-id: https://svn.apache.org/repos/asf/lenya/branches/branch_1_2_x_shibboleth@604913 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/ac/World.java
+++ src/java/org/apache/lenya/ac/World.java
@@ -26,6 +26,8 @@ import java.io.Serializable;
  */
 public final class World implements Identifiable, Accreditable, Serializable {
     
+    private static final long serialVersionUID = 1L;
+    
     private static final String NAME = "everyone";
 
     /**
