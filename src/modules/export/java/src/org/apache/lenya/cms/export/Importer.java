7aeaefa39ec323d93feb4ea5a141daca6b649a62
Richard Frovarp <rfrovarp@apache.org>
2008-03-11 15:49:11 +0000

    [minor change] Clean up package line
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@635980 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/export/java/src/org/apache/lenya/cms/export/Importer.java
+++ src/modules/export/java/src/org/apache/lenya/cms/export/Importer.java
@@ -14,7 +14,8 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  *
- */package org.apache.lenya.cms.export;
+ */
+package org.apache.lenya.cms.export;
 
 import org.apache.avalon.framework.logger.AbstractLogEnabled;
 import org.apache.avalon.framework.logger.Logger;
