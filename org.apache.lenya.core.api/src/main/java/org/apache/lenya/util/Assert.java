669415c7ec06caeea005ac207f47ff63a27e34a4
Andreas Hartmann <andreas@apache.org>
2009-02-05 22:23:01 +0000

    [minor change] removed invalid char
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@741315 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.lenya.core.api/src/main/java/org/apache/lenya/util/Assert.java
+++ org.apache.lenya.core.api/src/main/java/org/apache/lenya/util/Assert.java
@@ -19,7 +19,7 @@ package org.apache.lenya.util;
 
 /**
  * Assertion utility.
- * @deprecated Use {@link org.springframework.util.Assert}Êinstead.
+ * @deprecated Use {@link org.springframework.util.Assert} instead.
  */
 public final class Assert {
 
