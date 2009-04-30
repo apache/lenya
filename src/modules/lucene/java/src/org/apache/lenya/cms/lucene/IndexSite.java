8c4844d182db87a2d71f9ba10ff0de287a5e25f0
Andreas Hartmann <andreas@apache.org>
2009-04-30 09:55:06 +0000

    Add option to index all areas, fixes bug 47128.
    git-svn-id: https://svn.apache.org/repos/asf/lenya/branches/BRANCH_2_0_X@770138 13f79535-47bb-0310-9956-ffa450edef68

--- src/modules/lucene/java/src/org/apache/lenya/cms/lucene/IndexSite.java
+++ src/modules/lucene/java/src/org/apache/lenya/cms/lucene/IndexSite.java
@@ -17,8 +17,12 @@
  */
 package org.apache.lenya.cms.lucene;
 
+import java.util.Arrays;
+
+import org.apache.avalon.framework.service.ServiceException;
 import org.apache.lenya.cms.publication.Area;
 import org.apache.lenya.cms.publication.Document;
+import org.apache.lenya.cms.publication.DocumentException;
 import org.apache.lenya.cms.publication.DocumentFactory;
 import org.apache.lenya.cms.publication.Publication;
 import org.apache.lenya.cms.publication.URLInformation;
@@ -29,6 +33,17 @@ import org.apache.lenya.cms.usecase.AbstractUsecase;
  */
 public class IndexSite extends AbstractUsecase {
 
+    public static final String PARAM_AREA = "area";
+    public static final String PARAM_INDEX_AREA = "indexArea";
+    
+    public void prepareView() {
+        String url = getSourceURL();
+        URLInformation info = new URLInformation(url);
+        String area = info.getArea();
+        setParameter(PARAM_AREA, area);
+        setParameter(PARAM_INDEX_AREA, "all");
+    }
+    
     public void doExecute() throws Exception {
         String url = getSourceURL();
         URLInformation info = new URLInformation(url);
@@ -36,8 +51,23 @@ public class IndexSite extends AbstractUsecase {
 
         DocumentFactory factory = getDocumentFactory();
         Publication pub = factory.getPublication(pubId);
-        Area area = pub.getArea(info.getArea());
 
+        String areaName = getParameterAsString(PARAM_INDEX_AREA);
+
+        String[] areaNames = pub.getAreaNames();
+        if (areaName != null && Arrays.asList(areaNames).contains(areaName)) {
+            Area area = pub.getArea(areaName);
+            indexArea(area);
+        } else {
+            for (int a = 0; a < areaNames.length; a++) {
+                Area area = pub.getArea(areaNames[a]);
+                indexArea(area);
+            }
+        }
+
+    }
+
+    protected void indexArea(Area area) throws ServiceException, DocumentException {
         Document[] docs = area.getDocuments();
 
         IndexUpdater updater = null;
@@ -45,8 +75,8 @@ public class IndexSite extends AbstractUsecase {
             updater = (IndexUpdater) this.manager.lookup(IndexUpdater.ROLE);
             for (int i = 0; i < docs.length; i++) {
                 try {
-                    updater.index(getSession(), docs[i].getResourceType(), pubId, area.getName(),
-                            docs[i].getUUID(), docs[i].getLanguage());
+                    updater.index(getSession(), docs[i].getResourceType(), area.getPublication()
+                            .getId(), area.getName(), docs[i].getUUID(), docs[i].getLanguage());
                 } catch (Exception e) {
                     String message = "Error indexing document [" + docs[i].getPath() + ":"
                             + docs[i].getLanguage() + "], UUID=" + docs[i].getUUID();
