cc0a656f1ee44e58a8b3dc2032e94bd36c2dfeb6
Florent André <florent@apache.org>
2010-11-09 17:47:30 +0000

    add a try for set up the spring environment
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@1033120 13f79535-47bb-0310-9956-ffa450edef68

--- org.apache.lenya.core.acusecases/src/test/java/org/apache/lenya/cms/ac/usecases/LoginTest.java
+++ org.apache.lenya.core.acusecases/src/test/java/org/apache/lenya/cms/ac/usecases/LoginTest.java
@@ -3,13 +3,23 @@ package org.apache.lenya.cms.ac.usecases;
 
 import static org.junit.Assert.assertEquals;
 
+import org.apache.lenya.utils.test.SpringEnv;
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-public class LoginTest {
-
-	@Ignore("Find a way to retrieve all the test context") @Test
-    public void testParametersInit(){
+	public class LoginTest {
+		
+		String mockURL = "/test/authoring";
+		@Before
+		public void setUp(){
+			SpringEnv.setMockRequestContextHolder(mockURL);
+		}
+		
+		
+		//Ignore("Find a way to test usecase") 
+		@Ignore("Find a way to test usecase") @Test
+    public void testInitParameters(){
 		Login login = new Login();
 		login.initParameters();
 		String pubName = login.getParameterAsString("Publication");
