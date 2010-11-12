a427f8a17446912d1796f566fec395c59f1d64b5
Florent André <florent@apache.org>
2010-11-12 10:13:33 +0000

    - add moved class to core-observation
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@1034334 13f79535-47bb-0310-9956-ffa450edef68

---
+++ org.apache.lenya.core.observation/src/main/java/org/apache/lenya/cms/observation/DocumentEventSource.java
@@ -0,0 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.lenya.cms.observation;
+
+import org.apache.commons.lang.Validate;
+import org.apache.lenya.cms.publication.Document;
+import org.apache.lenya.cms.publication.DocumentIdentifier;
+import org.apache.lenya.cms.publication.ResourceType;
+
+public class DocumentEventSource {
+    
+    private DocumentIdentifier identifier;
+    private ResourceType resourceType;
+
+    public DocumentEventSource(Document doc) {
+        Validate.notNull(doc, "document");
+        this.identifier = doc.getIdentifier();
+    }
+    
+    public DocumentIdentifier getIdentifier() {
+        return this.identifier;
+    }
+
+    public ResourceType getResourceType() {
+        return resourceType;
+    }
+
+}
---
+++ org.apache.lenya.core.observation/src/main/java/org/apache/lenya/cms/observation/RepositoryEventFactory.java
@@ -0,0 +1,133 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
+package org.apache.lenya.cms.observation;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.lenya.cms.publication.Document;
+import org.apache.lenya.cms.publication.DocumentImpl;
+import org.apache.lenya.cms.publication.Publication;
+import org.apache.lenya.cms.repository.Node;
+import org.apache.lenya.cms.repository.Session;
+
+/**
+ * Factory to create repository events.
+ */
+public class RepositoryEventFactory {
+
+    private static final Log logger = LogFactory.getLog(RepositoryEventFactory.class);
+
+    /**
+     * Creates a repository event without source..
+     * @param session The session.
+     * @param descriptor The descriptor.
+     * @return An event.
+     */
+    public static final RepositoryEvent createEvent(Session session, Object descriptor) {
+        return new RepositoryEvent(session, descriptor);
+    }
+
+    /**
+     * Creates a repository event for a node.
+     * @param doc The document.
+     * @param descriptor The descriptor.
+     * @return An event.
+     */
+    public static final RepositoryEvent createEvent(Document doc, Object descriptor) {
+        Node node = ((DocumentImpl) doc).getRepositoryNode();
+        RepositoryEvent event = new RepositoryEvent(node.getRepositorySession(),
+                new DocumentEventSource(doc), descriptor);
+        event.setNodeUri(node.getSourceURI());
+        int[] revisions = node.getHistory().getRevisionNumbers();
+        if (revisions.length > 0) {
+            event.setRevision(revisions[0]);
+        }
+        return event;
+
+    }
+
+    /**
+     * Creates a repository event for a node.
+     * @param node The node.
+     * @param descriptor The descriptor.
+     * @return An event.
+     */
+    public static final RepositoryEvent createEvent(Node node, Object descriptor) {
+        RepositoryEvent event;
+        Document doc = null;
+        if (!node.getSourceURI().endsWith("meta")) {
+            doc = getDocument(node);
+        }
+        if (doc != null) {
+            event = createEvent(doc, descriptor);
+        } else {
+            event = new RepositoryEvent(node.getRepositorySession(), descriptor);
+            event.setNodeUri(node.getSourceURI());
+        }
+        return event;
+    }
+
+    /**
+     * @param node The node.
+     * @return The document represented by the node or <code>null</code> if the node doesn't
+     *         represent a document.
+     */
+    protected static final Document getDocument(Node node) {
+
+        final String sourceUri = node.getSourceURI();
+        if (sourceUri.endsWith(".xml")) {
+            return null;
+        }
+
+        Document doc = null;
+
+        if (!sourceUri.startsWith("lenya://")) {
+            throw new IllegalStateException("The source URI [" + sourceUri
+                    + "] doesn't start with lenya://");
+        }
+
+        String path = sourceUri.substring("lenya://lenya/pubs/".length());
+
+        String[] steps = path.split("/");
+        String pubId = steps[0];
+        String area = steps[2];
+
+        try {
+
+            org.apache.lenya.cms.publication.Session session = (org.apache.lenya.cms.publication.Session) node
+                    .getRepositorySession();
+            Publication pub = session.getPublication(pubId);
+            String docPath = path.substring((pubId + "/content/" + area).length());
+
+            String uuid = docPath.substring(1, docPath.length() - "/en".length());
+            String language = docPath.substring(docPath.length() - "en".length());
+
+            doc = pub.getArea(area).getDocument(uuid, language);
+
+            if (doc == null) {
+                // this happens if the node was not a document node
+                logger.info("No document found for node [" + sourceUri + "]");
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return doc;
+    }
+
+}
