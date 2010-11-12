c3d811ce2cf1c4febf0c9abea8db1f865bb3454b
Florent André <florent@apache.org>
2010-11-12 18:52:26 +0000

    add api classes
    git-svn-id: https://svn.apache.org/repos/asf/lenya/trunk@1034509 13f79535-47bb-0310-9956-ffa450edef68

---
+++ org.apache.lenya.core.repository.api/src/main/java/org/apache/lenya/cms/observation/RepositoryEvent.java
@@ -0,0 +1,41 @@
+package org.apache.lenya.cms.observation;
+
+import org.apache.lenya.cms.repository.Session;
+
+public interface RepositoryEvent {
+
+	/**
+	 * @return The session.
+	 */
+	public abstract Session getSession();
+
+	/**
+	 * @return The descriptor.
+	 */
+	public abstract Object getDescriptor();
+
+	public abstract String toString();
+
+	/**
+	 * @param nodeUri The source URI of the affected node.
+	 */
+	public abstract void setNodeUri(String nodeUri);
+
+	/**
+	 * @return The source URI of the affected node.
+	 */
+	public abstract String getNodeUri();
+
+	/**
+	 * @param revision The latest revision of the node at the time the event was created.
+	 */
+	public abstract void setRevision(int revision);
+
+	/**
+	 * @return The latest revision of the node at the time the event was created.
+	 */
+	public abstract int getRevision();
+
+	public abstract Object getSource();
+
+}
