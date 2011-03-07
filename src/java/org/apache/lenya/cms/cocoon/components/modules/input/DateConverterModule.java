31c4d0c48091e49c2ffde7fc1b4bd8a82a89b51f
Andreas Hartmann <andreas@apache.org>
2011-03-07 15:50:08 +0000

    Set locale for DateConverterModule.
    git-svn-id: https://svn.apache.org/repos/asf/lenya/branches/BRANCH_2_1_X@1078818 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/lenya/cms/cocoon/components/modules/input/DateConverterModule.java
+++ src/java/org/apache/lenya/cms/cocoon/components/modules/input/DateConverterModule.java
@@ -19,46 +19,52 @@ package org.apache.lenya.cms.cocoon.components.modules.input;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 import java.util.Map;
 
 import org.apache.avalon.framework.configuration.Configuration;
 import org.apache.avalon.framework.configuration.ConfigurationException;
 import org.apache.avalon.framework.thread.ThreadSafe;
 import org.apache.cocoon.components.modules.input.AbstractInputModule;
+import org.apache.commons.lang.LocaleUtils;
 
 /**
- * The DateConverterModule converts a date string from one format into 
- * another format. 
- * The conversion is defined by the nested elements &lt;src-pattern/&gt; and 
- * &lt;pattern/&gt; of the module declaration.
- *
+ * The DateConverterModule converts a date string from one format into another format. The
+ * conversion is defined by the nested elements &lt;src-pattern/&gt; and &lt;pattern/&gt; of the
+ * module declaration.
+ * 
  */
 public class DateConverterModule extends AbstractInputModule implements ThreadSafe {
 
-    public Object getAttribute(String name, Configuration modeConf, Map objectModel) throws ConfigurationException {
-        
+    public Object getAttribute(String name, Configuration modeConf,
+            @SuppressWarnings("rawtypes") Map objectModel) throws ConfigurationException {
+
         String srcPattern = (String) this.settings.get("src-pattern");
         String pattern = (String) this.settings.get("pattern");
-        
+        String locale = (String) this.settings.get("locale");
+
         if (modeConf != null) {
             srcPattern = modeConf.getChild("src-pattern").getValue(srcPattern);
             pattern = modeConf.getChild("pattern").getValue(pattern);
+            locale = modeConf.getChild("locale").getValue(locale);
         }
 
-        if (srcPattern==null) {
+        if (srcPattern == null) {
             throw new ConfigurationException("Source date pattern not specified.");
         }
-        if (pattern==null) {
+        if (pattern == null) {
             throw new ConfigurationException("Date pattern not specified.");
         }
-        
+
         try {
-            SimpleDateFormat srcFormat = new SimpleDateFormat(srcPattern); 
-            SimpleDateFormat format = new SimpleDateFormat(pattern);
+            final SimpleDateFormat srcFormat = new SimpleDateFormat(srcPattern);
+            final Locale loc = (locale == null) ? Locale.getDefault() : LocaleUtils
+                    .toLocale(locale);
+            final SimpleDateFormat format = new SimpleDateFormat(pattern, loc);
             Date date = srcFormat.parse(name);
             return format.format(date);
         } catch (Exception e) {
-            throw new ConfigurationException("Could not convert date: "+name, e);
+            throw new ConfigurationException("Could not convert date: " + name, e);
         }
     }
 }
