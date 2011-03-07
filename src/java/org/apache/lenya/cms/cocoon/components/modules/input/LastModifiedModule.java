52ba8969188d989b080e513e4ca9b50c2f594ec0
Andreas Hartmann <andreas@apache.org>
2011-03-07 16:17:15 +0000

    Add last-modified header for CSS files.
    git-svn-id: https://svn.apache.org/repos/asf/lenya/branches/BRANCH_2_1_X@1078829 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/lenya/cms/cocoon/components/modules/input/LastModifiedModule.java
@@ -0,0 +1,50 @@
+package org.apache.lenya.cms.cocoon.components.modules.input;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+import org.apache.avalon.framework.configuration.Configuration;
+import org.apache.avalon.framework.configuration.ConfigurationException;
+import org.apache.avalon.framework.service.ServiceException;
+import org.apache.avalon.framework.service.ServiceManager;
+import org.apache.avalon.framework.service.Serviceable;
+import org.apache.excalibur.source.Source;
+import org.apache.excalibur.source.SourceResolver;
+
+public class LastModifiedModule extends AbstractInputModule implements Serviceable {
+
+    private ServiceManager manager;
+    protected static final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss Z");
+
+    @Override
+    public Object getAttribute(String name, Configuration modeConf,
+            @SuppressWarnings("rawtypes") Map objectModel) throws ConfigurationException {
+
+        SourceResolver resolver = null;
+        try {
+            resolver = (SourceResolver) this.manager.lookup(SourceResolver.ROLE);
+            Source source = null;
+            try {
+                source = resolver.resolveURI(name);
+                return format.format(new Date(source.getLastModified()));
+            } finally {
+                if (source != null) {
+                    resolver.release(source);
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (resolver != null) {
+                this.manager.release(resolver);
+            }
+        }
+    }
+
+    @Override
+    public void service(ServiceManager manager) throws ServiceException {
+        this.manager = manager;
+    }
+
+}
